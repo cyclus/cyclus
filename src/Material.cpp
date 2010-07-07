c4dec9f3529181dade9948aa38e0577b6f4d3231
Paul Wilson <uwgonuke@gmail.com>
2010-07-07 10:16:02 +0000

    Set up simplified input for materials.

--- src/Material.cpp
+++ src/Material.cpp
@@ -6,6 +6,56 @@ using namespace std;
 #include "Material.h"
 
 
+void Material::normalize(CompMap &comp_map)
+{
+    double sum_total_comp = 0;
+    CompMap::iterator entry;
+
+    for (entry = comp_map.begin(); entry != comp_map.end(); entry++)
+	sum_total_comp += (*entry).second;
+
+    for (entry = comp_map.begin(); entry != comp_map.end(); entry++)
+	(*entry).second /= sum_total_comp;
+
+}
+
+void Material::rationalize_A2M()
+{
+    
+    total_mass = 0;
+
+    for(CompMap::iterator entry = atom_comp.begin();
+	entry != atom_comp.end();
+	entry++)
+    {
+	mass_comp[(*entry).first] = (*entry).second * getA((*entry).first);
+	total_mass += mass_comp[(*entry).first];
+    }
+
+    total_mass *= total_atoms;
+
+    normalize(mass_comp);
+
+}
+
+void Material::rationalize_M2A()
+{
+
+    total_atoms = 0;
+    for(CompMap::iterator entry = mass_comp.begin();
+	entry != mass_comp.end();
+	entry++)
+    {
+	atom_comp[(*entry).first] = (*entry).second / getA((*entry).first);
+	total_atoms += atom_comp[(*entry).first];
+    }
+
+    total_atoms *= total_mass;
+
+    normalize(atom_comp);
+
+}
+
 Material::Material(istream &input)
 {
 
@@ -13,45 +63,38 @@ Material::Material(istream &input)
     bool atomComp = true;
     int numIsos;
     Iso isotope;
-    Atoms atom_dens;
-    Mass mass_dens;
+    double sum_total_comp, entry;
 
     input >> comp_type >> numIsos;
+    
+    CompMap &comp_map = ( "atom" != comp_type ? mass_comp : atom_comp );
+    double &read_total_comp = ( "atom" != comp_type ? total_mass : total_atoms);
 
-    if ("atom" != comp_type)
-	atomComp = false;
-
+    input >> read_total_comp;
+    
     for (int isoNum=0; isoNum<numIsos; isoNum++)
     {
-	input >> isotope;
-	input >> (atomComp ? atom_comp[isotope] : mass_comp[isotope]);
+	input >> isotope >> entry;
+	comp_map[isotope] = entry;
     }
 
-    if (atomComp)
-	atom2mass();
+    normalize(comp_map);
+
+    if ( "atom" != comp_type)
+	rationalize_M2A();
     else
-	mass2atom();
+	rationalize_A2M();
 
 }
 
-void Material::printAtomComp()
+void Material::printComp(string header, CompMap comp_map)
 {
 
-    cout << "Atomic composition:" << endl;
-    for (AtomComp::iterator iso = atom_comp.begin();
-	 iso != atom_comp.end();
+    cout << header << endl;
+    for (CompMap::iterator iso = comp_map.begin();
+	 iso != comp_map.end();
 	 iso++)
 	cout << (*iso).first << " : " <<  (*iso).second << endl;
     
 }
 
-void Material::printMassComp()
-{
-
-    cout << "Mass composition:" << endl;
-    for (MassComp::iterator iso = mass_comp.begin();
-	 iso != atom_comp.end();
-	 iso++)
-	cout << (*iso).first << " : " <<  (*iso).second << endl;
-
-}
--- src/Material.h
+++ src/Material.h
@@ -8,31 +8,39 @@ typedef int Iso;
 typedef int Ele;
 typedef double Atoms;
 typedef double Mass;
-typedef map<Iso,Atoms> AtomComp;
-typedef map<Iso,Mass> MassComp;
+typedef map<int,double> CompMap;
 
 class Material
 {
 
 private:
 
-    AtomComp atom_comp;
-    MassComp mass_comp;
+    CompMap atom_comp, mass_comp;
+    Mass total_mass;
+    Atoms total_atoms;
     
-    void atom2mass();
-    void mass2atom();
+    double getZ(Iso isotope) { return isotope/10/1000; };
+    double getA(Iso isotope) { return ((isotope/10)%1000); };
+    
+    void normalize(CompMap &comp_map);
+    void rationalize_A2M();
+    void rationalize_M2A();
 
     bool atomEqualsMass;
     
 public:
-
+    
     Material() {};
     Material(std::istream &input);
-
-    void print() { printAtomComp(); printMassComp();};
-    void printAtomComp();
-    void printMassComp();
-
+    
+    void print() {
+        printComp("Atom composition:", atom_comp); 
+	cout << "Total atoms: " << total_atoms << endl;
+	printComp("Mass composition:", mass_comp);
+	cout << "Total mass: " << total_mass << endl;
+        }
+    void printComp(string header, CompMap comp_map);
+    
 };
 
 
