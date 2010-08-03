9aba391e13f181080945db63973be47419085ceb
Katy Huff <katyhuff@gmail.com>
2010-08-03 15:21:31 +0000

    This moves a lot of function definitions in the Logician class from the .h file to the .cpp file.

--- src/Logician.cpp
+++ src/Logician.cpp
@@ -7,6 +7,12 @@
 Logician* Logician::_instance = 0;
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Logician::Logician() {};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Logician::~Logician() {};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Logician* Logician::Instance() {
   // If we haven't created a Logician yet, create and return it.
   if (0 == _instance)
@@ -16,8 +22,22 @@ Logician* Logician::Instance() {
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Model* Logician::getModelByName(ModelList list, const string search_name) {
-  
+void Logician::addModel(Model* new_model, ModelList &list)
+{ 
+  list.push_back(new_model);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::printModelList(ModelList list) 
+{
+  for (ModelList::iterator model = list.begin();
+       model != list.end();
+       model++) (*model)->print();
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getModelByName(ModelList list, string search_name)
+{
   Model* found_model = NULL;
   
   for(ModelList::iterator model=list.begin();
@@ -30,8 +50,122 @@ Model* Logician::getModelByName(ModelList list, const string search_name) {
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void Logician::printRecipes(){
+void Logician::addFacility(Model* new_facility) 
+{ 
+  facilities.push_back(new_facility); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::printFacilities()                
+{
+  printModelList(facilities); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int Logician::getNumFacilities()                
+{ 
+  return facilities.size(); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getFacilityByID(int ID)        
+{ 
+  return facilities[ID]; 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getFacilityByName(string name) 
+{ 
+  return getModelByName(facilities,name); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::addMarket(Model* new_market)   
+{ 
+  markets.push_back(new_market); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::printMarkets()                 
+{ 
+  printModelList(markets); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int Logician::getNumMarkets()                 
+{ 
+  return markets.size(); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getMarketByID(int ID)        
+{ 
+  return markets[ID]; 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getMarketByName(string name) 
+{ 
+  return getModelByName(markets,name); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getMarketByCommodity(string commodity_name) 
+{ 
+  return commodity_market_map[getCommodity(commodity_name)]; 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getMarketByCommodity(Commodity* commod) 
+{ 
+  return commodity_market_map[commod]; 
+} 
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::  registerCommodityMarket(Commodity* commod, Model* market)
+{ 
+  commodity_market_map[commod] = market; 
+} 
   
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::addRegion(Model* new_region)   
+{ 
+  regions.push_back(new_region); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::printRegions()                 
+{ 
+  printModelList(regions); 
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int Logician::getNumRegions()                 
+{ 
+  return regions.size(); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getRegionByID(int ID)        
+{ 
+  return regions[ID]; 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Model* Logician::getRegionByName(string name) 
+{ 
+  return getModelByName(regions,name); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::addRecipe(string name, Material* new_mat) 
+{ 
+  recipes[name] = new_mat; 
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::printRecipes()
+{
   for (RecipeList::iterator recipe=recipes.begin();
       recipe != recipes.end();
       recipe++){
@@ -40,5 +174,34 @@ void Logician::printRecipes(){
   }
 }
 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int Logician::getNumRecipes()                                  
+{ 
+  return recipes.size(); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Material* Logician::getRecipe(string name)                     
+{ 
+  return recipes[name]; 
+} 
+  
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Logician::addCommodity(Commodity* new_commod) 
+{ 
+  commodities[new_commod->getName()] = new_commod; 
+} 
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int Logician::getNumCommodities()                              
+{
+  return commodities.size(); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Commodity* Logician::getCommodity(string name)                 
+{ 
+  return commodities[name]; 
+} 
 
 
--- src/Logician.h
+++ src/Logician.h
@@ -35,7 +35,7 @@ private:
 	 * The (protected) constructor for this class, which can only 
 	 * be called indirectly by the client.
 	 */
-	Logician() { };
+	Logician();
 
 	/// lists of models
 	ModelList facilities, markets, regions;
@@ -52,7 +52,7 @@ private:
 	/**
 	 * (Recursively) deletes this Logician (and the objects it oversees).
 	 */
-	~Logician() {};
+	~Logician() ;
 
 
 public:
@@ -71,18 +71,15 @@ public:
 	 * @param new_model pointer to model-based entity to be added
 	 * @param list list to which this entity should be added
 	 */
-	void addModel(Model* new_model, ModelList &list)
-    	      { list.push_back(new_model);};
+	void addModel(Model* new_model, ModelList &list);
+
 	/*
 	 * Generic routine to print a list of model-based entities
 	 *
 	 * @param list list to print
 	 */
-	void printModelList(ModelList list)	
-  {for (ModelList::iterator model = list.begin();
-        model != list.end();
-        model++) (*model)->print();
-	}
+	void printModelList(ModelList list);	
+
 	/* 
 	 * Search a list of models for a particular name
 	 *
@@ -91,62 +88,166 @@ public:
 	 */
 	Model* getModelByName(ModelList list, string search_name);
 
-	/// add a facility to the list
-	void addFacility(Model* new_facility) { facilities.push_back(new_facility); }
-	/// print list of facilities
-	void printFacilities()                { printModelList(facilities); };
-	/// get number of facilities
-	int getNumFacilities()                { return facilities.size(); }
-	/// get a pointer to the facility based on its ID number
-	Model* getFacilityByID(int ID)        { return facilities[ID]; }
-	Model* getFacilityByName(string name) { return getModelByName(facilities,name); }
-	
-	/// add a market to the list
-	void addMarket(Model* new_market)   { markets.push_back(new_market); }
-	/// print list of markets
-	void printMarkets()                 { printModelList(markets); };
-	/// get number of markets
-	int getNumMarkets()                 { return markets.size(); }
-	/// get a pointer to a market based on its ID number
-	Model* getMarketByID(int ID)        { return markets[ID]; }
-	/// get a pointer to a market based on its name
-	Model* getMarketByName(string name) { return getModelByName(markets,name); }
-	/// get a pointer to a market based on its commodity name
-	Model* getMarketByCommodity(string commodity_name) 
-               { return commodity_market_map[getCommodity(commodity_name)]; };
-	/// get a pointer to a market based on its commodity pointer
-	Model* getMarketByCommodity(Commodity* commod) 
-               { return commodity_market_map[commod]; } ;
-	/// register a commodity with a market
-	void   registerCommodityMarket(Commodity* commod, Model* market)
-	       { commodity_market_map[commod] = market; } ;
-	
-	/// add a region to the list
-	void addRegion(Model* new_region)   { regions.push_back(new_region); }
-	/// print list of regions
-	void printRegions()                 { printModelList(regions); };
-	/// get number of regions
-	int getNumRegions()                 { return regions.size(); }
-	/// get a pointer to a region based on its ID number
-	Model* getRegionByID(int ID)        { return regions[ID]; }
-	/// get a pointer to a region based on its name
-	Model* getRegionByName(string name) { return getModelByName(regions,name); }
+	/**
+   * add a facility to the list
+   *
+   * @param new_facility the new facility to add to the list
+   */
+	void addFacility(Model* new_facility);
 
-	/// add a recipe to the list
-	void addRecipe(string name, Material* new_mat) { recipes[name] = new_mat; };
-	/// print list of recipes
-	void printRecipes();
-	/// get number or recipes
-	int getNumRecipes()                                  { return recipes.size(); }
-	/// get a pointer to the recipe based on its name
-	Material* getRecipe(string name)                     { return recipes[name]; } 
+	/**
+   * print list of facilities
+   */
+	void printFacilities();
+
+	/** 
+   * get number of facilities
+   */
+	int getNumFacilities(); 
+
+	/**
+   * get a pointer to the facility based on its ID number
+   *
+   * @param ID the ID number of the facility whose pointer to return.
+   */
+	Model* getFacilityByID(int ID);        
+
+  /**
+   * get a pointer to the facility based on its name
+   *
+   * @param name the name of the facility, a string
+   */
+	Model* getFacilityByName(string name); 
 	
-	/// add a commodity to the list
-	void addCommodity(Commodity* new_commod) { commodities[new_commod->getName()] = new_commod; } ;
-	/// get number or commodities
-	int getNumCommodities()                              { return commodities.size(); }
-	/// get a pointer to the recipe based on its name
-	Commodity* getCommodity(string name)                 { return commodities[name]; } 
+	/**
+   * add a market to the list
+   *
+   * @param new_market the market to add to the list
+   */
+	void addMarket(Model* new_market);   
+
+	/**
+   * print list of markets
+   */
+	void printMarkets();
+
+	/**
+   * returns number of markets
+   */
+	int getNumMarkets();                 
+
+	/**
+   * get a pointer to a market based on its ID number
+   *
+   * @param ID the ID number of the market to return
+   */
+	Model* getMarketByID(int ID);        
+
+	/**
+   * get a pointer to a market based on its name
+   *
+   * @param name the name of the market whose pointer to return
+   */
+	Model* getMarketByName(string name); 
+
+	/**
+   * get a pointer to a market based on its commodity name
+   *
+   * @param commodity_name the name of the commodity
+   */
+	Model* getMarketByCommodity(string commodity_name);
+
+	/**
+   * get a pointer to a market based on its commodity pointer
+   *
+   * @param commod the pointer to a commodity
+   */
+	Model* getMarketByCommodity(Commodity* commod); 
+                ;
+
+	/**
+   * register a commodity with a market
+   * 
+   * @param commod a pointer to the commodity to register
+   * @param market a pointer to the market with which to register the commod.
+   */
+	void   registerCommodityMarket(Commodity* commod, Model* market);
+	        ;
+	
+	/**
+   * add a region to the list
+   *
+   * @param new_region
+   */
+	void addRegion(Model* new_region);   
+
+	/** 
+   * print list of regions
+   */
+	void printRegions();
+
+	/**
+   * get number of regions
+   */
+	int getNumRegions();                 
+
+	/**
+   * get a pointer to a region based on its ID number
+   *
+   * @param ID the ID number of the region for which to return a pointer
+   */
+	Model* getRegionByID(int ID);        
+
+	/**
+   * get a pointer to a region based on its name
+   *
+   * @param name the name of the region for which to return a pointer
+   */
+	Model* getRegionByName(string name); 
+
+	/**
+   * add a recipe to the list
+   *
+   * @param name the name of the recipe to add 
+   * @param new_mat the material object to add to the recipes list
+   */
+	void addRecipe(string name, Material* new_mat);
+
+	/**
+   * print list of recipes
+   */
+	void printRecipes();
+
+	/**
+   * get number of recipes
+   */
+	int getNumRecipes();                                  
+
+	/**
+   * get a pointer to the recipe based on its name
+   *
+   * @param name the name of the recipe for which to return a material pointer.
+   */
+	Material* getRecipe(string name);                      
+	
+	/**
+   * add a commodity to the list
+   *
+   * @param new_commod the new commodity to add to the list 
+   */
+	void addCommodity(Commodity* new_commod);
+
+	/**
+   * get number or commodities
+   */
+	int getNumCommodities(); 
+
+	/**
+   * get a pointer to the commodity based on its name
+   *
+   * @param name the name of the commodity for which to return a pointer.
+   */
+	Commodity* getCommodity(string name);
 
 
 };
