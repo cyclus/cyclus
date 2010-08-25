6be6a45ea94b7595fbbc980fd5d5272962e65c44
Katy Huff <katyhuff@gmail.com>
2010-08-25 14:37:40 +0000

    This adds a NullMarket which behaves very much like the GreedyMarket but doesn't reject requests when they cannot be filled completely. Instead, it fills the first biggest request until it either runs out of offers or that request has been filled. If it can be filled, it then starts to fill the remaining requests with the remaining offers.

---
+++ src/Models/Market/NullMarket.cpp
@@ -0,0 +1,176 @@
+// NullMarket.cpp
+// Implements the NullMarket class
+#include <iostream>
+
+#include "NullMarket.h"
+
+#include "Logician.h"
+#include "GenException.h"
+#include "InputXML.h"
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+void NullMarket::receiveMessage(Message *msg)
+{
+  messages.insert(msg);
+
+  if (msg->getAmount() > 0){
+    offers.insert(indexedMsg(msg->getAmount(),msg));
+  }
+  else{
+    requests.insert(indexedMsg(-msg->getAmount(),msg));
+  }
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+void NullMarket::reject_request(sortedMsgList::iterator request)
+{
+
+  // delete the tentative orders
+  while ( orders.size() > firmOrders)
+  {
+    delete orders.back();
+    orders.pop_back();
+  }
+
+  // put all matched offers back in the sorted list
+  while (matchedOffers.size() > 0)
+  {
+    Message *msg = *(matchedOffers.begin());
+    offers.insert(indexedMsg(msg->getAmount(),msg));
+    matchedOffers.erase(msg);
+  }
+
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+void NullMarket::process_request()
+{
+  // update pointer to firm orders
+  firmOrders = orders.size();
+
+  while (matchedOffers.size() > 0)
+  {
+    Message *msg = *(matchedOffers.begin());
+    messages.erase(msg);
+    matchedOffers.erase(msg);
+  }
+}
+ 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+bool NullMarket::match_request(sortedMsgList::iterator request)
+{
+  sortedMsgList::iterator offer;
+  double requestAmt,offerAmt, toRet;
+  Message *offerMsg, *requestMsg;
+
+  requestAmt = (*request).first;
+  requestMsg = (*request).second;
+  
+  // if this request is not yet satisfied &&
+  // there are more offers left
+  while ( requestAmt > 0 && offers.size() > 0)
+  {
+    // get a new offer
+    offer = offers.end();
+    offer--;
+    offerAmt = (*offer).first;
+    offerMsg = (*offer).second;
+
+    // pop off this offer
+    offers.erase(offer);
+  
+    if (requestAmt > offerAmt) { 
+      // put a new message in the order stack
+      // it goes down to supplier
+      offerMsg->setRequesterID(requestMsg->getRequesterID());
+      offerMsg->setDir(down);
+
+      // Queue an order
+      matchedOffers.insert(offerMsg);
+      // Zero out the boolean. 
+      // At least some of the request will be met.
+      toRet = 0;
+
+      orders.push_back(offerMsg);
+
+      cout << "NullMarket has resolved a match from "
+          << offerMsg->getSupplierID()
+          << " to "
+          << offerMsg->getRequesterID() 
+          << " for the amount:  " 
+          << offerMsg->getAmount() << endl;
+
+      requestAmt -= offerAmt;
+    } 
+    else {
+      // split offer
+
+      // queue a new order
+      Message* maybe_offer = new Message(*offerMsg);
+      maybe_offer->setAmount(requestAmt);
+      maybe_offer->setDir(down);
+      maybe_offer->setRequesterID(requestMsg->getRequesterID());
+
+      matchedOffers.insert(offerMsg);
+
+      orders.push_back(maybe_offer);
+
+      cout << "NullMarket has resolved a match from "
+          << maybe_offer->getSupplierID()
+          << " to "
+          << maybe_offer->getRequesterID() 
+          << " for the amount:  " 
+          << maybe_offer->getAmount() << endl;
+
+      // make a new offer with reduced amount
+      offerAmt -= requestAmt;
+      Message *new_offer = new Message(*offerMsg);
+      new_offer->setAmount(offerAmt);
+
+      
+      // call this method for consistency
+      receiveMessage(new_offer);
+
+      // zero out request
+      requestAmt = 0;
+
+      // Zero out the boolean. 
+      // All of the request will be met.
+      toRet = 0;
+    }
+  }
+
+  // if any of the request has been met, return true.
+  return (0 == toRet);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+void NullMarket::resolve()
+{
+  sortedMsgList::iterator request;
+
+  firmOrders = 0;
+
+  /// while requests remain and there is at least one offer left
+  while (requests.size() > 0)
+  {
+    request = requests.end();
+    request--;
+    
+    if(match_request(request)) {
+      process_request();
+    } 
+    else {
+      cout << "The request from Requester "<< (*request).second->getRequesterID() 
+          << " for the amount " << (*request).first 
+          << " rejected. "<<endl;
+      reject_request(request);
+    }
+    // remove this request
+    messages.erase((*request).second);
+    requests.erase(request);
+  }
+
+  executeOrderQueue();
+}
+
---
+++ src/Models/Market/NullMarket.h
@@ -0,0 +1,152 @@
+// NullMarket.h
+#if !defined(_NULLMARKET_H)
+#define _NULLMARKET_H
+#include <iostream>
+#include <map>
+#include <deque>
+
+#include "MarketModel.h"
+
+/**
+ * The NullMarket class inherits from the MarketModel class and is dynamically
+ * loaded by the Model class when requested.
+ * 
+ * This market will take a set of requests and match the biggest requests with
+ * the biggest offers first.
+ *
+ */
+class NullMarket : public MarketModel  
+{
+/* --------------------
+ * all MODEL classes have these public members
+ * --------------------
+ */
+
+public:
+  /**
+   * Default constructor
+   */
+  NullMarket() {};
+  
+  /** 
+   * Destructor
+   */
+  ~NullMarket() {};
+  
+  // different ways to populate an object after creation
+  /// initialize an object from XML input
+  virtual void init(xmlNodePtr cur)  { MarketModel::init(cur); };
+
+  /// initialize an object by copying another
+  virtual void copy(NullMarket* src) { MarketModel::copy(src); } ;
+
+  /** 
+   * Prints information about this model.
+   */
+  virtual void print()       { MarketModel::print();   } ;
+
+/* -------------------- */
+
+/* --------------------
+ * all COMMUNICATOR classes have these members
+ * --------------------
+ */
+
+  /**
+   * The market receives an offer or request that has been passed from the
+   * facility to the institution to the region.
+   *
+   * @param msg is a pointer to the message, an Message object
+   */
+  virtual void receiveMessage(Message* msg);
+/* -------------------- */
+
+
+/* --------------------
+ * all MARKETMODEL classes have these members
+ * --------------------
+ */
+
+  /**
+   * This instructs the market to make matches with the offers and requests it
+   * has on hand.
+   */
+  virtual void resolve();
+
+/* -------------------- */
+
+/* --------------------
+ * _THIS_ MARKETMODEL class has these members
+ * --------------------
+ */
+
+/* -------------------- */
+
+private:
+  /**
+   * The messages that have been indexed
+   */
+  typedef pair<double,Message*> indexedMsg;
+
+  /**
+   * The messages of both offer and request types that have been sorted
+   * according to thie size.
+   */
+  typedef multimap<double,Message*> sortedMsgList;
+  
+  /** 
+   * The requests that have been sorted according to their size.
+   */
+  sortedMsgList requests;
+
+  /**
+   * The offers that have been sorted according to their size.
+   */
+  sortedMsgList offers;
+
+  /**
+   * The set of pointers to offers that have been matched.
+   */
+  set<Message*> matchedOffers;
+  
+  /**
+   * This function adds an Message object to the list of matchedOffers
+   *
+   * @param msg a pointer to the message to add
+   */
+  void add(Message* msg);
+
+  /**
+   * A boolean that keeps track of whether requests have been matched.
+   * True if matched, false if umatched.
+   */
+  bool match_request(sortedMsgList::iterator request);
+
+  /**
+   * A boolean that keeps track of whether requests have been rejected.
+   * True if rejected, false if not rejected.
+   */
+  void reject_request(sortedMsgList::iterator request);
+
+  /**
+   * This function processes the information in a request.
+   */
+  void process_request();
+
+};
+
+/* --------------------
+ * all MODEL classes have these members
+ * --------------------
+ */
+extern "C" Model* construct() {
+  return new NullMarket();
+}
+
+extern "C" void destruct(Model* p) {
+  delete p;
+}
+
+/* -------------------- */
+
+#endif
