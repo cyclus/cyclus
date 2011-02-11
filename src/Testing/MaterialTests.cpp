154addddca7093e6e8fd2440f0a4a854080afe44
Katy Huff <katyhuff@gmail.com>
2011-02-11 18:23:50 +0000

    More testing stuff, for the wiki. Includes broken MaterialTest file. To be fixed during workshop in a bit.

---
+++ src/Testing/MaterialTests.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "Material.h"
+
+class MaterialTest : public ::testing::Test {
+public:
+
+  CompMap test_comp;
+  string test_mat_unit;
+  string test_rec_name;
+  double test_size;
+  Basis test_type;
+  Material* test_mat;
+
+  virtual void SetUp(){
+  Iso u235 = 92235;
+  Atoms one = 1.0;
+  test_comp[u235]=one;
+  test_mat_unit = "test_mat_unit";
+  test_rec_name = "test_rec_name";
+  test_size = 10.0;
+  test_type = atomBased;
+
+  //Material* test_mat = new Material(test_comp, test_mat_unit, test_rec_name, test_size, test_type); 
+  Material* test_mat = new Material(); 
+  }
+};
+
+TEST(MaterialTest, ManualConstructor){
+  //EXPECT_EQ(test_mat->getUnits(), "test_mat_unit");
+  EXPECT_EQ(MaterialTest::test_mat->getTotAtoms(),0);
+}
+
+
+
+
+
