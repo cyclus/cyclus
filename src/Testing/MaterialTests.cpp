0c584bf5baedbfbe26bce3fbbf13d563f4882940
Katy Huff <katyhuff@gmail.com>
2011-02-14 22:55:13 +0000

    improves MaterialTest::ManualConstructor test to do slightly more interesting things.

--- src/Testing/MaterialTests.cpp
+++ src/Testing/MaterialTests.cpp
@@ -4,6 +4,8 @@
 
 class MaterialTest : public ::testing::Test {
   protected:
+    Iso u235;
+    Atoms one;
     CompMap test_comp;
     string test_mat_unit;
     string test_rec_name;
@@ -12,7 +14,7 @@ class MaterialTest : public ::testing::Test {
     Material* test_mat;
 
     virtual void SetUp(){
-      Iso u235 = 92235;
+      u235 = 92235;
       Atoms one = 1.0;
       test_comp[u235]=one;
       test_mat_unit = "test_mat_unit";
@@ -21,13 +23,16 @@ class MaterialTest : public ::testing::Test {
       test_type = atomBased;
 
       test_mat = new Material(test_comp, test_mat_unit, test_rec_name, test_size, test_type); 
-      //Material* test_mat = new Material(); 
     };
 };
 
 TEST_F(MaterialTest, ManualConstructor) {
-  EXPECT_EQ(test_mat->getUnits(), "test_mat_unit");
-  //EXPECT_EQ(MaterialTest::test_mat->getTotAtoms(),0);
+  EXPECT_EQ(       test_mat->getUnits(),        "test_mat_unit"       );
+  EXPECT_EQ(       test_mat->getTotAtoms(),     10                    );
+  ASSERT_NEAR(     test_mat->getTotMass(),      2.35,            0.001); // 10mol U235 ~= 2.35kg
+  ASSERT_NEAR(     test_mat->getMassComp(u235), 1,               0.001); // normalized 
+  ASSERT_NEAR(     test_mat->getComp(u235),     1,               0.001); // normalized 
 }
+
 
 
