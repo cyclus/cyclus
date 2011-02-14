2fe2723af9e18c0d8e426b97e24f60c056bf7e9b
Katy Huff <katyhuff@gmail.com>
2011-02-14 22:33:17 +0000

    This adds a functioning test within the MaterialTest test case. It is called ManualConstructor tests one of the material connstrucors in a very basic way.

--- src/Testing/MaterialTests.cpp
+++ src/Testing/MaterialTests.cpp
@@ -1,28 +1,33 @@
 #include <gtest/gtest.h>
 #include "Material.h"
 
-TEST(MaterialTest, ManualConstructor){
-  CompMap test_comp;
-  string test_mat_unit;
-  string test_rec_name;
-  double test_size;
-  Basis test_type;
-  Material* test_mat;
 
-  Iso u235 = 92235;
-  Atoms one = 1.0;
-  test_comp[u235]=one;
-  test_mat_unit = "test_mat_unit";
-  test_rec_name = "test_rec_name";
-  test_size = 10.0;
-  test_type = atomBased;
+class MaterialTest : public ::testing::Test {
+  protected:
+    CompMap test_comp;
+    string test_mat_unit;
+    string test_rec_name;
+    double test_size;
+    Basis test_type;
+    Material* test_mat;
 
-  test_mat = new Material(test_comp, test_mat_unit, test_rec_name, test_size, test_type); 
-  //Material* test_mat = new Material(); 
+    virtual void SetUp(){
+      Iso u235 = 92235;
+      Atoms one = 1.0;
+      test_comp[u235]=one;
+      test_mat_unit = "test_mat_unit";
+      test_rec_name = "test_rec_name";
+      test_size = 10.0;
+      test_type = atomBased;
 
+      test_mat = new Material(test_comp, test_mat_unit, test_rec_name, test_size, test_type); 
+      //Material* test_mat = new Material(); 
+    };
+};
+
+TEST_F(MaterialTest, ManualConstructor) {
   EXPECT_EQ(test_mat->getUnits(), "test_mat_unit");
   //EXPECT_EQ(MaterialTest::test_mat->getTotAtoms(),0);
-} // MaterialTest
-
+}
 
 
