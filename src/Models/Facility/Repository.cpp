f6e2f0f18a3d2ede1a1dab2c7fd159a83675bea6
Katy Huff <katyhuff@gmail.com>
2011-07-21 14:09:55 +0000

    This adds skeletal repository facility models to version control.

---
+++ src/Models/Facility/Repository.cpp
@@ -0,0 +1,231 @@
+// Repository.cpp
+// Implements the Repository class
+#include <iostream>
+
+#include "Repository.h"
+
+#include "Logician.h"
+#include "GenException.h"
+#include "InputXML.h"
+#include "Timer.h"
+
+
+
+/**
+ * The Repository class inherits from the FacilityModel class and is dynamically
+ * loaded by the Model class when requested.
+ * 
+ * This facility model is intended to calculate nuclide and heat metrics over
+ * time in the repository. It will make appropriate requests for spent fuel 
+ * which derive from heat- and perhaps dose- limited space availability. 
+ *
+ * BEGINNING OF SIMULATION
+ * At the beginning of the simulation, this facility model loads the 
+ * components within it and figures out its initial capacity for each heat or 
+ * dose generating waste type it expects to accept. 
+ *
+ * TICK
+ * Examining the stocks, materials recieved last month are emplaced.
+ * The repository determines its current capacity for the first of the 
+ * incommodities (waste classifications?) and requests as much 
+ * of the incommodities that it can fit. The next incommodity is on the docket 
+ * for next month. 
+ *
+ * TOCK
+ * The repository passes the Tock radially outward through its 
+ * components.
+ *
+ * (r = 0) -> -> -> -> -> -> -> ( r = R ) 
+ * mat -> form -> package -> buffer -> barrier -> near -> far
+ *
+ * RECEIVE MATERIAL
+ * Put the material in the stocks
+ *
+ * RECEIVE MESSAGE
+ * reject it, I don't do messages.
+ *
+ *
+ */
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::init(xmlNodePtr cur)
+{ 
+  FacilityModel::init(cur);
+  
+  // move XML pointer to current model
+  cur = XMLinput->get_xpath_element(cur,"model/Repository");
+
+  // initialize ordinary objects
+  capacity = atof(XMLinput->get_xpath_content(cur,"capacity"));
+  inventory_size = atof(XMLinput->get_xpath_content(cur,"inventorysize"));
+  startOpYr = atoi(XMLinput->get_xpath_content(cur,"startOperYear"));
+  startOpMo = atoi(XMLinput->get_xpath_content(cur,"startOperMonth"));
+
+  // The repository accepts any commodities designated waste.
+  // This will be a list
+  string commod_name;
+  Commodity* in_commod;
+  xmlNodeSetPtr nodes = XMLinput->get_xpath_elements(cur, "incommodity");
+
+  // there are potentially many in commodities, but no outcommodities
+  for (int i=0;i<nodes->nodeNr;i++){
+    xmlNodePtr ptr = nodes->nodeTab[i];
+
+    // get in_commod
+    commod_name = XMLinput->get_xpath_content(ptr,"text");
+    in_commod = LI->getCommodity(commod_name);
+    if (NULL == in_commod)
+      throw GenException("Input commodity '" + commod_name 
+          + "' does not exist for facility '" + getName() 
+          + "'.");
+    in_commods.push_back(in_commod);
+  };
+
+  stocks = deque<Material*>();
+  inventory = deque< Material* >();
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void Repository::copy(Repository* src)
+{
+
+  FacilityModel::copy(src);
+
+  capacity = src->capacity;
+  inventory_size = src->inventory_size;
+  startOpYr = src->startOpYr;
+  startOpMo = src->startOpMo;
+  in_commods = src->in_commods;
+
+  stocks = deque<Material*>();
+  inventory = deque< Material>();
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::copyFreshModel(Model* src)
+{
+  copy((Repository*)(src));
+}
+
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::print() 
+{ 
+  FacilityModel::print(); 
+  cout << "stores commodity {"
+      << in_commods.front()->getName();
+      << "} among others."  << endl;
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::receiveMessage(Message* msg)
+{
+  throw GenException("Repository doesn't know what to do with a msg.");
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::receiveMaterial(Transaction trans, vector<Material*> manifest)
+{
+  // grab each material object off of the manifest
+  // and move it into the stocks.
+  for (vector<Material*>::iterator thisMat=manifest.begin();
+       thisMat != manifest.end();
+       thisMat++)
+  {
+    cout<<"Repository " << ID << " is receiving material with mass "
+        << (*thisMat)->getTotMass() << endl;
+    stocks.push_front(*thisMat);
+  }
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::handleTick(int time)
+{
+  // EMPLACE WASTE
+  emplaceWaste();
+
+  // MAKE A REQUEST
+  if(this->checkStocks() == 0){
+    // It chooses the next incommodity in the preference lineup
+    Commodity* request_commod;
+    request_commod = in_commods.front().first;
+
+    // It then moves that commodity from the front to the back of the preference lineup
+    in_commods.push_back(request_commod_pair);
+    in_commods.pop_front();
+  
+    // It can accept amounts however small
+    Mass requestAmt;
+    Mass minAmt = 0;
+    // The Repository should ask for material unless it's full
+    Mass inv = this->checkInventory();
+    // including how much is already in its stocks
+    Mass sto = this->checkStocks(); 
+    // this will be a request for free stuff
+    double commod_price = 0;
+    // subtract inv and sto from inventory max size to get total empty space
+    Mass space = inventory_size - inv - sto;
+  
+    if (space == 0){
+      // don't request anything
+    }
+    else if (space <= capacity){
+      Communicator* recipient = (Communicator*)(in_commod->getMarket());
+      // if empty space is less than monthly acceptance capacity
+      requestAmt = space;
+      // recall that requests have a negative amount
+      Message* request = new Message(up, in_commod, -requestAmt, minAmt, 
+                                       commod_price, this, recipient);
+        // pass the message up to the inst
+        (request->getInst())->receiveMessage(request);
+    }
+    // otherwise
+    else if (space >= capacity){
+    // the upper bound is the monthly acceptance capacity
+      requestAmt = capacity;
+      // recall that requests have a negative amount
+      Message* request = new Message(up, in_commod, -requestAmt, minAmt, commod_price,
+                                     this, recipient); 
+      // send it
+      sendMessage(request);
+  };
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+void Repository::handleTock(int time)
+{
+  // send tock to the emplaced material module
+  
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+Mass Repository::checkInventory(){
+  Mass total = 0;
+
+  // Iterate through the inventory and sum the amount of whatever
+  // material unit is in each object.
+  for (deque< Material*>::iterator iter = inventory.begin(); 
+       iter != inventory.end(); 
+       iter ++){
+    total += (*iter)->getTotMass();
+  }
+
+  return total;
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+Mass Repository::checkStocks(){
+  Mass total = 0;
+
+  // Iterate through the stocks and sum the amount of whatever
+  // material unit is in each object.
+
+  if(!stocks.empty()){
+    for (deque< Material* >::iterator iter = stocks.begin(); 
+         iter != stocks.end(); 
+         iter ++){
+        total += (*iter)->getTotMass();
+    };
+  };
+  return total;
+}
+
+
---
+++ src/Models/Facility/Repository.h
@@ -0,0 +1,227 @@
+// Repository.h
+#if !defined(_REPOSITORY_H)
+#define _REPOSITORY_H
+#include <iostream>
+#include <queue>
+
+#include "FacilityModel.h"
+
+/**
+ * The Repository class inherits from the FacilityModel class and is 
+ * dynamically
+ * loaded by the Model class when requested.
+ * 
+ * This facility model does very little.  New material is added to 
+ * queue inventory
+ * and old material is removed from the same queue inventory.
+ *
+ */
+
+class Repository : public FacilityModel  {
+/* --------------------
+ * all MODEL classes have these members
+ * --------------------
+ */
+
+public:
+  /** Default constructor for the Repository class.
+   */
+  Repository() {};
+
+  /**
+   * Destructor for the Repository class. 
+   */
+  ~Repository() {};
+  
+  // different ways to populate an object after creation
+  /// initialize an object from XML input
+  virtual void init(xmlNodePtr cur);
+
+  /// initialize an object by copying another
+  virtual void copy(Repository* src);
+  /**
+   * This drills down the dependency tree to initialize all relevant 
+   * parameters/containers.
+   *
+   * Note that this function must be defined only in the specific 
+   * model in question and not in any inherited models preceding it.
+   *
+   * @param src the pointer to the original model to be copied
+   */
+  virtual void copyFreshModel(Model* src);
+
+  /**
+   * Print information about this model
+   */
+  virtual void print();
+
+/* ------------------- */ 
+
+/* --------------------
+ * all COMMUNICATOR classes have these members
+ * --------------------
+ */
+
+public:
+  /**
+   * When the facility receives a message, execute any transaction 
+   * therein
+   */
+    virtual void receiveMessage(Message* msg);
+
+/* -------------------- */
+
+/* --------------------
+ * all FACILITYMODEL classes have these members
+ * --------------------
+ */
+
+public:
+    /**
+     * This sends material up the Inst/Region/Logician line
+     * to be passed back down to the receiver
+     *
+     * @param trans the Transaction defining the order being filled
+     * @param receiver the last facility to receive this transaction
+     */
+    virtual void sendMaterial(Message* msg, const Communicator* 
+        receiver);
+    
+    /**
+     * The facility receives the materials other facilities have sent.
+     *
+     * @param trans the Transaction defining the order being filled
+     * @param manifest the list of material objects being received
+     */
+    virtual void receiveMaterial(Transaction trans, vector<Material*> 
+        manifest);
+
+    /**
+     * The handleTick function specific to the Repository.
+     * At each tick, it requests as much raw inCommod as it can 
+     * process this * month and offers as much outCommod as it 
+     * will have in its inventory by the end of the month.
+     *
+     * @param time the time of the tick
+     */
+    virtual void handleTick(int time);
+
+    /**
+     * The handleTick function specific to the Repository.
+     * At each tock, it processes material and handles orders, and 
+     * records this month's actions.
+     *
+     * @param time the time of the tock
+     */
+    virtual void handleTock(int time);
+
+
+/* ------------------- */ 
+
+/* --------------------
+ * _THIS_ FACILITYMODEL class has these members
+ * --------------------
+ */
+
+public:
+
+protected:
+    /**
+     * The Repository has many input commodities
+     */
+    deque<Commodity*> in_commods;
+
+    /**
+     * The Repository has a limit to how material it can process.
+     * Units vary. It will be in the commodity unit per month.
+     */
+    double capacity;
+
+    /**
+     * The stocks of fresh fuel assemblies available.
+     */
+    deque<InFuel> stocks;
+
+    /**
+     * The inventory of spent fuel assemblies.
+     */
+    deque<OutFuel> inventory;
+
+    /**
+     * get the total mass of the stuff in the inventory
+     *
+     * @return the total mass of the processed materials in storage
+     */
+    Mass checkInventory();
+
+    /**
+     * get the total mass of the stuff in the stocks
+     *
+     * @return the total mass of the raw materials in storage
+     */
+    Mass checkStocks();
+
+    /**
+     * The maximum size to which the inventory may grow..
+     * The Repository must stop processing the material in its stocks 
+     * when its inventory is full.
+     */
+    Mass inventory_size;
+
+    /**
+     * The number of months that a facility stays operational.
+     * hopefully, this repository is forever, but ust in case... 
+     */
+    int lifetime;
+
+    /**
+     * The year in which construction of the facility begins.
+     * (maybe this should just be in the deployment description?)
+     */
+    int startConstrYr;
+
+    /**
+     * The month in which construction of the facility begins.
+     * (maybe this should just be in the deployment description?)
+     */
+    int startConstrMo;
+
+    /**
+     * The year in which operation of the facility begins.
+     * (maybe this should just be in the deployment description?)
+     */
+    int startOpYr;
+
+    /**
+     * The month in which operation of the facility begins.
+     * (maybe this should just be in the deployment description?)
+     */
+    int startOpMo;
+
+    /**
+     * The percent of the time the facility functions at 100% 
+     * capacity.
+     * (it should be less than one. Double check that.)
+     */
+    double CF;
+
+/* ------------------- */ 
+
+};
+
+/* --------------------
+ * all MODEL classes have these members
+ * --------------------
+ */
+
+extern "C" Model* construct() {
+    return new Repository();
+}
+
+extern "C" void destruct(Model* p) {
+    delete p;
+}
+
+/* ------------------- */ 
+
+#endif
