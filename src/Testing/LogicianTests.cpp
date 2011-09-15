f6babf0033583fb38174f8591d2a0d0cd21abb93
Robert Carlsen <princessandsuperman@gmail.com>
2011-09-15 21:11:07 +0000

    Added file for Logician unit testing and have coded the unit test 'shell' for the Logician.

---
+++ src/Testing/LogicianTests.cpp
@@ -0,0 +1,111 @@
+#include <gtest/gtest.h>
+#include "Logician.h"
+#include "Model.h"
+#include "Material.h"
+#include "Commodity.h"
+
+#include <string>
+#include <map>
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class TestModel : public Model {
+  public:
+    int pre_history_handled_runs_;
+    int tick_handled_runs_;
+    int tock_handled_runs_;
+    int market_resolved_runs_;
+
+    TestModel() {}
+
+    TestModel(string aname, ModelType amodel_type) {
+      handle = aname;
+      name = aname;
+
+      // this line needs to be changed
+      model_type = "type-generic";
+
+      pre_history_handled_runs_ = 0;
+      tick_handled_runs_ = 0;
+      tock_handled_runs_ = 0;
+      market_resolved_runs_ = 0;
+    }
+
+    void handlePreHistory(int time) {
+      pre_history_handled_runs_ += 1;
+    }
+
+    void handleTick(int time) {
+      tick_handled_runs_ += 1;
+    }
+
+    void handleTock(int time) {
+      tock_handled_runs_ += 1;
+    }
+
+    void resolve() {
+      market_resolved_runs_ += 1;
+    }
+
+    void copyFreshModel(Model* model) {}
+};
+
+class LogicianTest : public ::testing::Test {
+  protected:
+    Logician* logician_;
+    std::vector<TestModel> model_list;
+
+    virtual void SetUp(){
+      logician_ = Logician::Instance();
+
+      buildModels();
+    };
+
+  private:
+    void buildModels() {
+      addModel("market_1", MARKET);
+      addModel("market_2", MARKET);
+
+      addModel("facility_1", FACILITY);
+      addModel("facility_2", FACILITY);
+
+      addModel("region_1", REGION);
+      addModel("region_2", REGION);
+    }
+
+    void addModel(string name, ModelType type) {
+      TestModel test_model;
+      test_model = TestModel(name, type);
+      model_list.push_back(test_model);
+    }
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+TEST_F(LogicianTest, AddRetrieveModels) {
+  // finish writing test
+  EXPECT_TRUE(true);
+}
+
+TEST_F(LogicianTest, AddRetrieveCommodity) {
+  // finish writing test
+  EXPECT_TRUE(true);
+}
+
+TEST_F(LogicianTest, AddRetrieveMaterial) {
+  // finish writing test
+  EXPECT_TRUE(true);
+}
+
+TEST_F(LogicianTest, CommodityRegistration) {
+  // finish writing test
+  EXPECT_TRUE(true);
+}
+
+TEST_F(LogicianTest, ExecutionOrder) {
+  // prehistory, timestep, #calls, etc.
+
+  // finish writing test
+  EXPECT_TRUE(true);
+}
+
