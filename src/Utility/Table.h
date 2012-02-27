0e912d40b92e0223a5690e0c48cb3edfdaceb82a
gidden <matthew.gidden@gmail.com>
2012-02-27 12:30:38 -0600

    More documentation written for the Table class.

--- src/Utility/Table.h
+++ src/Utility/Table.h
@@ -34,16 +34,92 @@ typedef std::pair<key, foreign_key_ref> foreign_key;
 //   Indicies
 //typedef std::vector<col_name> index;
 
+
+/*!
+   @brief
+   The Table class is designed as part of the Cyclus Database
+   API. Tables are responsible for holding information about
+   themselves and keep a queue of command instructions for
+   writing or updating rows as needed. Any Module that wishes
+   to maintain specific data not already in the Core Cyclus
+   Database Tables must define their own Table.
+   
+   @section introduction Introduction
+   This class models a relational database table. It specifically
+   provides support for SQL-style table creation, row insertion,
+   and row updating commands; however, it can hold any command 
+   that is in the form of a stringstream.
+   
+   @section definition Defining a Table
+   A Table must be explictly defined. That is, any class using a 
+   table must EXPLICITLY call the Table's tableDefined() 
+   function. It is at this point that the Book Keeper is made 
+   aware of the Table's existence.
+
+   A Table is considered defined if it has at least one 
+   column and a primary key.
+
+   @section data Generic Data Management
+   The Table class utilizes the hold_any class defined in 
+   Boost::Spirit at
+   boost/spirit/home/support/detail/hold_any.hpp. Specifically,
+   the hold_any class uses small optimization and supports the
+   streaming operators.
+
+   Currently, the only check that must be made on a datum's type
+   is whether or not is actually a string. In this case, the
+   Table class applies single quotation marks before and after
+   the datum, as required by SQL languages.
+*/
+
 class Table : IntrusiveBase<Table> {
  private:
-  // private members
+  /**
+   * Stores the table's name, declared during construction.
+   */
   table_name name_;
-  primary_key primary_key_;
-  std::vector<foreign_key> foreign_keys_;
+
+  /**
+   * A storage container for each column's title
+   */
   std::vector<col_name> col_names_;
+
+  /**
+   * A storage container for each column's title and data type
+   * @gidden can we combine these two?
+   */
   std::vector<column> columns_;
-  std::vector<command*> row_commands_;
+
+  /**
+   * Every table must have a defined primary key
+   */
+  primary_key primary_key_;
+
+  /**
+   * A table can have more than one foreign key
+   */
+  std::vector<foreign_key> foreign_keys_;
+
+  /**
+   * A storage container for the table's indicies
+   * @gidden this needs to be supported
+   */
   //std::vector<index> indicies_;
+
+  /**
+   * A storage container for each of the commands a table 
+   * amasses during a simulation. The Book Keeper has the final say
+   * on when these commands are actually executed. The Table alerts
+   * the Book Keeper if the number of commands exceeds the 
+   * threshold defined in Table.cpp.
+   */
+  std::vector<command*> row_commands_;
+
+  /**
+   * A boolean which keeps track of when a Table is defined.
+   * A Table can only be defined once, and it's parameters
+   * can not be changed after.
+   */
   bool defined_;
 
  public:
