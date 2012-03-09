1c84f3a4225563db4556e2662dd20dadbedd303e
Robert Carlsen <rwcarlsen@gmail.com>
2012-03-09 11:29:10 -0600

    progress on MaterialStore tests.  Some tweaking to header comments as well.

--- src/MaterialStore.h
+++ src/MaterialStore.h
@@ -13,6 +13,16 @@ class CycNegQtyException: public CycException {
 };
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*!
+MaterialStore 
+
+Methods that begin with a "set", "make", "add", or "remove" prefix change the
+state/behavior of the store; other methods do not.
+
+Default constructed material store has zero (finite) capacity and splits
+material objects when necessary during removal.
+*/
 class MaterialStore {
 
 public:
@@ -34,19 +44,9 @@ public:
   on constituent resource objects' units).
 
   @throws CycOverCapException thrown if the new capacity is lower than
-  the quantity of material resources that already exist in the store. This
-  includes attempting to set the capacity to a negative value.
+  the quantity of material resources that already exist in the store.
   */
   void setCapacity(double cap);
-
-  /*!
-  space returns the quantity of space remaining in this store.
-
-  It is effectively the difference between the capacity and the sum total
-  of constituent material resource quantities. Never throws.  Returns -1 if
-  the store is unlimited
-  */
-  double space();
 
   /*!
   inventory returns the total quantity of constituent material objects in the
@@ -54,31 +54,69 @@ public:
   */
   double inventory();
 
+  /*!
+  space returns the quantity of space remaining in this store.
+
+  It is effectively the difference between the capacity and the inventory.
+  Never throws.  Returns -1 if the store is unlimited.
+  */
+  double space();
+
+  /// unlimited returns whether this store has unlimited capacity. Never throws.
+  bool unlimited();
+
   /// makeUnlimited sets the store's capacity to be infinite. Never throws.
   void makeUnlimited();
 
   /*!
-  notSplitableOver sets removal behavior to never split resource objects and to
-  return just larger than the specified remove quantity. Never throws.
+  makeLimited sets the store's capacity finite and sets it to the specified value.
+
+  @throws CycOverCapException thrown if the new capacity is lower than
+  the quantity of material resources that already exist in the store.
+  */
+  void makeLimited(double cap);
+
+  /*!
+  splitable returns true if material object splitting can occur during
+  calls to removeQty. Never throws.
+  */
+  bool splitable();
+
+  /*!
+  overQtyOnRemove returns true if material object splitting cannot occur during
+  calls to removeQty and the store will return a quantity greater than the
+  specified removal quantity.
+
+  Returns false if material object splitting cannot occur during calls to
+  removeQty and the store will return a quantity less than the specified
+  removal quantity.
+  
+  Never throws.
+  */
+  bool overQtyOnRemove();
+
+  /*!
+  makeNotSplitableOver sets removal behavior to never split resource objects
+  and to return just larger than the specified remove quantity. Never throws.
 
   Example: If an attempt to remove quantity 10.5 from the store is made and the
   store contains several resurce objects all of quantity 2.5, the remove method
   would return a set of 5 resource objects totaling quantity 12.5.
   */
-  void notSplitableOver();
+  void makeNotSplitableOver();
 
   /*!
-  notSplitableUnder sets removal behavior to never split resource objects and
+  makeNotSplitableUnder sets removal behavior to never split resource objects and
   to return just smaller than the specified remove quantity. Never Throws.
 
   Example: If an attempt to remove quantity 10.5 from the store
   store contains several resurce objects all of quantity 2.5, the remove
   method would return a set of 4 resource objects totaling quantity 10.0.
   */
-  void notSplitableUnder();
+  void makeNotSplitableUnder();
 
   /*!
-  splitable sets removal behavior to split resource objects in order to
+  makeSplitable sets removal behavior to split resource objects in order to
   return the exact specified remove quantity. Never throws.
 
   Example: If an attempt to remove quantity 10.5 from the store store contains
@@ -88,15 +126,15 @@ public:
   quantity 2.0 would be retained in the store while the one of quantity 0.5
   would be the fifth of the returned resources.
   */
-  void splitable();
+  void makeSplitable();
 
   /*!
   removeQty removes the specified quantity of material resources from the
   store.
 
   Materials are split or not split according to the behavior set by the
-  splitable, notSplitableUnder, and notSplitableOver methods.  Materials are
-  retrieved in the order they were added (i.e. oldest first).
+  makeSplitable, makeNotSplitableUnder, and makeNotSplitableOver methods.
+  Materials are retrieved in the order they were added (i.e. oldest first).
 
   @throws CycNegQtyException the specified removal quantity is larger than the
   store's current inventory.
@@ -152,14 +190,19 @@ public:
 
 private:
 
+  /// true if this store has an infinite capacity
   bool unlimited_;
 
-  bool splitable_;
+  /// true if mat retrieval splits objects to match request qty exactly
+  bool makeSplitable_;
 
+  /// true if unsplitable retrievals should return more than request qty
   bool over_;
 
+  /// maximum inventory this store can hold
   double capacity_;
 
+  /// list of constituent material objects forming the store's inventory
   std::vector<mat_rsrc_ptr> mats_;
 
 };
--- src/Testing/MaterialStoreTests.cpp
+++ src/Testing/MaterialStoreTests.cpp
@@ -18,7 +18,10 @@ class MaterialStoreTest : public ::testing::Test {
     mat_rsrc_ptr mat1_;
     mat_rsrc_ptr mat2_;
 
-    MaterialStore store_;
+    MaterialStore store_; // default constructed mat store
+    MaterialStore filled_store_;
+
+    double neg_cap, zero_cap, cap, low_cap;
 
     virtual void SetUp() {
       try {
@@ -33,7 +36,7 @@ class MaterialStoreTest : public ::testing::Test {
 
         v2_m_oxygen = 2;
         v2_m_u235 = 20;
-        v2_m_pu240 = 2000;
+        v2_m_pu240 = 200;
 
         vect1_.setMass(oxygen, v1_m_oxygen);
         vect1_.setMass(u235, v1_m_u235);
@@ -45,6 +48,15 @@ class MaterialStoreTest : public ::testing::Test {
 
         mat1_ = mat_rsrc_ptr(new Material(vect1_));
         mat2_ = mat_rsrc_ptr(new Material(vect2_));
+
+        neg_cap = -1;
+        zero_cap = 0;
+        cap = 334; // should be higher than mat1+mat2 masses
+        low_cap = 332; // should be lower than mat1_mat2 masses
+
+        filled_store_.setCapacity(cap);
+        filled_store_.addOne(mat1_);
+        filled_store_.addOne(mat2_);
       } catch (std::exception err) {
         LOG(LEV_ERROR, "MSTest") << err.what();
         FAIL() << "An exception was thrown in the fixture SetUp.";
@@ -59,77 +71,183 @@ To check:
 
   * after makeUnlimited:
     * can add materials without restraint
-    * capacity and space always return -1 regardless of how many materials are added
 
 */
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MaterialStoreTest, GetCapacity) {
+TEST_F(MaterialStoreTest, SetCapacityExceptions) {
+  EXPECT_THROW(store_.setCapacity(neg_cap), CycOverCapException);
+  EXPECT_THROW(filled_store_.setCapacity(low_cap), CycOverCapException);
+  EXPECT_NO_THROW(store_.setCapacity(zero_cap));
+  EXPECT_NO_THROW(store_.setCapacity(cap));
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, GetCapacityExceptions) {
   ASSERT_NO_THROW(store_.capacity());
+  store_.setCapacity(zero_cap);
+  ASSERT_NO_THROW(store_.capacity());
+  store_.setCapacity(zero_cap);
+  ASSERT_NO_THROW(store_.capacity());
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, InitialCapacity) {
   EXPECT_DOUBLE_EQ(store_.capacity(), 0.0);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MaterialStoreTest, SetCapacity) {
-  double capacity = 1500.0;
-  EXPECT_THROW(store_.setCapacity(-1), CycRangeException);
-  ASSERT_NO_THROW(store_.setCapacity(capacity));
-  EXPECT_DOUBLE_EQ(store_.capacity(), capacity);
+TEST_F(MaterialStoreTest, GetSetCapacity) {
+  store_.setCapacity(zero_cap);
+  EXPECT_DOUBLE_EQ(store_.capacity(), zero_cap);
+
+  store_.setCapacity(cap);
+  EXPECT_DOUBLE_EQ(store_.capacity(), cap);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MaterialStoreTest, GetSpace) {
-  double capacity = 1500.0;
-  store_.setCapacity(capacity);
   ASSERT_NO_THROW(store_.space());
-  EXPECT_DOUBLE_EQ(store_.space(), capacity);
+  EXPECT_DOUBLE_EQ(store_.space(), 0.0);
+
+  store_.setCapacity(zero_cap);
+  ASSERT_NO_THROW(store_.space());
+  EXPECT_DOUBLE_EQ(store_.space(), zero_cap);
+
+  store_.setCapacity(cap);
+  ASSERT_NO_THROW(store_.space());
+  EXPECT_DOUBLE_EQ(store_.space(), cap);
+
+  double space = cap - (mat1_.quantity() + mat2_.quantity());
+  ASSERT_NO_THROW(filled_store_.space());
+  EXPECT_DOUBLE_EQ(filled_store_.space(), space);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, GetInventory) {
+  ASSERT_NO_THROW(store.inventory());
+  EXPECT_DOUBLE_EQ(store_.inventory(), 0.0);
+
+  ASSERT_NO_THROW(filled_store_.inventory());
+  double inventory = mat1_.quantity() + mat2_.quantity();
+  EXPECT_DOUBLE_EQ(filled_store_.inventory(), inventory);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, InitialUnlimited) {
+  ASSERT_NO_THROW(store_.unlimited());
+  EXPECT_EQ(store_.unlimited(), false);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MaterialStoreTest, MakeUnlimited) {
-  double capacity = 1500.0;
-  store_.setCapacity(capacity);
   ASSERT_NO_THROW(store_.makeUnlimited());
-  EXPECT_EQ(store_.capacity(), -1);
-  EXPECT_EQ(store_.space(), -1);
+  store_.makeUnlimited();
+  EXPECT_EQ(store_.unlimited(), true);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MaterialStoreTest, NotSplitableOver) {
+TEST_F(MaterialStoreTest, UnlimitedCap) {
+  store_.makeUnlimited();
+  EXPECT_NO_THROW(store_.setCapacity(cap));
+  EXPECT_DOUBLE_EQ(store_.capacity(), -1.0);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MaterialStoreTest, NotSplitableUnder) {
+TEST_F(MaterialStoreTest, UnlimitedSpace) {
+  store_.makeUnlimited();
+  EXPECT_DOUBLE_EQ(store_.space(), -1.0);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MaterialStoreTest, Splitable) {
+TEST_F(MaterialStoreTest, MakeLimitedExceptions) {
+  store_.makeUnlimited();
+  filled_store_.makeUnlimited();
+  ASSERT_THROW(store_.makeLimited(neg_cap), CycOverCapException);
+  ASSERT_THROW(filled_store_.makeLimited(low_cap), CycOverCapException);
+
+  // be sure that unlimited status doesn't change if exceptions are thrown
+  EXPECT_EQ(store_.unlimited(), true);
+  EXPECT_EQ(store_.limited(), true);
+
+  ASSERT_NO_THROW(store_.makeLimited(zero_cap));
+  ASSERT_NO_THROW(store_.makeLimited(cap));
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, MakeLimited) {
+  store_.makeUnlimited();
+  filled_store_.makeUnlimited();
+
+  ASSERT_EQ(store_.unlimited(), true);
+  ASSERT_EQ(filled_store_.unlimited(), true);
+
+  store_.makeLimited(zero_cap);
+  filled_store_.makeLimited(cap);
+
+  ASSERT_EQ(store_.unlimited(), false);
+  ASSERT_EQ(filled_store_.unlimited(), false);
+
+  EXPECT_DOUBLE_EQ(store_.capacity(), zero_cap);
+  EXPECT_DOUBLE_EQ(filled_store_.capacity(), cap);
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MaterialStoreTest, RemoveQty) {
-  // returns a std::vector
+TEST_F(MaterialStoreTest, DefaultRemoveProperties) {
+  ASSERT_NO_THROW(store_.splitable());
+  EXPECT_EQ(store_.splitable(), true);
+
+  ASSERT_NO_THROW(store_.overQtyOnremove());
+  EXPECT_EQ(store_.overQtyOnremove(), false);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, MakeSplitableAndNot) {
+  ASSERT_NO_THROW(store_.makeSplitable());
+  store_.makeSplitable();
+  EXPECT_EQ(store_.splitable(), true);
+  EXPECT_EQ(store_.overQtyOnRemove(), false);
+
+  ASSERT_NO_THROW(store_.makeNotSplitableUnder());
+  store_.makeNotSplitableUnder();
+  EXPECT_EQ(store_.splitable(), false);
+  EXPECT_EQ(store_.overQtyOnRemove(), false);
+
+  ASSERT_NO_THROW(store_.makeNotSplitableOver());
+  store_.makeNotSplitableOver();
+  EXPECT_EQ(store_.splitable(), false);
+  EXPECT_EQ(store_.overQtyOnRemove(), true);
+
+  ASSERT_NO_THROW(store_.makeSplitable());
+  store_.makeSplitable();
+  EXPECT_EQ(store_.splitable(), true);
+  EXPECT_EQ(store_.overQtyOnRemove(), false);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, RemoveQtySplitable) {
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, RemoveQtyNotSplitableOver) {
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(MaterialStoreTest, RemoveQtyNotSplitableUnder) {
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MaterialStoreTest, RemoveOne) {
-  // returns mat_rsrc_ptr
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MaterialStoreTest, RemoveNum) {
-  // returns a std::vector
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MaterialStoreTest, AddOne) {
-  // takes a mat_rsrc_ptr
-  // throw if added mat breaks capacity
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MaterialStoreTest, AddAll) {
-  // takes a std::vector
-  // throw if added mats break capacity
 }
 
