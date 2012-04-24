b1bc92a0970743f678f40503b8a16998fe51011f
Robert Carlsen <rwcarlsen@gmail.com>
2012-04-24 10:25:37 -0500

    housekeeping. Added helpful comments to addChild and setParent methods.

--- src/Testing/InstModelTests.cpp
+++ src/Testing/InstModelTests.cpp
@@ -4,16 +4,11 @@
 #include "InstModelTests.h"
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//- - - - - - - - - - - Param per-Inst Tests  - - - - - - - - - - - - - - - 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 TEST_P(InstModelTests, CopyInst) {
   InstModel* new_inst = inst_model_;
   EXPECT_NO_THROW(new_inst->copy(inst_model_)); 
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(InstModelTests, AddPrototype) {
-  EXPECT_EQ( inst_model_->isAvailablePrototype(test_facility_), false );
-  EXPECT_NO_THROW( inst_model_->wrapAddPrototype(test_facility_) );
-  EXPECT_EQ( inst_model_->isAvailablePrototype(test_facility_), true );
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -27,3 +22,11 @@ TEST_P(InstModelTests, Tock) {
   int time = 1;
   EXPECT_NO_THROW(inst_model_->handleTock(time));
 }
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(InstModelTests, AddPrototype) {
+  EXPECT_FALSE( inst_model_->isAvailablePrototype(test_facility_));
+  EXPECT_NO_THROW( inst_model_->wrapAddPrototype(test_facility_) );
+  EXPECT_TRUE( inst_model_->isAvailablePrototype(test_facility_));
+}
+
