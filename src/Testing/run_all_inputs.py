d7d26621e465bc2321b28f169c03d252e394d45f
Katy Huff <katyhuff@gmail.com>
2012-05-22 15:34:02 -0500

    added more modularity and test structure as well as docstrings

--- src/Testing/run_all_inputs.py
+++ src/Testing/run_all_inputs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import subprocess
 import os
 import re
  
@@ -10,7 +11,7 @@ def main():
     files = get_files(path)
     for file in files :
         file_to_test = TestFile(file) 
-        file_to_test.test_no_errors()
+        file_to_test.run_tests()
 
 def check_inputs():
     if len(sys.argv) != 2: 
@@ -26,19 +27,47 @@ def get_files(path):
         if '.git' in dirs:
             dirs.remove('.git')
         for name in files: 
-            if re.search("\.xml",name):
+            if re.search("null\.xml",name):
                 files.append(os.path.join(root, name))
             else :
                 files.remove(name)
     return files
 
 class TestFile():
-    '''An object representing the inputxml file to test'''
+    """An object representing the inputxml file to test"""
     def __init__(self, file_path):
         self.name = file_path # strip off front bit
   
-    def test_no_errors(self):
-        os.system("./cyclus -v9 " + self.name)
+    def run_tests(self):
+        """Runs all of the input file tests"""
+        if self.test_no_crash() : 
+            output = self.get_output()
+            self.test_no_errors(output)
+            self.test_expected(output)
+
+    def get_output(self):
+        """Returns the output from running the FileTest"""
+        flags = " -v9 " + self.name
+        try :
+            subprocess.check_call(["./cyclus", flags])
+        except subprocess.CalledProcessError, e:
+            print "native CalledProcessError.output = " + e.output
+        output = subprocess.check_output("./cyclus"+flags, stderr=subprocess.STDOUT, shell=True)
+        return output
         
+    def test_no_errors(self, output):
+        """returns true if there were no errors or segfaults running this TestFile"""
+        to_ret = True
+        if re.search("ERROR",output) or re.search("Segmentation fault",output):
+            to_ret = False
+            print "Test " + self.name + " resulted in errors: "
+            print output
+       return to_ret 
+
+   def test_expected(self, output):
+       """This function is currently a placeholder. It is intended to print the 
+       difference between the current output and the output gathered the last 
+       time the test was run"""
+       return True
 
 if __name__ == '__main__' : main()
