f39cfd42d38b9d8f9caf02c5dd95c08c00a52fd3
Katy Huff <katyhuff@gmail.com>
2012-05-22 13:54:46 -0500

    made skeletal run_all_inputs file, added workflow, path, class for TestFile.

--- src/Testing/run_all_inputs.py
+++ src/Testing/run_all_inputs.py
@@ -1,15 +1,43 @@
+#!/usr/bin/python
+
+import sys
 import os
 import re
+ 
+def main():
+    check_inputs(sys.argv)
+    path = get_path()
+    files = get_files(path)
+    for file in files :
+        file_to_test = TestFile(file) 
+        file_to_test.test_no_errors()
 
-
-def get_path() :
-    path = argv[1]
+def check_inputs():
+    if len(sys.argv) != 2: 
+        print 'Usage: run_all_inputs.py <path_to_Input_folder>' 
+        sys.exit(1) 
+ 
+def get_path():
+    path = sys.argv[1]
     return path
 
-def get_xml_files() :
+def get_files(path):
     for root, dirs, files in os.walk(path, followlinks=True):
         if '.git' in dirs:
             dirs.remove('.git')
         for name in files: 
-            if re.search("\.xml",name):
-                os.system("./cyclus " + os.path.join(root, name))
+            if not re.search("\.xml",name):
+                files.remove(name)
+    return files
+
+class TestFile(self, file_path):
+    '''An object representing the inputxml file to test'''
+  def __init__(self, file_path):
+    self.name = file_path # strip off front bit
+    self. = 
+
+  def test(self):
+    os.system("./cyclus " + os.path.join(root, name))
+    print("The Classy classmember speaks with clarity.")
+
+if __name__ == '__main__' : main()
