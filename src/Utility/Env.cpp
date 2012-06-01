daf9fd6f64e606184029afb6a8842f292c672207
Katy Huff <katyhuff@gmail.com>
2012-06-01 14:25:03 -0500

    Cleaned up oddly formatted log message.

--- src/Utility/Env.cpp
+++ src/Utility/Env.cpp
@@ -31,15 +31,14 @@ string Env::pathBase(std::string path) {
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 string Env::getCyclusPath() {
   // return the join of cwd_ and rel path to cyclus
-  CLOG(LEV_DEBUG4) << "Cyclus absolute path retrieved: " 
-                  << cwd_ / path_from_cwd_to_cyclus_;
-
   boost::filesystem::path path;
   if(path_from_cwd_to_cyclus_.has_root_path()) {
     path = path_from_cwd_to_cyclus_.normalize();
   } else {
     path = (cwd_ / path_from_cwd_to_cyclus_).normalize();
   }
+  CLOG(LEV_DEBUG4) << "Cyclus absolute path retrieved: " 
+                  <<  path.string();
   return path.string();
 }
 
