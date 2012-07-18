84289f75edfc23c0f772bd6fdb4a962d8e78fe85
gidden <matthew.gidden@gmail.com>
2012-07-18 13:17:12 -0500

    added printing capability to symbolic functions and querying of demand functions to supply demand manager. also cleaned up line lengths

--- src/Core/Utility/SupplyDemandManager.cpp
+++ src/Core/Utility/SupplyDemandManager.cpp
@@ -2,45 +2,56 @@
 
 using namespace std;
 
-// --------------------------------------------------------------------------------------
+// -------------------------------------------------------------------
 SupplyDemandManager::SupplyDemandManager() {
   commodities_ = map<Commodity,CommodityInformation,CommodityCompare>();
 }
 
-// --------------------------------------------------------------------------------------
-void SupplyDemandManager::registerCommodity(const Commodity& commodity, const FunctionPtr fp, 
-                                          const std::vector<Producer>& producers) {
+// -------------------------------------------------------------------
+void 
+SupplyDemandManager::registerCommodity(const Commodity& commodity, 
+                                       const FunctionPtr fp, 
+                                       const std::vector<Producer>& producers) {
   commodities_.insert(pair<Commodity,CommodityInformation>
                    (commodity,CommodityInformation(commodity,fp,producers)));  
 }
 
-// --------------------------------------------------------------------------------------
-void SupplyDemandManager::registerProducer(const Commodity& commodity, 
+// -------------------------------------------------------------------
+void SupplyDemandManager::registerProducer(const Commodity& commodity,
                                            const Producer& producer) {
   commodities_.find(commodity)->second.registerProducer(producer);
 }
 
-// --------------------------------------------------------------------------------------
-double SupplyDemandManager::demand(const Commodity& commodity, int time) {
+// -------------------------------------------------------------------
+double SupplyDemandManager::demand(const Commodity& commodity, 
+                                   int time) {
   return commodities_.find(commodity)->second.demand(time);
 }
 
-// --------------------------------------------------------------------------------------
+// -------------------------------------------------------------------
+FunctionPtr 
+SupplyDemandManager::demandFunction(const Commodity& commodity) {
+  return commodities_.find(commodity)->second.demandFunction();
+}
+
+// -------------------------------------------------------------------
 double SupplyDemandManager::supply(const Commodity& commodity) {
   return commodities_.find(commodity)->second.supply();
 }
 
-// --------------------------------------------------------------------------------------
-void SupplyDemandManager::increaseSupply(const Commodity& commodity, double amt) { 
+// -------------------------------------------------------------------
+void SupplyDemandManager::increaseSupply(const Commodity& commodity, 
+                                         double amt) { 
   commodities_.find(commodity)->second.increaseSupply(amt); 
 }
 
-// --------------------------------------------------------------------------------------
+// -------------------------------------------------------------------
 int SupplyDemandManager::nProducers(const Commodity& commodity) {
   return commodities_.find(commodity)->second.nProducers(); 
 }
 
-// --------------------------------------------------------------------------------------
-Producer* SupplyDemandManager::producer(const Commodity& commodity, int index) {
+// -------------------------------------------------------------------
+Producer* SupplyDemandManager::producer(const Commodity& commodity, 
+                                        int index) {
   return commodities_.find(commodity)->second.producer(index); 
 }
