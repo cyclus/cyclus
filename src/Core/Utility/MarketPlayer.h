f58c98daa1953d683a555716b67b2545d3e6a8f3
Matthew Gidden <matthew.gidden@gmail.com>
2012-08-06 10:47:52 -0500

    created MarketPlayer class

--- src/Core/Utility/MarketPlayer.h
+++ src/Core/Utility/MarketPlayer.h
@@ -3,8 +3,16 @@
 #define MARKETPLAYER_H
 
 #include "SupplyDemand.h"
+#include "MarketPlayerManager.h"
 
 #include <map>
+#include <vector>
+
+/// an iterator for the manager map
+typedef map<Commodity,
+            vector<MarketPlayerManager*> >::iterator ManagerIterator;
+/// an iterator for the production map
+typedef map<Commodity,double>::iterator ProductionIterator;
 
 /**
    A MarketPlayer is an agent mixin that allows for querying
@@ -31,13 +39,14 @@ class MarketPlayer {
 
   /**
      set the production capacity for a commodity
-     @param the produced commodity
+     @param value the value of the production capacity
+     @param commod the produced commodity
    */
-  void setProductionCapacity(Commodity& commod);
+  void setProductionCapacity(double& value,Commodity& commod);
 
   /**
      return the production capacity for a commodity
-     @param the produced commodity
+     @param commod the produced commodity
    */
   double productionCapacity(Commodity& commod);
 
@@ -51,20 +60,45 @@ class MarketPlayer {
   /**
      inform the manager that you are entering the market for a 
      commodity
-     @param the commodity of the market to enter
+     @param commod the commodity of the market to enter
    */
   void enterMarket(Commodity& commod);
 
   /**
      inform the manager that you are leaving the market for a 
      commodity
-     @param the commodity of the market to leave
+     @param commod the commodity of the market to leave
    */
   void leaveMarket(Commodity& commod);
 
  private:
-  std::map<Commodity, std::vector<MarketPlayerManager*> > managers_;
+  /// the production capacity of each commodity
   std::map<Commodity, double> production_capacity_;
+
+  /// the managers of each commodity
+  std::map<Commodity, std::vector<MarketPlayerManager*> > managers_;
+  
+  /**
+     throws an error saying the commodity is not registered
+     @param commod the commodity that is ot registered
+   */
+  void throwRegistrationException(Commodity& commod);
+
+  /**
+     Verifies that commod is in production_capacity_. If not, 
+     throwRegistrationException() is called.
+     @param commod the commodity to verify
+     @return an iterator to the value/key pair in production_capacity_
+   */
+  ProductionIterator checkCommodityProduction(Commodity& commod);
+
+  /**
+     Verifies that commod is in managers_. If not, 
+     throwRegistrationException() is called.
+     @param commod the commodity to verify
+     @return an iterator to the value/key pair in managers_
+   */
+  ManagerIterator checkCommodityManagement(Commodity& commod);
 };
 
 #endif
