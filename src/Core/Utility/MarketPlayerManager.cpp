8f9c4d3cab678d59b52223567e901f3c364d625b
Matthew Gidden <matthew.gidden@gmail.com>
2012-08-06 11:24:05 -0500

    finished market player manager class. need to write tests for both.

--- src/Core/Utility/MarketPlayerManager.cpp
+++ src/Core/Utility/MarketPlayerManager.cpp
@@ -1,10 +1,29 @@
 #include "MarketPlayerManager.h"
 
-// -------------------------------------------------------------------
-MarketPlayerManager::MarketPlayerManager() {}
+using namespace std;
 
 // -------------------------------------------------------------------
-void MarketPlayerManager::playerEnteringMarket(MarketPlayer* m) {}
+MarketPlayerManager::MarketPlayerManager(Commodity& commod) : 
+  commod_(commod) {
+  players_ = set<MarketPlayer*>();
+}
 
 // -------------------------------------------------------------------
-void MarketPlayerManager::playerLeavingMarket(MarketPlayer* m) {}
+void MarketPlayerManager::playerEnteringMarket(MarketPlayer* m) {
+  players_.insert(m);
+}
+
+// -------------------------------------------------------------------
+void MarketPlayerManager::playerLeavingMarket(MarketPlayer* m) {
+    players_.erase(m);
+}
+
+// -------------------------------------------------------------------
+double MarketPlayerManager::playerProductionCapacity() {
+  double value = 0;
+  set<MarketPlayer*>::iterator it;
+  for (it = players_.begin(); it != players_.end(); it++) {
+    value += (*it)->productionCapacity(commod_);
+  }
+  return value;
+}
--- src/Core/Utility/MarketPlayerManager.h
+++ src/Core/Utility/MarketPlayerManager.h
@@ -2,14 +2,46 @@
 #ifndef MARKETPLAYERMANAGER_H
 #define MARKETPLAYERMANAGER_H
 
+#include "SupplyDemand.h"
+
+#include <set>
+
 class MarketPlayerManager;
 #include "MarketPlayer.h"
 
 class MarketPlayerManager {
  public:
-  MarketPlayerManager();
+  /**
+     constructor
+     @param commod the commodity to associate with this manager
+   */
+  MarketPlayerManager(Commodity& commod);
+
+  /**
+     add a player from the list of players
+     @param m the player to add
+   */
   void playerEnteringMarket(MarketPlayer* m);
+
+  /**
+     remove a player from the list of players
+     @param m the player to remove
+   */
   void playerLeavingMarket(MarketPlayer* m);
+
+  /**
+     returns the total production capacity of all players in the
+     market
+     @return the total production capacity of the market
+   */
+  double playerProductionCapacity();
+  
+ private:
+  /// the commodity associated with this manager
+  Commodity commod_;
+
+  /// the players associated with this manager
+  std::set<MarketPlayer*> players_;
 };
 
 #endif
