ca510d1c71142284aeb513de1486a570b6b75788
Katy Huff <katyhuff@gmail.com>
2012-08-22 16:58:45 -0500

    a good start on #345

--- src/Core/Models/Facility/TestFacility/TestFacility.h
+++ src/Core/Models/Facility/TestFacility/TestFacility.h
@@ -9,13 +9,11 @@
 class TestFacility: public FacilityModel {
  public:
   
-  void receiveMessage(msg_ptr msg) {
-    msg->setDir(DOWN_MSG);
-  }
+  void receiveMessage(msg_ptr msg) ;
 
-  void copyFreshModel(Model* src) { copy(dynamic_cast<TestFacility*>(src)); }
+  void copyFreshModel(Model* src) ;
 
-  void receiveMaterial(Transaction trans, std::vector<mat_rsrc_ptr> manifest) { }
+  void receiveMaterial(Transaction trans, std::vector<mat_rsrc_ptr> manifest) ;
 
 };
 
