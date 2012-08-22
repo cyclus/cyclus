be218ef5446ad1f2512dcdf9031869881dd7bd8e
Katy Huff <katyhuff@gmail.com>
2012-08-22 16:58:45 -0500

    a good start on #345

---
+++ src/Core/Models/Facility/TestFacility/TestFacility.cpp
@@ -0,0 +1,18 @@
+#include "FacilityModel.h"
+#include "Material.h"
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void TestFacility::receiveMessage(msg_ptr msg) {
+  msg->setDir(DOWN_MSG);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void TestFacility::copyFreshModel(Model* src) { 
+  copy(dynamic_cast<TestFacility*>(src)); 
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void TestFacility::receiveMaterial(Transaction trans, 
+    std::vector<mat_rsrc_ptr> manifest) { 
+}
+
