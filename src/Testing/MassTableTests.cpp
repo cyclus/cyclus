aba2f22d09c735e5cf23243262014c5eebc714cc
Katy Huff <katyhuff@gmail.com>
2012-08-25 14:59:35 -0500

    only have time to test this at the moment.

--- src/Testing/MassTableTests.cpp
+++ src/Testing/MassTableTests.cpp
@@ -3,10 +3,6 @@
 #include "MassTableTests.h"
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-TEST_F(MassTableTest, Constructors){
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 TEST_F(MassTableTest, gramsPerMol){
   EXPECT_NEAR(235, MT->gramsPerMol(u235_),0.5);
   EXPECT_NEAR(241, MT->gramsPerMol(am241_),0.5);
@@ -14,8 +10,3 @@ TEST_F(MassTableTest, gramsPerMol){
   EXPECT_NEAR(208, MT->gramsPerMol(pb208_), 0.5);
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
