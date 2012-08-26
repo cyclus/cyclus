37f5332482867f6116912b4271482dc8cdc875d7
Paul Wilson <wilsonp@engr.wisc.edu>
2012-08-26 12:39:39 -0500

    update RegionModel to use QueryEngine

--- src/Core/Models/RegionModel.cpp
+++ src/Core/Models/RegionModel.cpp
@@ -9,10 +9,10 @@
 #include "Model.h"
 #include "InstModel.h"
 #include "CycException.h"
-#include "InputXML.h"
 #include "Timer.h"
 #include "Logger.h"
 #include "Prototype.h"
+#include "QueryEngine.h"
 
 using namespace std;
 
@@ -22,21 +22,20 @@ RegionModel::RegionModel() {
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-void RegionModel::init(xmlNodePtr cur) { 
-  Model::init(cur); // name_ and model_impl_
-  RegionModel::initAllowedFacilities(cur); // allowedFacilities_
+void RegionModel::init(QueryEngine* qe) { 
+  Model::init(qe); // name_ and model_impl_
+  RegionModel::initAllowedFacilities(qe); // allowedFacilities_
   RegionModel::addRegionAsRootNode(); // parent_ and tick listener, model 'born'
-  RegionModel::addChildrenToTree(cur); // children->setParent, requires init()
+  RegionModel::addChildrenToTree(qe); // children->setParent, requires init()
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void RegionModel::initAllowedFacilities(xmlNodePtr cur) {   
-  xmlNodeSetPtr fac_nodes = 
-    XMLinput->get_xpath_elements(cur,"allowedfacility");
+void RegionModel::initAllowedFacilities(QueryEngine* qe) {   
+  int num_allowed_fac = qe->numElementsMatchingQuery("allowedfacility");
   string fac_name;
   Model* new_fac;
-  for (int i=0;i<fac_nodes->nodeNr;i++){
-    fac_name = (const char*)fac_nodes->nodeTab[i]->children->content;
+  for (int i=0;i<num_allowed_fac;i++) {
+    fac_name = qe->getElementContent("allowedfacility",i);
     new_fac = dynamic_cast<Model*>(Prototype::getRegisteredPrototype(fac_name));
     allowedFacilities_.insert(new_fac);
   }
@@ -49,12 +48,13 @@ void RegionModel::addRegionAsRootNode() {
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-void RegionModel::addChildrenToTree(xmlNodePtr cur) {   
+void RegionModel::addChildrenToTree(QueryEngine* qe) {   
   string inst_name;
   Model* inst;
-  xmlNodeSetPtr inst_nodes = XMLinput->get_xpath_elements(cur,"institution");
-  for (int i=0;i<inst_nodes->nodeNr;i++){
-    inst_name = (const char*)XMLinput->get_xpath_content(inst_nodes->nodeTab[i],"name");
+  int num_inst = qe->numElementsMatchingQuery("institution");
+  for (int i=0;i<num_inst;i++){
+    QueryEngine* iqe = qe->queryElement("institution",i);
+    inst_name = iqe->getElementContent("name");
     inst = Model::getModelByName(inst_name);
     inst->enterSimulation(this);
   }
