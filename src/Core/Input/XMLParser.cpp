b4dd46e15244574e07d417f0b12b2c852e7fb0ab
gidden <matthew.gidden@gmail.com>
2012-08-28 23:25:49 -0500

    added an xml parser

---
+++ src/Core/Input/XMLParser.cpp
@@ -0,0 +1,56 @@
+// XMLParser.cpp
+
+#include "XMLParser.h"
+
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+using namespace xmlpp;
+using namespace boost;
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+XMLParser::XMLParser(std::stringstream& xml_input_snippet, 
+                     std::stringstream& xml_schema_snippet) {
+  initParser(xml_input_snippet);
+  if (xml_schema_snippet.rdbuf()->in_avail() > 0) {
+    validateFileAgaisntSchema(xml_schema_snippet);
+  }
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+XMLParser::XMLParser(std::stringstream& xml_input_snippet) {
+  initParser(xml_input_snippet);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+XMLParser::~XMLParser() {
+  parser_.reset();
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void XMLParser::validateFileAgaisntSchema(std::stringstream&
+                                          xml_schema_snippet) {
+  XMLParser schema_parser(xml_schema_snippet);
+  SchemaValidator validator(*schema_parser.document());
+  validator.validate(this->document());
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void XMLParser::initParser(std::stringstream& xml_input_snippet) {
+  parser_ = shared_ptr<DomParser>(new DomParser());
+  try {
+    parser_->parse_memory(xml_input_snippet.str());
+    if(!parser_) {
+      throw CycLoadXMLException("Could not parse xml file.");
+    }
+  } catch(const std::exception& ex) {
+    throw CycLoadXMLException("Error loading xml file: " + 
+                              string(ex.what()));
+  }
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+xmlpp::Document* XMLParser::document() {
+  return parser_->get_document();
+}
