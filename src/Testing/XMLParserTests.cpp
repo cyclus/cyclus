fb79a3db9c8196f23b89d6fa4b863282e07cad62
Matthew Gidden <matthew.gidden@gmail.com>
2012-08-29 15:36:39 -0500

    framework provided for testing the xml parser

---
+++ src/Testing/XMLParserTests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include "XMLParserTests.h"
+
+#include "CycException.h"
+
+using namespace std;
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void XMLParserTests::SetUp() {}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void XMLParserTests::TearDown() {}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(XMLParserTests,EnterLeaveTests) {} 
---
+++ src/Testing/XMLParserTests.h
@@ -0,0 +1,13 @@
+// XMLParserTests.h
+#include <gtest/gtest.h>
+
+#include "XMLParser.h"
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+class XMLParserTests : public ::testing::Test {
+ protected:
+
+ public:
+  virtual void SetUp();
+  virtual void TearDown();
+};
