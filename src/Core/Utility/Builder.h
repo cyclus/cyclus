5cdd0a5f1550a249959d92aefb47b739ff7147e1
gidden <matthew.gidden@gmail.com>
2012-09-27 16:07:58 -0500

    added a builder class which simply provides an interface to entities that can be built

---
+++ src/Core/Utility/Builder.h
@@ -0,0 +1,61 @@
+#ifndef BUILDER_H
+#define BUILDER_H
+
+#include <set>
+
+#include "CommodityProducer.h"
+
+namespace ActionBuilding
+{
+  /**
+     a mixin to provide information about commodity producers that can
+     be built
+   */
+  class Builder
+  {
+  public:
+    /// constructor
+    Builder();
+
+    /// virtual destructor for inheritence
+    virtual ~Builder();
+
+    // protected: @MJGFlag - should be protected. revise when tests can
+    // be found by classes in the Utility folder
+    /**
+       register a commodity producer with the manager
+       @param producer the producer
+     */
+    void registerProducer(SupplyDemand::CommodityProducer* producer);
+
+    /**
+       unregister a commodity producer with the manager
+       @param producer the producer
+     */
+    void unRegisterProducer(SupplyDemand::CommodityProducer* producer);
+
+    /**
+       @return the number of buildings managed by this builder
+     */
+    double nBuildingPrototypes();
+    
+    /**
+       @return the beginning iterator of the set of producers
+     */
+    std::set<CommodityProducer*>::iterator begin();
+
+    /**
+       @return the beginning iterator of the set of producers
+     */
+    std::set<CommodityProducer*>::iterator end();
+
+  private:
+    /// the set of managed producers
+    std::set<CommodityProducer*> producers_;
+
+    //#include "CommodityProducerManagerTests.h"
+    //friend class CommodityProducerManagerTests; 
+    // @MJGFlag - removed for the same reason as above
+  };
+}
+#endif
