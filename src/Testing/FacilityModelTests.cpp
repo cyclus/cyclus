68efcde079974f4b0f7631939d23b607ff3b417e
gidden <matthew.gidden@gmail.com>
2013-02-06 12:35:36 -0600

    changed commenting out the tests to disabling them

--- src/Testing/FacilityModelTests.cpp
+++ src/Testing/FacilityModelTests.cpp
@@ -3,15 +3,15 @@
 
 #include "FacilityModelTests.h"
 
-// //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-// TEST_P(FacilityModelTests, Tick) {
-//   int time = 1;
-//   facility_model_->handleTick(time);
-//     //EXPECT_NO_THROW();
-// }
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+TEST_P(FacilityModelTests, DISABLED_Tick) {
+  int time = 1;
+  facility_model_->handleTick(time);
+    //EXPECT_NO_THROW();
+}
 
-// //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-// TEST_P(FacilityModelTests, Tock) {
-//   int time = 1;
-//   EXPECT_NO_THROW(facility_model_->handleTock(time));
-// }
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+TEST_P(FacilityModelTests, DISABLED_Tock) {
+  int time = 1;
+  EXPECT_NO_THROW(facility_model_->handleTock(time));
+}
