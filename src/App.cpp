2ed4a90ddd904092b41c1120df20bd08cb368176
Katy Huff <katyhuff@gmail.com>
2013-02-18 11:17:36 -0600

    extraneous try

--- src/App.cpp
+++ src/App.cpp
@@ -100,19 +100,18 @@ int main(int argc, char* argv[]) {
   string output_path;
   string fname = "cyclus.sqlite";
   // Create the output file
-  try {
-    if (vm.count("output-path")){
-      output_path = vm["output-path"].as<string>();
-      string ext = ".sqlite";
-      string::size_type sql_ext_pos = output_path.rfind(ext);
-      if (sql_ext_pos != string::npos) {
-        string::size_type last_slash_pos = output_path.rfind("/");
-        int fname_len = output_path.length() - last_slash_pos - 1;
-        fname = output_path.substr( last_slash_pos+1, fname_len);
-        output_path = output_path.substr(0,output_path.length()-fname_len); 
-      }
-    } else { 
-      output_path = Env::checkEnv("PWD");
+  if (vm.count("output-path")){
+    output_path = vm["output-path"].as<string>();
+    string ext = ".sqlite";
+    string::size_type sql_ext_pos = output_path.rfind(ext);
+    if (sql_ext_pos != string::npos) {
+      string::size_type last_slash_pos = output_path.rfind("/");
+      int fname_len = output_path.length() - last_slash_pos - 1;
+      fname = output_path.substr( last_slash_pos+1, fname_len);
+      output_path = output_path.substr(0,output_path.length()-fname_len); 
+    }
+  } else { 
+    output_path = Env::checkEnv("PWD");
     }
   try {
     BI->createDB(output_path, fname);
