8d1df12983c49a251901e6298eeb5b94f22806a6
Robert Carlsen <rwcarlsen@gmail.com>
2013-04-23 14:18:18 -0500

    @rwcarlsen updated work in the previous commit and adds tests for the envrionment and dynamic module classes

--- src/Core/Models/DynamicModule.h
+++ src/Core/Models/DynamicModule.h
@@ -10,106 +10,88 @@ typedef Model* create_t();
 typedef void destroy_t(Model*);
 
 class DynamicModule {
- public:
-  /// @return the global library suffix
-  static const std::string suffix();
+  public:
+    /// @return the global library suffix
+    static const std::string suffix();
 
-  /**
-     constructor
-     sets:
-     * module_name_
-     * abs_path_
-     * constructor_name_
-     * destructor_name_
-     @param type the module type
-     @param name the name of the module
-   */
-  DynamicModule(std::string type, std::string name);
-  
-  /**
-     destructor
-     calls:
-     * closeLibrary()
-   */
-  virtual ~DynamicModule();
+    /**
+       Creates a new dynamically loadable module.
+       @param type the module type
+       @param name the name of the module
+     */
+    DynamicModule(std::string type, std::string name);
 
-  /// @return the module name
-  std::string name();
+    /// Cleanup and calls closeLibrary
+    virtual ~DynamicModule();
 
-  /**
-     construct an instance of this module
-     @return a fresh instance
-   */
-  Model* constructInstance();
+    /// @return the module name
+    std::string name();
 
-  /**
-     call this destructor on an instance of a Model
-     @param model the model to destruct
-  */
-  void destructInstance(Model* model);
+    /**
+       construct an instance of this module
+       @return a fresh instance
+     */
+    Model* constructInstance();
 
-  /**
-     initializes the module by calling
-     * setPath()
-     * openLibrary()
-     * setConstructor()
-     * setDestructor()
-   */
-  void initialize();
+    /**
+       call this destructor on an instance of a Model
+       @param model the model to destruct
+    */
+    void destructInstance(Model* model);
 
-  /**
-     sets the absolute path to the dynamic library based on parameters set
-     during construction
-   */
-  void setPath();
+    /// Locates and initializes a dynamic module for use in the simulation
+    void initialize();
 
-  /**
-     closes the library
-   */
-  void closeLibrary();
+    /// closes the loaded module dynamic lib
+    void closeLibrary();
 
-  /// @return the path to the dynamic library
-  std::string path();
-  
- private:
-  /// the path to the library
-  std::string abs_path_;
-  
-  /// the name of the module
-  std::string module_name_;
+    /**
+      If this path for this module has not been discovered yet, path searches for it.
+      @exception CycIOException the library path was not found
+      @return the filepath of the dynamic library.
+    */
+    std::string path();
 
-  /// the type of module
-  std::string type_;
+  private:
+    /// the path to the library
+    std::string abs_path_;
 
-  /// the name of all module constructor functions
-  std::string constructor_name_;
+    /// the name of the module
+    std::string module_name_;
 
-  /// the name of all module destructor functions
-  std::string destructor_name_;
+    /// the type of module
+    std::string type_;
 
-  /// the library to open and close
-  void * module_library_;
+    /// the name of all module constructor functions
+    std::string constructor_name_;
 
-  /// a functor for the constructor
-  create_t * constructor_;
-  
-  /// a functor for the destructor
-  destroy_t * destructor_;
+    /// the name of all module destructor functions
+    std::string destructor_name_;
 
-  /**
-     opens the library
-   */
-  void openLibrary();
+    /// the library to open and close
+    void* module_library_;
 
-  /**
-     sets the constructor member
-   */
-  void setConstructor();
+    /// a functor for the constructor
+    create_t* constructor_;
 
-  /**
-     sets the destructor member
-   */
-  void setDestructor();
+    /// a functor for the destructor
+    destroy_t* destructor_;
+
+    /// uses dlopen to on the module shared lib
+    void openLibrary();
+
+    /// sets the constructor member
+    void setConstructor();
+
+    /// sets the destructor member
+    void setDestructor();
+
+    /**
+       sets the absolute path to the dynamic library based on parameters set
+       during construction
+     */
+    void setPath();
+
 };
 
 #endif
--- src/Testing/DynamicLoadingTests.cpp
+++ src/Testing/DynamicLoadingTests.cpp
@@ -8,77 +8,58 @@
 //#include <cstdlib>
 #include <stdlib.h>
 #include <fstream>
+#include <iostream>
 #include "boost/filesystem.hpp"
 
 using namespace std;
 namespace fs = boost::filesystem;
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-class DynamicLoadingTests : public ::testing::Test 
-{
-public:  
-  virtual void SetUp()
-  {
-  }
-
-  virtual void TearDown()
-  {
-  }
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(DynamicLoadingTests,LoadTestFacility) 
-{
-  EXPECT_NO_THROW(Model::loadModule("Facility","TestFacility");
-                  Model::unloadModules(););
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(DynamicLoadingTests, LoadTestFacility) {
+  Model::loadModule("Facility", "TestFacility");
+  Model::unloadModules();
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(DynamicLoadingTests,FindNonStandardPath) 
-{
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(DynamicLoadingTests, FindNonStandardPath) {
   // set up
   string name = "otherfac";
   string lib_name = "lib" + name + DynamicModule::suffix();
-  stringstream path;
-  path << Env::getInstallPath() << Env::pathDelimeter() << 
-    "test_path" << Env::pathDelimeter();
-  fs::create_directory(fs::path(path.str()));
-  string abs_path = path.str()+lib_name;
-  // add file
-  ofstream f(abs_path.c_str());
+  fs::path path = fs::path(Env::getInstallPath()) / fs::path("test_path") / fs::path(lib_name);
+
+  // create file
+  fs::create_directory(path.parent_path());
+  ofstream f(path.string().c_str());
   f.close();
+
   // add path to env
-  string cmd = Env::moduleEnvVar()+"="+path.str();
+  string cmd = "CYCLUS_MODULE_PATH=" + path.parent_path().string();
   putenv((char*)cmd.c_str());
+
   // test
-  DynamicModule mod = DynamicModule("Facility",name);
-  cout << getenv(Env::moduleEnvVar().c_str()) << endl;
-  EXPECT_NO_THROW(mod.setPath());
-  EXPECT_STREQ(abs_path.c_str(),mod.path().c_str());
-  // clean up
-  fs::remove_all(fs::path(path.str()));
+  DynamicModule mod = DynamicModule("Facility", name);
+  EXPECT_EQ(path.string(), mod.path());
+
+  fs::remove_all(path);
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(DynamicLoadingTests,LoadLibError) 
-{
-  DynamicModule mod = DynamicModule("Facility","not_a_fac");
-  EXPECT_THROW(mod.setPath(),CycIOException);
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(DynamicLoadingTests, LoadLibError) {
+  DynamicModule mod = DynamicModule("Facility", "not_a_fac");
+  EXPECT_THROW(mod.initialize(), CycIOException);
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(DynamicLoadingTests,ConstructTestFacility) 
-{
-  EXPECT_NO_THROW(Model::loadModule("Facility","TestFacility");
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(DynamicLoadingTests, ConstructTestFacility) {
+  EXPECT_NO_THROW(Model::loadModule("Facility", "TestFacility");
                   Model* fac = Model::constructModel("TestFacility");
                   Model::deleteModel(fac);
                   Model::unloadModules(););
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(DynamicLoadingTests,cloneTestFacility) 
-{
-  EXPECT_NO_THROW(Model::loadModule("Facility","TestFacility");
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(DynamicLoadingTests, cloneTestFacility) {
+  EXPECT_NO_THROW(Model::loadModule("Facility", "TestFacility");
                   Model* fac = Model::constructModel("TestFacility");
                   Prototype* clone = dynamic_cast<Prototype*>(fac)->clone();
                   Model::deleteModel(dynamic_cast<Model*>(clone));
