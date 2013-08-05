b432b4c214d1647564d07fc375a342ab5558b042
gidden <matthew.gidden@gmail.com>
2013-08-05 12:55:15 -0500

    updated solver interface documentation

--- src/Core/cyclopts/solver_interface.h
+++ src/Core/cyclopts/solver_interface.h
@@ -9,25 +9,36 @@
 
 namespace cyclus {
 namespace cyclopts {
-/// interface class to set up and solve a constraint program
+/// A general interface class for all derived cyclopts solvers to set up and 
+/// solve a constraint program.
 class SolverInterface {
  public:
-  /// constructor, sets the solver to use
+  /// constructor
+  /// @param solver a pointer to the concrete solver to which an interface will
+  /// be provided.
   SolverInterface(SolverPtr solver);
 
-  /// add a constraint
+  /// add a variable
+  /// @param v a pointer to the variable to register with the problem
   void RegisterVariable(cyclus::cyclopts::VariablePtr v);
 
   /// set the objective function
+  /// @param obj a pointer to the objective function to register
   void RegisterObjFunction(cyclus::cyclopts::ObjFuncPtr obj);
 
   /// add a variable to the objective function
+  /// @param v the variable to add to the obj function
+  /// @param modifier the modifier for that variable in the obj function
   void AddVarToObjFunction(cyclus::cyclopts::VariablePtr v, double modifier);
 
   /// add a constraint
+  /// @param c a pointer to a constraint function to register
   void RegisterConstraint(cyclus::cyclopts::ConstraintPtr c);
 
   /// add a variable to a constraint
+  /// @param v the variable to add to a constraint
+  /// @param modifier the modifier for that variable in the constraint
+  /// @param c the constraint to add it to
   void AddVarToConstraint(cyclus::cyclopts::VariablePtr v, double modifier, 
                           cyclus::cyclopts::ConstraintPtr c);
 
