1669c1ab6d520be2f1da2889b615f20cc2703efc
Robert Carlsen <rwcarlsen@gmail.com>
2013-08-26 16:27:56 -0500

    polished resource class doc comments

--- src/Core/resource.h
+++ src/Core/resource.h
@@ -9,48 +9,57 @@ namespace cyclus {
 
 typedef std::string ResourceType;
 
+/// Resource defines an abstract interface implemented by types that are
+/// offered, requested, and transferred between simulation agents. Resources
+/// represent the lifeblood of a simulation.
 class Resource {
  public:
   typedef boost::shared_ptr<Resource> Ptr;
 
   virtual ~Resource() { };
 
-  /// returns the unique id corresponding to this resource and its current
-  /// state.
+  /// Returns the unique id corresponding to this resource and its current
+  /// state.  All resource id's are unique - even across different resource
+  /// types/implementations.
   const int id() const {
     return id_;
   };
 
-  /// assigns a new, unique id to this resource and its state.
+  /// Assigns a new, unique id to this resource and its state. This should be
+  /// called by resource implementations whenever their state changes.  A call to
+  /// BumpId is not always accompanied by a change to the state id.
   void BumpId();
 
-  /// returns an id representing the specific resource implementation's
-  /// internal state.  Any change to the state_id should be accompanied by a
-  /// call to BumpId.
+  /// Returns an id representing the specific resource implementation's
+  /// internal state that is not accessible via the Resource class public
+  /// interface.  Any change to the state_id should be accompanied by a call to
+  /// BumpId.
   virtual int state_id() const = 0;
 
+  /// A unique type/name for the concrete resource implementation.
   virtual const ResourceType type() const = 0;
 
-  /// returns an untracked (not part of the simulation) copy of the resource.
-  /// A cloned resource should not ever record anything in the output
-  /// database.
+  /// Returns an untracked (not part of the simulation) copy of the resource.
+  /// A cloned resource should never record anything in the output database.
   virtual Ptr Clone() const = 0;
 
-  /// records the resource's state to the output database.  This method
+  /// Records the resource's state to the output database.  This method
   /// should generally NOT record data accessible via the Resource class
   /// public methods (e.g.  state_id, units, type, quantity)
   virtual void Record() const = 0;
 
-  /// Returns the units this resource is based in.
+  /// Returns the units this resource is based in (e.g. "kg").
   virtual std::string units() const = 0;
 
-  /// returns the quantity of this resource with dimensions as specified by
-  /// units().
+  /// Returns the quantity of this resource with dimensions as specified by
+  /// the return value of units().
   virtual double quantity() const = 0;
 
-  /// splits the resource and returns the extracted portion as a new resource
+  /// Splits the resource and returns the extracted portion as a new resource
   /// object.  Allows for things like ResourceBuff and market matching to
   /// split offers/requests of arbitrary resource implementation type.
+  ///
+  /// @return a new resource object with same state id and quantity == quantity
   virtual Ptr ExtractRes(double quantity) = 0;
 
  private:
