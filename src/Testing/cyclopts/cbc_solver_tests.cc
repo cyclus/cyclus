22472f5c1d5ba8005a3ee847471ef8c0e56f3183
gidden <matthew.gidden@gmail.com>
2013-08-30 15:07:14 -0500

    removed bounded 2 variable test, starting simple with 1 variable

--- src/Testing/cyclopts/cbc_solver_tests.cc
+++ src/Testing/cyclopts/cbc_solver_tests.cc
@@ -3,6 +3,8 @@
 
 #include <boost/any.hpp>
 
+#include "CoinModel.hpp"
+
 #include "cyclopts/cbc_solver.h"
 #include "cyclopts/function.h"
 #include "cyclopts/solver.h"
@@ -10,7 +12,7 @@
 #include "cyclopts/variable.h"
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST(CycloptsCBCSolverTests, 1VarIP) {
+TEST(CycloptsCBCSolverTests, 1VarIPUnbounded) {
   // usings
   using boost::any_cast;
   using cyclus::cyclopts::SolverPtr;
@@ -25,7 +27,7 @@ TEST(CycloptsCBCSolverTests, 1VarIP) {
   using cyclus::cyclopts::ObjectiveFunction;
 
   // problem instance values
-  int x_exp = 1;
+  int x_exp = 0;
   int upper = 1;
   int lower = 0;
   double obj_mod = 1.0;
@@ -35,11 +37,11 @@ TEST(CycloptsCBCSolverTests, 1VarIP) {
   SolverInterface csi(solver);
 
   // set up objective function
-  ObjFuncPtr obj(new ObjectiveFunction(ObjectiveFunction::MAX));
+  ObjFuncPtr obj(new ObjectiveFunction(ObjectiveFunction::MIN));
   csi.RegisterObjFunction(obj);
 
   // set up variables
-  VariablePtr x(new IntegerVariable(lower, upper));
+  VariablePtr x(new IntegerVariable(lower, Variable::INF));
   csi.RegisterVariable(x);
 
   // objective function
@@ -88,60 +90,6 @@ TEST(CycloptsCBCSolverTests, 2VarIP) {
   VariablePtr x(new IntegerVariable(0, Variable::INF));
   csi.RegisterVariable(x);
   VariablePtr y(new IntegerVariable(0, Variable::INF));
-  csi.RegisterVariable(y);
-
-  // configure constraint and objective function
-  csi.AddVarToConstraint(x, cap_x, c);
-  csi.AddVarToConstraint(y, cap_y, c);
-  csi.AddVarToObjFunction(x, cost_x);
-  csi.AddVarToObjFunction(y, cost_y);
-  
-  // solve and get solution
-  csi.Solve();
-
-  // check
-  EXPECT_EQ(x_exp, any_cast<int>(x->value()));
-  EXPECT_EQ(y_exp, any_cast<int>(y->value()));
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST(CycloptsCBCSolverTests, 2VarIPBounded) {
-  // usings
-  using boost::any_cast;
-  using cyclus::cyclopts::SolverPtr;
-  using cyclus::cyclopts::CBCSolver;
-  using cyclus::cyclopts::Constraint;
-  using cyclus::cyclopts::ConstraintPtr;
-  using cyclus::cyclopts::ObjFuncPtr;
-  using cyclus::cyclopts::SolverInterface;
-  using cyclus::cyclopts::Variable;
-  using cyclus::cyclopts::VariablePtr;
-  using cyclus::cyclopts::IntegerVariable;
-  using cyclus::cyclopts::ObjectiveFunction;
-
-  // problem instance values
-  int x_exp = 1, y_exp = 2;
-  double cap_x = 3.0, cap_y = 10.0, cost_x = 1.0, cost_y = 2.0;
-  double unmet_demand = 22.0;
-  int upper = 5;
-  int lower = 0;
-  
-  // set up solver and interface
-  SolverPtr solver(new CBCSolver());
-  SolverInterface csi(solver);
-
-  // set up objective function
-  ObjFuncPtr obj(new ObjectiveFunction(ObjectiveFunction::MIN));
-  csi.RegisterObjFunction(obj);
-
-  // set up constraint
-  ConstraintPtr c(new Constraint(Constraint::GTEQ, unmet_demand));
-  csi.RegisterConstraint(c);
-
-  // set up variables
-  VariablePtr x(new IntegerVariable(lower, upper));
-  csi.RegisterVariable(x);
-  VariablePtr y(new IntegerVariable(lower, upper));
   csi.RegisterVariable(y);
 
   // configure constraint and objective function
