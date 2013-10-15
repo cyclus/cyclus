bc447504cd2be9ac82ad97d6cca9b6b4e0b80258
Matthew Gidden <matthew.gidden@gmail.com>
2013-10-15 19:43:10 -0400

    moved all files out of optim dirs into their respective top levels

--- src/cyc_limits.h
+++ src/cyc_limits.h
@@ -1,6 +1,9 @@
 #ifndef CYCLIMITS_H
 #define CYCLIMITS_H
 
+#include <cmath>
+#include <limits>
+
 namespace cyclus {
 /// a generic epsilon value
 inline double eps() {
@@ -11,6 +14,18 @@ inline double eps() {
 inline double eps_rsrc() {
   return 1e-6;
 }
+
+/// maximum value for a function modifier (i.e., a_i for variable x_i)
+static const double kModifierLimit = pow(10,10);
+
+/// maximum (+ or -) value for an integer variable
+static const int kIntBoundLimit = std::numeric_limits<int>::max();
+
+/// maximum (+ or -) value for an linear variable
+static const double kLinBoundLimit = std::numeric_limits<double>::max();
+
+/// epsilon value to turn determine difference between constraint values
+static const double kConstraintEps = 1e-08;
 }
 
 #endif
