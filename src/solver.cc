05092009870eb36018bd0396d69b58b53524609a
gidden <matthew.gidden@gmail.com>
2013-10-16 15:02:57 -0500

    put solver impl under cyclus namespace

--- src/solver.cc
+++ src/solver.cc
@@ -3,15 +3,18 @@
 #include <map>
 #include <vector>
 
+namespace cyclus {
+
 // -----------------------------------------------------------------------------
-cyclus::Solver::Solver() {
-  index_ = std::map<cyclus::VariablePtr, int>();
+Solver::Solver() {
+  index_ = std::map<VariablePtr, int>();
 }
 
 // -----------------------------------------------------------------------------
-void cyclus::Solver::PopulateIndices(
-    std::vector<cyclus::VariablePtr>& variables) {
+void Solver::PopulateIndices(std::vector<VariablePtr>& variables) {
   for (int i = 0; i < variables.size(); i++) {
     index_[variables.at(i)] = i;
   }
 }
+
+} // namespace cyclus
