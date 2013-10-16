313b06ceecb8ab3c6f62ee07845b9d8922c837d0
gidden <matthew.gidden@gmail.com>
2013-10-16 15:05:19 -0500

    moved solver interface impl under cyclus namespace

--- src/solver_interface.cc
+++ src/solver_interface.cc
@@ -7,59 +7,56 @@
 
 #include "cyc_limits.h"
 
+namespace cyclus {
+
 // -----------------------------------------------------------------------------
-cyclus::SolverInterface::SolverInterface(SolverPtr s) : solver_(s) {
-  constraints_ = std::vector<cyclus::ConstraintPtr>();
-  variables_ = std::vector<cyclus::VariablePtr>();
-  modifier_limit_ = cyclus::kModifierLimit; // this is a bandaid, I don't know why it has to happen... somethings up with cbc
+SolverInterface::SolverInterface(SolverPtr s) : solver_(s) {
+  constraints_ = std::vector<ConstraintPtr>();
+  variables_ = std::vector<VariablePtr>();
+  modifier_limit_ = kModifierLimit; // this is a bandaid, I don't know why it has to happen... somethings up with cbc
 };
 
 // -----------------------------------------------------------------------------
-void cyclus::SolverInterface::RegisterVariable(
-    cyclus::VariablePtr v) {
+void SolverInterface::RegisterVariable(VariablePtr v) {
   variables_.push_back(v);
 }
 
 // -----------------------------------------------------------------------------
-void cyclus::SolverInterface::RegisterObjFunction(
-    cyclus::ObjFuncPtr obj) {
+void SolverInterface::RegisterObjFunction(ObjFuncPtr obj) {
   obj_ = obj;
 }
 
 // -----------------------------------------------------------------------------
-void cyclus::SolverInterface::AddVarToObjFunction(
-    cyclus::VariablePtr v, 
-    double modifier) {
+void SolverInterface::AddVarToObjFunction(VariablePtr v, double modifier) {
   // need to check that v is in variables_
   CheckModifierBounds(modifier);
   obj_->AddVariable(v, modifier);
 }
 
 // -----------------------------------------------------------------------------
-void cyclus::SolverInterface::RegisterConstraint(
-    cyclus::ConstraintPtr c) {
+void SolverInterface::RegisterConstraint(ConstraintPtr c) {
   constraints_.push_back(c);
 }
 
 // -----------------------------------------------------------------------------
-void cyclus::SolverInterface::AddVarToConstraint(
-    cyclus::VariablePtr v, 
+void SolverInterface::AddVarToConstraint(
+    VariablePtr v, 
     double modifier, 
-    cyclus::ConstraintPtr c) {
+    ConstraintPtr c) {
   CheckModifierBounds(modifier);
   // need to check that v is in variables_ and c is in constraints_
-  std::vector<cyclus::ConstraintPtr>::iterator it;
+  std::vector<ConstraintPtr>::iterator it;
   it = find(constraints_.begin(), constraints_.end(), c);
   it->get()->AddVariable(v, modifier);
 }
 
 // -----------------------------------------------------------------------------
-void cyclus::SolverInterface::Solve() {
+void SolverInterface::Solve() {
   solver_->Solve(variables_, obj_, constraints_);
 }
 
 // -------------------------------------------------------------------
-void cyclus::SolverInterface::CheckModifierBounds(double modifier) {
+void SolverInterface::CheckModifierBounds(double modifier) {
   if (modifier > modifier_limit_) {
     std::stringstream msg;
     msg << "Cannot add modifier " 
@@ -69,3 +66,5 @@ void cyclus::SolverInterface::CheckModifierBounds(double modifier) {
     throw std::runtime_error(msg.str());
   }
 }
+
+} // namespace cyclus
