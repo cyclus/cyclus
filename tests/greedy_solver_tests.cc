39079dd17eaff85ed9f9a06876eb07607a43b278
Matthew Gidden <matthew.gidden@gmail.com>
2013-11-20 15:09:18 -0600

    added greedy solver test case 3

--- tests/greedy_solver_tests.cc
+++ tests/greedy_solver_tests.cc
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "greedy_solver_tests.h"
 
 using cyclus::ExchangeGraph;
@@ -84,5 +86,92 @@ TEST(GrSolverTests, Case2c) {
   Match exp = Match(g.arcs_[0], cap);
   ASSERT_TRUE(g.matches.size() > 0);
   EXPECT_EQ(exp, g.matches[0]);
+}
+/* --- */
+
+/* --- */
+// Case 3:
+// 2 supply node with capacity, c1 & c2
+// 1 request node with request quantity, q
+// flow from s1 -> r := f1
+// flow from s2 -> r := f2
+TEST(GrSolverTests, Case3a) {
+  // q > c1
+  // f1 = c1, f2 DNE
+  double q = 5;
+  double c1 = 5;
+  double c2 = 10;
+  ExchangeGraph g = SetUp3(q, c1, c2);
+  GreedySolver solver(&g);
+  solver.Solve();
+  
+  ASSERT_TRUE(g.arcs_.size() > 0);
+  EXPECT_EQ(g.arcs_.size(), 2);
+  
+  Match exp = Match(g.arcs_[0], c1);
+  Match arr[] = {exp};
+  std::vector<Match> vexp(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  EXPECT_EQ(vexp, g.matches);
+}
+
+TEST(GrSolverTests, Case3b) {
+  // q - c1 < c2
+  // f1 = c1, f2 = q - c1
+  double q = 5;
+  double c1 = 3;
+  double c2 = 3;
+  ExchangeGraph g = SetUp3(q, c1, c2);
+  GreedySolver solver(&g);
+  solver.Solve();
+
+  ASSERT_TRUE(g.arcs_.size() > 1);
+  EXPECT_EQ(g.arcs_.size(), 2);
+  
+  Match exp1 = Match(g.arcs_[0], c1);
+  Match exp2 = Match(g.arcs_[1], q - c1);
+  Match arr[] = {exp1, exp2};
+  std::vector<Match> vexp(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  EXPECT_EQ(vexp, g.matches);
+}
+
+TEST(GrSolverTests, Case3c) {
+  // q - c1 = c2
+  // f1 = c1, f2 = c2
+  double q = 5;
+  double c1 = 3;
+  double c2 = 2;
+  ExchangeGraph g = SetUp3(q, c1, c2);
+  GreedySolver solver(&g);
+  solver.Solve();
+  
+  ASSERT_TRUE(g.arcs_.size() > 1);
+  EXPECT_EQ(g.arcs_.size(), 2);
+  
+  Match exp1 = Match(g.arcs_[0], c1);
+  Match exp2 = Match(g.arcs_[1], c2);
+  Match arr[] = {exp1, exp2};
+  std::vector<Match> vexp(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  EXPECT_EQ(vexp, g.matches);
+}
+
+
+TEST(GrSolverTests, Case3d) {
+  // q - c1 > c2
+  // f1 = c1, f2 = c2
+  double q = 5;
+  double c1 = 3;
+  double c2 = 1;
+  ExchangeGraph g = SetUp3(q, c1, c2);
+  GreedySolver solver(&g);
+  solver.Solve();
+    
+  ASSERT_TRUE(g.arcs_.size() > 1);
+  EXPECT_EQ(g.arcs_.size(), 2);
+  
+  Match exp1 = Match(g.arcs_[0], c1);
+  Match exp2 = Match(g.arcs_[1], c2);
+  Match arr[] = {exp1, exp2};
+  std::vector<Match> vexp(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  EXPECT_EQ(vexp, g.matches);
 }
 /* --- */
--- tests/greedy_solver_tests.h
+++ tests/greedy_solver_tests.h
@@ -50,17 +50,60 @@ cyclus::ExchangeGraph SetUp2(double qty, double capacity) {
   u->unit_capacities[a.get()].push_back(1);
   v->unit_capacities[a.get()].push_back(1);
   
+  RequestSet::Ptr request(new RequestSet(qty));
+  request->capacities.push_back(qty*5); // some large number
+  request->AddNode(u);  
+  g.AddRequestSet(request);
+
   NodeSet::Ptr supply(new NodeSet());
   supply->capacities.push_back(capacity);
-  supply->AddNode(u);  
+  supply->AddNode(v);  
   g.AddSupplySet(supply);
+
+  g.AddArc(a);
+  
+  return g;
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::ExchangeGraph SetUp3(double qty, double cap1, double cap2) {
+  using cyclus::ExchangeGraph;
+  using cyclus::Node;
+  using cyclus::NodeSet;
+  using cyclus::RequestSet;
+  using cyclus::Arc;
+  using cyclus::Match;
+  
+  ExchangeGraph g;
+  
+  Node::Ptr u(new Node());
+  Node::Ptr v(new Node());
+  Node::Ptr w(new Node());
+  Arc::Ptr a1(new Arc(u, v));
+  Arc::Ptr a2(new Arc(u, w));
+
+  u->unit_capacities[a1.get()].push_back(1);
+  v->unit_capacities[a1.get()].push_back(1);
+  u->unit_capacities[a2.get()].push_back(1);
+  w->unit_capacities[a2.get()].push_back(1);
 
   RequestSet::Ptr request(new RequestSet(qty));
   request->capacities.push_back(qty*5); // some large number
-  request->AddNode(v);  
+  request->AddNode(u);  
   g.AddRequestSet(request);
+  
+  NodeSet::Ptr supply1(new NodeSet());
+  supply1->capacities.push_back(cap1);
+  supply1->AddNode(v);  
+  g.AddSupplySet(supply1);
+  
+  NodeSet::Ptr supply2(new NodeSet());
+  supply2->capacities.push_back(cap2);
+  supply2->AddNode(w);  
+  g.AddSupplySet(supply2);
 
-  g.AddArc(a);
+  g.AddArc(a1);
+  g.AddArc(a2);
   
   return g;
 };
