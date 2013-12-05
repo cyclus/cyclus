510410ddec95d2f59cf6dd1139f85138747366f7
gidden <matthew.gidden@gmail.com>
2013-12-05 14:30:21 -0600

    added a docstring line about the preference ordering

--- src/greedy_solver.h
+++ src/greedy_solver.h
@@ -19,11 +19,12 @@ class ExchangeGraph;
 /// @brief The GreedySolver provides the implementation for a "greedy" solution
 /// to a resource exchange graph.
 ///
-/// Given an ExchangeGraph, the greedy solver will march through each RequestGroup
-/// in the graph, matching request nodes "greedily" with supply nodes. Each
-/// request node will attempt to be supplied by supplier arcs as long as those
-/// supplier arcs have some excess capacity. The algorithm terminates when one
-/// of the following conditions is met:
+/// Given an ExchangeGraph, the greedy solver will march through each
+/// RequestGroup in the graph, matching request nodes "greedily" with supply
+/// nodes. Each request node will attempt to be supplied by supplier arcs as
+/// long as those supplier arcs have some excess capacity. The possible
+/// suppliers will be ordered by descending preference. The algorithm terminates
+/// when one of the following conditions is met:
 ///   1) All RequestGroups are satisfied
 ///   2) All SupplySets are at capacity
 class GreedySolver: public ExchangeSolver {
