b127a0dded759c16cb132675624a376e47e8f896
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-11 12:57:58 -0600

    Initital iplementation of source facility.

--- integration_tests/Models/SourceFacility/source_facility_tests.cc
+++ integration_tests/Models/SourceFacility/source_facility_tests.cc
@@ -1,76 +1,76 @@
 #include <gtest/gtest.h>
 
-#include "stub_facility.h"
+#include "source_facility.h"
 
 #include "context.h"
 #include "facility_model_tests.h"
 #include "model_tests.h"
 
-using stubs::StubFacility;
+using cyclus::SourceFacility;
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class StubFacilityTest : public ::testing::Test {
+class SourceFacilityTest : public ::testing::Test {
  protected:
   cyclus::TestContext tc_;
-  StubFacility* src_facility_;
+  SourceFacility* src_facility_;
 
   virtual void SetUp(){
-    src_facility_ = new StubFacility(tc_.get());
+    src_facility_ = new SourceFacility(tc_.get());
   };
-  
+
   virtual void TearDown() {}
 };
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(StubFacilityTest, clone) {
-  StubFacility* cloned_fac =
-      dynamic_cast<StubFacility*> (src_facility_->Clone());
+TEST_F(SourceFacilityTest, clone) {
+  SourceFacility* cloned_fac =
+      dynamic_cast<SourceFacility*> (src_facility_->Clone());
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubFacilityTest, InitialState) {
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SourceFacilityTest, InitialState) {
   // Test things about the initial state of the facility here
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubFacilityTest, Print) {
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SourceFacilityTest, Print) {
   EXPECT_NO_THROW(std::string s = src_facility_->str());
-  // Test StubFacility specific aspects of the print method here
+  // Test SourceFacility specific aspects of the print method here
 }
 
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubFacilityTest, ReceiveMessage) {
-  // Test StubFacility specific behaviors of the ReceiveMessage function here
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SourceFacilityTest, ReceiveMessage) {
+  // Test SourceFacility specific behaviors of the ReceiveMessage function here
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(StubFacilityTest, Tick) {
+TEST_F(SourceFacilityTest, Tick) {
   int time = 1;
   ASSERT_NO_THROW(src_facility_->Tick(time));
-  // Test StubFacility specific behaviors of the Tick function here
+  // Test SourceFacility specific behaviors of the Tick function here
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(StubFacilityTest, Tock) {
+TEST_F(SourceFacilityTest, Tock) {
   int time = 1;
   EXPECT_NO_THROW(src_facility_->Tock(time));
-  // Test StubFacility specific behaviors of the Tock function here
+  // Test SourceFacility specific behaviors of the Tock function here
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Model* StubFacilityModelConstructor(cyclus::Context* ctx) {
-  return dynamic_cast<cyclus::Model*>(new StubFacility(ctx));
+cyclus::Model* SourceFacilityModelConstructor(cyclus::Context* ctx) {
+  return dynamic_cast<cyclus::Model*>(new SourceFacility(ctx));
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::FacilityModel* StubFacilityConstructor(cyclus::Context* ctx) {
-  return dynamic_cast<cyclus::FacilityModel*>(new StubFacility(ctx));
+cyclus::FacilityModel* SourceFacilityConstructor(cyclus::Context* ctx) {
+  return dynamic_cast<cyclus::FacilityModel*>(new SourceFacility(ctx));
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-INSTANTIATE_TEST_CASE_P(StubFac, FacilityModelTests,
-                        ::testing::Values(&StubFacilityConstructor));
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+INSTANTIATE_TEST_CASE_P(SourceFac, FacilityModelTests,
+                        ::testing::Values(&SourceFacilityConstructor));
 
-INSTANTIATE_TEST_CASE_P(StubFac, ModelTests,
-                        ::testing::Values(&StubFacilityModelConstructor));
+INSTANTIATE_TEST_CASE_P(SourceFac, ModelTests,
+                        ::testing::Values(&SourceFacilityModelConstructor));
