33ce60bd040d09a320407cfef4c170f8412f07e7
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-11 12:34:18 -0600

    Implementation of a basic region model from stubs.

--- integration_tests/Models/NullRegion/null_region_tests.cc
+++ integration_tests/Models/NullRegion/null_region_tests.cc
@@ -1,75 +1,75 @@
 #include <gtest/gtest.h>
 
-#include "stub_region.h"
+#include "null_region.h"
 
 #include "region_model_tests.h"
 #include "model_tests.h"
 
-using stubs::StubRegion;
+using cyclus::NullRegion;
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class StubRegionTest : public ::testing::Test {
-  protected:
-    cyclus::TestContext tc_;
-    StubRegion* src_region_;
+class NullRegionTest : public ::testing::Test {
+ protected:
+  cyclus::TestContext tc_;
+  NullRegion* src_region_;
 
-    virtual void SetUp(){
-      src_region_ = new StubRegion(tc_.get());
-    };
+  virtual void SetUp(){
+    src_region_ = new NullRegion(tc_.get());
+  };
 
-    virtual void TearDown() {}
+  virtual void TearDown() {}
 };
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(StubRegionTest, clone) {
-  StubRegion* cloned_fac =
-      dynamic_cast<StubRegion*> (src_region_->Clone());
+TEST_F(NullRegionTest, clone) {
+  NullRegion* cloned_fac =
+      dynamic_cast<NullRegion*> (src_region_->Clone());
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubRegionTest, InitialState) {
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(NullRegionTest, InitialState) {
   // Test things about the initial state of the region here
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubRegionTest, Print) {
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(NullRegionTest, Print) {
   EXPECT_NO_THROW(std::string s = src_region_->str());
-  // Test StubRegion specific aspects of the print method here
+  // Test NullRegion specific aspects of the print method here
 }
 
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubRegionTest, ReceiveMessage) {
-  // Test StubRegion specific behaviors of the receiveMessage function here
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubRegionTest, Tick) {
-  int time = 1;
-  EXPECT_NO_THROW(src_region_->Tick(time));
-  // Test StubRegion specific behaviors of the handleTick function here
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-TEST_F(StubRegionTest, Tock) {
-  int time = 1;
-  EXPECT_NO_THROW(src_region_->Tick(time));
-  // Test StubRegion specific behaviors of the handleTock function here
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(NullRegionTest, ReceiveMessage) {
+  // Test NullRegion specific behaviors of the receiveMessage function here
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Model* StubRegionModelConstructor(cyclus::Context* ctx) {
-  return dynamic_cast<cyclus::Model*>(new StubRegion(ctx));
+TEST_F(NullRegionTest, Tick) {
+  int time = 1;
+  EXPECT_NO_THROW(src_region_->Tick(time));
+  // Test NullRegion specific behaviors of the handleTick function here
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::RegionModel* StubRegionConstructor(cyclus::Context* ctx) {
-  return dynamic_cast<cyclus::RegionModel*>(new StubRegion(ctx));
+TEST_F(NullRegionTest, Tock) {
+  int time = 1;
+  EXPECT_NO_THROW(src_region_->Tick(time));
+  // Test NullRegion specific behaviors of the handleTock function here
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-INSTANTIATE_TEST_CASE_P(StubRegion, RegionModelTests,
-                        ::testing::Values(&StubRegionConstructor));
-INSTANTIATE_TEST_CASE_P(StubRegion, ModelTests,
-                        ::testing::Values(&StubRegionModelConstructor));
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::Model* NullRegionModelConstructor(cyclus::Context* ctx) {
+  return dynamic_cast<cyclus::Model*>(new NullRegion(ctx));
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::RegionModel* NullRegionConstructor(cyclus::Context* ctx) {
+  return dynamic_cast<cyclus::RegionModel*>(new NullRegion(ctx));
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+INSTANTIATE_TEST_CASE_P(NullRegion, RegionModelTests,
+                        ::testing::Values(&NullRegionConstructor));
+INSTANTIATE_TEST_CASE_P(NullRegion, ModelTests,
+                        ::testing::Values(&NullRegionModelConstructor));
 
