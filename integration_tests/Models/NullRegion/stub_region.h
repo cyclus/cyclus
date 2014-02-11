67caa73282c36a368c5ff979296531364d9cb167
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-11 07:28:30 -0600

    Add stub facilities, institutions, and regions.

---
+++ integration_tests/Models/NullRegion/stub_region.h
@@ -0,0 +1,90 @@
+#ifndef STUB_REGION_H_
+#define STUB_REGION_H_
+
+#include <string>
+
+#include "context.h"
+#include "region_model.h"
+#include "query_engine.h"
+
+namespace stubs {
+
+/**
+   @class StubRegion 
+    
+   This RegionModel is intended 
+   as a skeleton to guide the implementation of new RegionModel models. 
+    
+   The StubRegion class inherits from the RegionModel class and is 
+   dynamically loaded by the Model class when requested. 
+    
+   @section intro Introduction 
+   Place an introduction to the model here. 
+    
+   @section modelparams Model Parameters 
+   Place a description of the required input parameters which define the 
+   model implementation. 
+    
+   @section optionalparams Optional Parameters 
+   Place a description of the optional input parameters to define the 
+   model implementation. 
+    
+   @section detailed Detailed Behavior 
+   Place a description of the detailed behavior of the model. Consider 
+   describing the behavior at the tick and tock as well as the behavior 
+   upon sending and receiving materials and messages. 
+ */
+class StubRegion : public cyclus::RegionModel {
+/* --------------------
+ * all REGIONMODEL classes have these members
+ * --------------------
+ */
+ public:
+  /**
+     Constructor for StubRegion Class 
+     @param ctx the cyclus context for access to simulation-wide parameters
+   */
+  StubRegion(cyclus::Context* ctx);
+
+  /**
+     every model should be destructable 
+   */
+  virtual ~StubRegion();
+    
+  /**
+     Initialize members related to derived module class
+
+     @param qe a pointer to a QueryEngine object containing initialization data
+   */
+  virtual void InitFrom(cyclus::QueryEngine* qe);
+  
+  /**
+     Initialize members for a cloned module.
+   */
+  virtual void InitFrom(StubRegion* m);
+
+  /**
+     A verbose printer for the StubRegion
+   */
+   virtual std::string str();
+
+  /**
+     Initializes a StubRegion object by copying the members of another.
+   */
+   virtual cyclus::Model* Clone();
+  
+/* -------------------- */
+
+
+/* --------------------
+ * all REGIONMODEL classes have these members
+ * --------------------
+ */
+
+/* ------------------- */ 
+
+};
+
+} // namespace stubs
+  
+#endif // STUB_REGION_H_
---
+++ integration_tests/Models/SinkFacility/stub_facility.h
@@ -0,0 +1,105 @@
+#ifndef CYCLUS_STUBS_STUB_FACILITY_H_
+#define CYCLUS_STUBS_STUB_FACILITY_H_
+
+#include <string>
+
+#include "context.h"
+#include "facility_model.h"
+#include "query_engine.h"
+
+namespace stubs {
+
+/**
+   @class StubFacility 
+    
+   This FacilityModel is intended 
+   as a skeleton to guide the implementation of new FacilityModel 
+   models.  
+   The StubFacility class inherits from the FacilityModel class and is 
+   dynamically loaded by the Model class when requested. 
+    
+   @section intro Introduction 
+   Place an introduction to the model here. 
+    
+   @section modelparams Model Parameters 
+   Place a description of the required input parameters which define the 
+   model implementation. 
+    
+   @section optionalparams Optional Parameters 
+   Place a description of the optional input parameters to define the 
+   model implementation. 
+    
+   @section detailed Detailed Behavior 
+   Place a description of the detailed behavior of the model. Consider 
+   describing the behavior at the tick and tock as well as the behavior 
+   upon sending and receiving materials and messages. 
+ */
+class StubFacility : public cyclus::FacilityModel  {
+/* --------------------
+ * all FACILITYMODEL classes have these members
+ * --------------------
+ */
+ public:
+  /**
+     Constructor for StubFacility Class 
+     @param ctx the cyclus context for access to simulation-wide parameters
+   */
+  StubFacility(cyclus::Context* ctx);
+
+  /**
+     every model should be destructable 
+   */
+  virtual ~StubFacility();
+    
+  /**
+     Initialize members related to derived module class
+
+     @param qe a pointer to a QueryEngine object containing initialization data
+   */
+  virtual void InitFrom(cyclus::QueryEngine* qe);
+
+  /**
+     Initialize members for a cloned module.
+   */
+  virtual void InitFrom(StubFacility* m);
+  
+  /**
+     A verbose printer for the StubFacility
+   */
+   virtual std::string str();
+
+  /**
+     Initializes a StubFacility object by copying the members of another.
+   */
+   virtual cyclus::Model* Clone();
+
+  /**
+     The handleTick function specific to the StubFacility. 
+      
+     @param time the time of the tick 
+   */
+  virtual void Tick(int time);
+
+  /**
+     The handleTick function specific to the StubFacility. 
+      
+     @param time the time of the tock 
+   */
+  virtual void Tock(int time);
+
+/* ------------------- */ 
+
+
+/* --------------------
+ * _THIS_ FACILITYMODEL class has these members
+ * --------------------
+ */
+
+/* ------------------- */ 
+
+};
+
+} // namespace stub
+  
+#endif // STUB_FACILITY_H_
+
