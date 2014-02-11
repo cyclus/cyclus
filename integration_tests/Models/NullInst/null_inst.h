82ad8f6e7bbaea3cd23b7a061a557d6825494635
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-11 11:23:18 -0600

    Implementation of null institution.

--- integration_tests/Models/NullInst/null_inst.h
+++ integration_tests/Models/NullInst/null_inst.h
@@ -1,5 +1,5 @@
-#ifndef STUB_INST_H_
-#define STUB_INST_H_
+#ifndef NULL_INST_H_
+#define NULL_INST_H_
 
 #include <string>
 
@@ -7,84 +7,79 @@
 #include "inst_model.h"
 #include "query_engine.h"
 
-namespace stubs {
+namespace cyclus {
 
 /**
-   @class StubInst 
-    
-   This InstModel is intended 
-   as a skeleton to guide the implementation of new InstModel models. 
-    
-   The StubInst class inherits from the InstModel class and is 
-   dynamically loaded by the Model class when requested. 
-    
-   @section intro Introduction 
-   Place an introduction to the model here. 
-    
-   @section modelparams Model Parameters 
-   Place a description of the required input parameters which define the 
-   model implementation. 
-    
-   @section optionalparams Optional Parameters 
-   Place a description of the optional input parameters to define the 
-   model implementation. 
-    
-   @section detailed Detailed Behavior 
-   Place a description of the detailed behavior of the model. Consider 
-   describing the behavior at the tick and tock as well as the behavior 
-   upon sending and receiving materials and messages. 
- */
-class StubInst : public cyclus::InstModel {
-/* --------------------
- * all INSTMODEL classes have these members
- * --------------------
- */
+  @class NullInst
+
+  This InstModel is intended
+  as a basic institution for Cyclus integration testing.
+
+  @section intro Introduction
+  This institution performs the most basics functions just enough for
+  integration testing.
+
+  @section modelparams Model Parameters
+  Model parameters are not expected for this model.
+
+  @section optionalparams Optional Parameters
+  Optional parameters are not expected for this model.
+
+  @section detailed Detailed Behavior
+  The behaviour of this model should be similar to the null institution
+  model implemented in cycamore.
+  */
+class NullInst : public cyclus::InstModel {
+  /* --------------------
+   * all INSTMODEL classes have these members
+   * --------------------
+   */
  public:
   /**
-     Constructor for StubInst Class 
-     @param ctx the cyclus context for access to simulation-wide parameters
-   */
-  StubInst(cyclus::Context* ctx);
+    Constructor for NullInst Class
+    @param ctx the cyclus context for access to simulation-wide parameters
+    */
+  NullInst(cyclus::Context* ctx);
 
   /**
-     every model should be destructable 
-   */
-  virtual ~StubInst();
-    
+    every model should be destructable
+    */
+  virtual ~NullInst();
+
   /**
-     Initializes the module member data from the data in a QueryEngine object
-      
-     @param qe is a QueryEngine object that contains intialization data
-   */
+    Initializes the module member data from the data in a QueryEngine object
+
+    @param qe is a QueryEngine object that contains intialization data
+    */
   virtual void InitFrom(cyclus::QueryEngine* qe);
 
   /**
-     Initialize members for a cloned module.
-   */
-  virtual void InitFrom(StubInst* m);
+    Initialize members for a cloned module.
+    */
+  virtual void InitFrom(NullInst* m);
 
   /**
-     Initializes a StubInst object by copying the members of another.
-   */
-   virtual cyclus::Model* Clone();
-   
+    Initializes a NullInst object by copying the members of another.
+    */
+  virtual cyclus::Model* Clone();
+
   /**
-     every model should be able to print a verbose description 
+    every model should be able to print a verbose description
+    */
+  virtual std::string str();
+
+  /* ------------------- */
+
+
+  /* --------------------
+   * _THIS_ INSTMODEL class has these members
+   * --------------------
    */
-   virtual std::string str();
 
-/* ------------------- */ 
-
-
-/* --------------------
- * _THIS_ INSTMODEL class has these members
- * --------------------
- */
-
-/* ------------------- */ 
+  /* ------------------- */
 
 };
 
-} // namespace stubs
-  
-#endif // STUB_INST_H_
+} // namespace cyclus
+
+#endif // NULL_INST_H_
