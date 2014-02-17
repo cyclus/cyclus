1b92aa6fae1c2e7b9ad232bd69cf87d720fb57aa
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-17 06:58:55 -0600

    Add tests for null sink developed previously in cycamore repository

---
+++ integration_tests/test_null_sink.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+
+from nose.tools import assert_false
+import os
+import tables
+import numpy as np
+from tools import check_cmd
+
+""" Tests """
+def test_null_sink():
+    """ Testing for null sink case without a source facility """
+    #Cyclus simulation input for null sink testing
+    sim_input = "./inputs/null_sink.xml"
+    holdsrtn = [1] # needed because nose does not send() to test generator
+    cmd = ["cyclus", "-o", "./output_temp.h5", "--input-file", sim_input]
+    check_cmd(cmd, '.', holdsrtn)
+    rtn = holdsrtn[0]
+    if rtn != 0:
+        return # don't execute further commands
+
+    output = tables.open_file("./output_temp.h5", mode = "r")
+    paths = ["/Transactions", "/TransactedResources"] # this must contain tables to test
+    #No resource exchange is expected
+    for path in paths:
+        yield assert_false, output.__contains__(path)
+
+    output.close()
+    os.remove("./output_temp.h5")
