4c5f60ad9a174784b39fee71ca175dc7ab61903f
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-22 17:07:49 -0600

    Source facility builds.

--- integration_tests/Models/SourceFacility/source_facility.cc
+++ integration_tests/Models/SourceFacility/source_facility.cc
@@ -1,50 +1,186 @@
 #include "source_facility.h"
 
-using cyclus::SourceFacility;
+#include <sstream>
+#include <limits>
+
+#include <boost/lexical_cast.hpp>
+
+#include "capacity_constraint.h"
+#include "cyc_limits.h"
+#include "context.h"
+#include "error.h"
+#include "logger.h"
+#include "request.h"
+
+namespace cyclus {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SourceFacility::SourceFacility(cyclus::Context* ctx)
-    : cyclus::FacilityModel(ctx) {};
+    : cyclus::FacilityModel(ctx),
+      out_commod_(""),
+      recipe_name_(""),
+      commod_price_(0),
+      capacity_(std::numeric_limits<double>::max()) {}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SourceFacility::~SourceFacility() {}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void SourceFacility::InitFrom(cyclus::QueryEngine* qe) {
-  cyclus::FacilityModel::InitFrom(qe);
-  qe = qe->QueryElement(ModelImpl());
-
-  cyclus::QueryEngine* input = qe->QueryElement("input");
-  // retrieve input data members here. For example :
-  // string query = "incommodity";
-  // incommodity_ = lexical_cast<double>(input->getElementContent(query));
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::string SourceFacility::schema() {
+  return
+    "  <element name =\"output\">          \n"
+    "    <ref name=\"outcommodity\"/>      \n"
+    "    <optional>                        \n"
+    "      <ref name=\"output_capacity\"/> \n"
+    "    </optional>                       \n"
+    "    <element name=\"recipe\">         \n"
+    "      <data type=\"string\"/>         \n"
+    "    </element>                        \n"
+    "  </element>                          \n";
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void SourceFacility::InitFrom(cyclus::QueryEngine* qe) {
+  cyclus::FacilityModel::InitFrom(qe);
+  qe = qe->QueryElement("model/" + ModelImpl());
+
+  using std::string;
+  using std::numeric_limits;
+  using boost::lexical_cast;
+  cyclus::QueryEngine* output = qe->QueryElement("output");
+
+  recipe(output->GetElementContent("recipe"));
+
+  string data = output->GetElementContent("outcommodity");
+  commodity(data);
+  cyclus::Commodity commod(data);
+  cyclus::CommodityProducer::AddCommodity(commod);
+
+  double cap = cyclus::GetOptionalQuery<double>(output,
+                                                "output_capacity",
+                                                numeric_limits<double>::max());
+  cyclus::CommodityProducer::SetCapacity(commod, cap);
+  capacity(cap);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::string SourceFacility::str() {
+  std::stringstream ss;
+  ss << cyclus::FacilityModel::str()
+     << " supplies commodity '"
+     << out_commod_ << "' with recipe '"
+     << recipe_name_ << "' at a capacity of "
+     << capacity_ << " kg per time step ";
+  return ss.str();
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cyclus::Model* SourceFacility::Clone() {
   SourceFacility* m = new SourceFacility(context());
   m->InitFrom(this);
   return m;
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void SourceFacility::InitFrom(SourceFacility* m) {
-  cyclus::FacilityModel::InitFrom(m);
-  // Initialize sourcefacility members for a cloned module here
+  FacilityModel::InitFrom(m);
+  commodity(m->commodity());
+  capacity(m->capacity());
+  recipe(m->recipe());
+  CopyProducedCommoditiesFrom(m);
+  current_capacity_ = capacity();
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-std::string SourceFacility::str() {
-  return FacilityModel::str();
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void SourceFacility::Tick(int time) {
+  LOG(cyclus::LEV_INFO3, "SrcFac") << FacName() << " is ticking {";
+  LOG(cyclus::LEV_INFO4, "SrcFac") << "will offer " << capacity_
+                                   << " kg of "
+                                   << out_commod_ << ".";
+  LOG(cyclus::LEV_INFO3, "SrcFac") << "}";
+  current_capacity_ = capacity_; // reset capacity
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void SourceFacility::Tick(int time) {}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void SourceFacility::Tock(int time) {
+  LOG(cyclus::LEV_INFO3, "SrcFac") << FacName() << " is tocking {";
+  LOG(cyclus::LEV_INFO3, "SrcFac") << "}";
+}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void SourceFacility::Tock(int time) {}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::Material::Ptr SourceFacility::GetOffer(
+    const cyclus::Material::Ptr target) const {
+  using cyclus::Material;
+  double qty = std::min(target->quantity(), capacity_);
+  return Material::CreateUntracked(qty, context()->GetRecipe(recipe_name_));
+}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::set<cyclus::BidPortfolio<cyclus::Material>::Ptr>
+SourceFacility::GetMatlBids(
+    const cyclus::CommodMap<cyclus::Material>::type& commod_requests) {
+  using cyclus::Bid;
+  using cyclus::BidPortfolio;
+  using cyclus::CapacityConstraint;
+  using cyclus::Material;
+  using cyclus::Request;
+
+  std::set<BidPortfolio<Material>::Ptr> ports;
+
+  if (commod_requests.count(out_commod_) > 0) {
+    BidPortfolio<Material>::Ptr port(new BidPortfolio<Material>());
+
+    const std::vector<Request<Material>::Ptr>& requests = commod_requests.at(
+        out_commod_);
+
+    std::vector<Request<Material>::Ptr>::const_iterator it;
+    for (it = requests.begin(); it != requests.end(); ++it) {
+      const Request<Material>::Ptr req = *it;
+      Material::Ptr offer = GetOffer(req->target());
+      port->AddBid(req, offer, this);
+    }
+
+    CapacityConstraint<Material> cc(capacity_);
+    port->AddConstraint(cc);
+    ports.insert(port);
+  }
+  return ports;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void SourceFacility::GetMatlTrades(
+    const std::vector< cyclus::Trade<cyclus::Material> >& trades,
+    std::vector<std::pair<cyclus::Trade<cyclus::Material>,
+                          cyclus::Material::Ptr> >& responses) {
+  using cyclus::Material;
+  using cyclus::Trade;
+
+  double provided = 0;
+  std::vector< cyclus::Trade<cyclus::Material> >::const_iterator it;
+  for (it = trades.begin(); it != trades.end(); ++it) {
+    double qty = it->amt;
+    current_capacity_ -= qty;
+    provided += qty;
+    // @TODO we need a policy on negatives..
+    Material::Ptr response = Material::Create(this,
+                                              qty,
+                                              context()->GetRecipe(recipe_name_));
+    responses.push_back(std::make_pair(*it, response));
+    LOG(cyclus::LEV_INFO5, "SrcFac") << name() << " just received an order"
+                                     << " for " << qty
+                                     << " of " << out_commod_;
+  }
+  if (cyclus::IsNegative(current_capacity_)) {
+    std::stringstream ss;
+    ss << "is being asked to provide " << provided
+       << " but its capacity is " << capacity_ << ".";
+    throw cyclus::ValueError(Model::InformErrorMsg(ss.str()));
+  }
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 extern "C" cyclus::Model* ConstructSourceFacility(cyclus::Context* ctx) {
   return new SourceFacility(ctx);
 }
+
+}  // namespace cyclus
--- integration_tests/Models/SourceFacility/source_facility.h
+++ integration_tests/Models/SourceFacility/source_facility.h
@@ -2,16 +2,27 @@
 #define CYCLUS_INTEGRATION_TESTS_MODELS_SOURCE_FACILITY_H_
 
 #include <string>
+#include <set>
+#include <vector>
 
-#include "context.h"
+#include "bid_portfolio.h"
+#include "commodity_producer.h"
+#include "exchange_context.h"
 #include "facility_model.h"
+#include "material.h"
 #include "query_engine.h"
+#include "trade.h"
 
 namespace cyclus {
 
+class Context;
+
 /**
   @class SourceFacility
-  More detailed description will come later.
+  This facility is a source of some commodity with some capacity.
+  This facility is sort of reduced copy of cycamore's source facility.
+  A lot of implementation code is barrowed from cycamore's source facility
+  implementation files.
 
   This FacilityModel is intended
   to be used for Cyclus integration tests as a basic source facility.
@@ -32,7 +43,8 @@ namespace cyclus {
   describing the behavior at the tick and tock as well as the behavior
   upon sending and receiving materials and messages.
   */
-class SourceFacility : public cyclus::FacilityModel  {
+class SourceFacility : public cyclus::FacilityModel, public cyclus::CommodityProducer {
+
   /* --------------------
    * all FACILITYMODEL classes have these members
    * --------------------
@@ -48,7 +60,8 @@ class SourceFacility : public cyclus::FacilityModel  {
     every model should be destructable
     */
   virtual ~SourceFacility();
-
+  virtual std::string schema();
+  virtual cyclus::Model* Clone();
   /**
     Initialize members related to derived module class
 
@@ -67,11 +80,6 @@ class SourceFacility : public cyclus::FacilityModel  {
   virtual std::string str();
 
   /**
-    Initializes a SourceFacility object by copying the members of another.
-    */
-  virtual cyclus::Model* Clone();
-
-  /**
     The handleTick function specific to the SourceFacility.
 
     @param time the time of the tick
@@ -85,17 +93,96 @@ class SourceFacility : public cyclus::FacilityModel  {
     */
   virtual void Tock(int time);
 
-  /* ------------------- */
+  /// @brief Responds to each request for this source facility's commodity.
+  /// If a given request is more than this facility's capacity, it will offer
+  /// its capacity.
+  virtual std::set<cyclus::BidPortfolio<cyclus::Material>::Ptr>
+      GetMatlBids(const cyclus::CommodMap<cyclus::Material>::type&
+                  commod_requests);
 
+  /// @brief respond to each trade with a material made from this facility's
+  /// recipe
+  ///
+  /// @param trades all trades in which this trader is the supplier
+  /// @param responses a container to populate with responses to each trade
+  virtual void GetMatlTrades(
+      const std::vector< cyclus::Trade<cyclus::Material> >& trades,
+      std::vector<std::pair<cyclus::Trade<cyclus::Material>,
+      cyclus::Material::Ptr> >& responses);
+  /* --- */
 
-  /* --------------------
-   * _THIS_ FACILITYMODEL class has these members
-   * --------------------
-   */
+  /* --- SourceFacility Members --- */
+  /**
+    @brief creates a material object to offer to a requester
+    @param target the material target a request desires
+    */
+  cyclus::Material::Ptr GetOffer(const cyclus::Material::Ptr target) const;
 
-  /* ------------------- */
+  /**
+    sets the output commodity name
+    @param name the commodity name
+    */
+  inline void commodity(std::string name) { out_commod_ = name; }
+
+  /// @return the output commodity
+  inline std::string commodity() const { return out_commod_; }
+
+  /**
+    sets the capacity of a material generated at any given time step
+    @param capacity the production capacity
+    */
+  inline void capacity(double capacity) {
+    capacity_ = capacity;
+    current_capacity_ = capacity_;
+  }
+
+  /// @return the production capacity at any given time step
+  inline double capacity() const { return capacity_; }
+
+  /**
+    sets the name of the recipe to be produced
+    @param name the recipe name
+    */
+  inline void recipe(std::string name) { recipe_name_ = name; }
+
+  /// @return the name of the output recipe
+  inline std::string recipe() const { return recipe_name_; }
+
+  /// @return the current timestep's capacity
+  inline double current_capacity() const { return current_capacity_; }
+
+ private:
+  /**
+    This facility has only one output commodity
+    */
+  std::string out_commod_;
+
+  /**
+    Name of the recipe this facility uses.
+    */
+  std::string recipe_name_;
+
+  /**
+    The capacity is defined in terms of the number of units of the
+    recipe that can be provided each time step.  A very large number
+    can be provided to represent infinte capacity.
+    */
+  double capacity_;
+
+  /**
+    The capacity at the current time step
+    */
+  double current_capacity_;
+
+  /**
+    The price that the facility will charge for its output commodity.
+    Units vary and are in dollars per inventory unit.
+    */
+  double commod_price_;
+  /* --- */
 };
 
 }  // namespace cyclus
 
 #endif  // CYCLUS_INTEGRATION_TESTS_MODELS_SOURCE_FACILITY_H_
+
