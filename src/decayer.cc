828a0a943dab40fc1e2f2da8f2d55272729c1287
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-22 19:50:34 -0600

    decayer.* : style corrections.

--- src/decayer.cc
+++ src/decayer.cc
@@ -17,7 +17,7 @@ DaughtersMap Decayer::daughters_ = DaughtersMap();
 Matrix Decayer::decay_matrix_ = Matrix();
 NucList Decayer::nuclides_tracked_ = NucList();
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Decayer::Decayer(const CompMap& comp) {
   if (!decay_info_loaded_) {
     Decayer::LoadDecayInfo();
@@ -32,7 +32,7 @@ Decayer::Decayer(const CompMap& comp) {
 
     // if the nuclide is tracked in the decay matrix
     if (parent_.count(nuc) > 0) {
-      int col = parent_[nuc].first; // get Vector position
+      int col = parent_[nuc].first;  // get Vector position
       pre_vect_(col, 1) = atom_count;
       // if it is not in the decay matrix, then it is added as a stable nuclide
     } else {
@@ -50,7 +50,7 @@ Decayer::Decayer(const CompMap& comp) {
   }
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void Decayer::LoadDecayInfo() {
   std::string path = Env::GetBuildPath() + "/share/decayInfo.dat";
   std::ifstream decayInfo(path.c_str());
@@ -62,7 +62,7 @@ void Decayer::LoadDecayInfo() {
   int jcol = 1;
   int nuc = 0;
   int nDaughters = 0;
-  double decayConst = 0; // decay constant, in inverse years
+  double decayConst = 0;  // decay constant, in inverse years
   double branchRatio = 0;
 
   decayInfo >> nuc;  // get first parent
@@ -109,14 +109,14 @@ void Decayer::LoadDecayInfo() {
     }
 
     daughters_[jcol] = temp;
-    ++jcol; // set next column
-    decayInfo >> nuc; // get next parent
+    ++jcol;  // set next column
+    decayInfo >> nuc;  // get next parent
     nuc *= 10000;  // put in id form
   }
   BuildDecayMatrix();
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 double Decayer::DecayConstant(int nuc) {
   if (!decay_info_loaded_) {
     Decayer::LoadDecayInfo();
@@ -128,23 +128,23 @@ double Decayer::DecayConstant(int nuc) {
   return 0;
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void Decayer::AddNucToList(int nuc) {
-  bool exists = (find(nuclides_tracked_.begin(), nuclides_tracked_.end(),
-                      nuc) != nuclides_tracked_.end());
+  bool exists = (find(nuclides_tracked_.begin(), nuclides_tracked_.end(), nuc)
+                 != nuclides_tracked_.end());
   if (!exists) {
     nuclides_tracked_.push_back(nuc);
   }
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void Decayer::GetResult(CompMap& comp) {
   // loops through the ParentMap and populates the passed CompMap with
   // the number density from the comp parameter for each nuclide
-  ParentMap::const_iterator parent_iter = parent_.begin(); // get first parent
+  ParentMap::const_iterator parent_iter = parent_.begin();  // get first parent
   while (parent_iter != parent_.end()) {
     int nuc = parent_iter->first;
-    int col = parent_.find(nuc)->second.first; // get Vector position
+    int col = parent_.find(nuc)->second.first;  // get Vector position
 
     // checks to see if the Vector position is valid
     if (col <= post_vect_.NumRows()) {
@@ -156,24 +156,24 @@ void Decayer::GetResult(CompMap& comp) {
     } else {
       LOG(LEV_ERROR, "none!") << "Decay Error - invalid Vector position";
     }
-    ++parent_iter; // get next parent
+    ++parent_iter;  // get next parent
   }
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void Decayer::BuildDecayMatrix() {
-  double decayConst = 0; // decay constant, in inverse years
+  double decayConst = 0;  // decay constant, in inverse years
   int jcol = 1;
   int n = parent_.size();
   decay_matrix_ = Matrix(n, n);
 
-  ParentMap::const_iterator parent_iter = parent_.begin(); // get first parent
+  ParentMap::const_iterator parent_iter = parent_.begin();  // get first parent
 
   // populates the decay matrix column by column
   while (parent_iter != parent_.end()) {
-    jcol = parent_iter->second.first; // determines column index
+    jcol = parent_iter->second.first;  // determines column index
     decayConst = parent_iter->second.second;
-    decay_matrix_(jcol, jcol) = -1 * decayConst; // sets A(i,i) value
+    decay_matrix_(jcol, jcol) = -1 * decayConst;  // sets A(i,i) value
 
     // processes the vector in the daughters map if it is not empty
     if (!daughters_.find(jcol)->second.empty()) {
@@ -185,22 +185,21 @@ void Decayer::BuildDecayMatrix() {
       // processes all daughters of the parent
       while (nuc_iter != daughters_.find(jcol)->second.end()) {
         int nuc = nuc_iter->first;
-        int irow = parent_.find(nuc)->second.first; // determines row index
+        int irow = parent_.find(nuc)->second.first;  // determines row index
         double branchRatio = nuc_iter->second;
-        decay_matrix_(irow, jcol) = branchRatio * decayConst; // sets A(i,j) value
+        decay_matrix_(irow, jcol) = branchRatio * decayConst;  // sets A(i,j) value
 
-        ++nuc_iter; // get next daughter
+        ++nuc_iter;  // get next daughter
       }
     }
-    ++parent_iter; // get next parent
+    ++parent_iter;  // get next parent
   }
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void Decayer::Decay(double years) {
   // solves the decay equation for the final composition
   post_vect_ = UniformTaylor::MatrixExpSolver(decay_matrix_, pre_vect_, years);
 }
 
-} // namespace cyclus
-
+}  // namespace cyclus
