a1de1245cfd895f89de8a0c7ea5514c126d2bd0c
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-24 04:59:52 -0600

    error.* : style corrections.

--- src/error.cc
+++ src/error.cc
@@ -5,13 +5,13 @@ namespace cyclus {
 
 const std::string Error::kPrefix("cyclus error: ");
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Error::Error(std::string msg) : msg_(msg) {}
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const char* Error::what() const throw() {
   std::string thrown = Error::kPrefix + msg_;
   return thrown.c_str();
 }
 
-} // namespace cyclus
+}  // namespace cyclus
