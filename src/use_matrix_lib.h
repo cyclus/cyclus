e1496ad9930e236ac5258331d3007d1602bdb46e
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-24 16:57:14 -0600

    use_matrix_lib.h and unix_helper_functions.h : style corrections.

--- src/use_matrix_lib.h
+++ src/use_matrix_lib.h
@@ -1,25 +1,25 @@
 // UseMatLib.h contains all of the declarations needed to use a specific
 // Matrix and/or Vector library in place of the default librares for the
 // decay calculations performed on a Material object.
-
-#ifndef USEMATLIB_H
-#define USEMATLIB_H
+#ifndef CYCLUS_SRC_USE_MATRIX_LIB_H_
+#define CYCLUS_SRC_USE_MATRIX_LIB_H_
 
 // To change the matrix library used:
 //
-//      #include "<Matrix Library>"
+// #include "<Matrix Library>"
 #include "l_matrix.h"
 
 namespace cyclus {
 
 // To change the matrix type:
 //
-//      typedef <Matrix Type> Matrix;
+// typedef <Matrix Type> Matrix;
 typedef LMatrix Matrix;
 
 // To change the vector type:
 //
-//      typedef <Vector Type> Vector;
+// typedef <Vector Type> Vector;
 typedef LMatrix Vector;
-} // namespace cyclus
-#endif
+
+}  // namespace cyclus
+#endif  // CYCLUS_SRC_USE_MATRIX_LIB_H_
