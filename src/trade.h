b09e46a0cbf93cd6fdf5d745e5557c427da39b89
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-25 13:06:15 -0600

    trade* : style corrections.

--- src/trade.h
+++ src/trade.h
@@ -1,5 +1,5 @@
-#ifndef CYCLUS_TRADE_H_
-#define CYCLUS_TRADE_H_
+#ifndef CYCLUS_SRC_TRADE_H_
+#define CYCLUS_SRC_TRADE_H_
 
 #include "bid.h"
 #include "request.h"
@@ -18,26 +18,25 @@ struct Trade {
   typename Bid<T>::Ptr bid;
   double amt;
   double price;
-  
-  Trade() : amt(0), price(0) {};
-  
+
+  Trade() : amt(0), price(0) {}
+
   Trade(typename Request<T>::Ptr request, typename Bid<T>::Ptr bid, double amt)
-    : request(request),
-      bid(bid),
-      amt(amt),
-      price(0) {};
+      : request(request),
+        bid(bid),
+        amt(amt),
+        price(0) {}
 };
 
 /// @brief Trade-Trade equality operator
 template<class T>
-bool operator==(const cyclus::Trade<T>& lhs,
-                const cyclus::Trade<T>& rhs) {
+bool operator==(const cyclus::Trade<T>& lhs, const cyclus::Trade<T>& rhs) {
   return  ((lhs.request == rhs.request) &&
            (lhs.bid == rhs.bid) &&
            (lhs.price == rhs.price) &&
            (lhs.amt == rhs.amt));
 }
 
-} // namespace cyclus
+}  // namespace cyclus
 
-#endif // ifndef CYCLUS_TRADE_H_
+#endif  // CYCLUS_SRC_TRADE_H_
