b51d6444c4b1b058c764d9e5977971210560d8da
gidden <matthew.gidden@gmail.com>
2014-02-27 23:53:00 -0600

    followed @rwcarlsen's suggestion and got an apparent work around

--- src/OsiCbcSolverInterface.cpp
+++ src/OsiCbcSolverInterface.cpp
@@ -1,12 +1,8 @@
-// #ifndef OsiCbcSolverInterface_CPP
-// #define OsiCbcSolverInterface_CPP
+#include "CbcConfig.h"
 
-#include "OsiCbcSolverInterface.hpp"
-
-#if CBC_VERSION_COMP
-#include "OsiCbcSolverInterface_2_8.cpp"
-#else
+// CBC_VERSION_MAJOR defined for Cbc > 2.5
+#ifndef CBC_VERSION_MAJOR
 #include "OsiCbcSolverInterface_2_5.cpp"
+#else
+#include "OsiCbcSolverInterface_2_8.cpp"
 #endif
-
-// #endif
--- src/OsiCbcSolverInterface.hpp
+++ src/OsiCbcSolverInterface.hpp
@@ -1,22 +1,13 @@
 #ifndef OsiCbcSolverInterface_HPP
 #define OsiCbcSolverInterface_HPP
 
-//#include <cstring>
-#include <stdio.h>
-#include <string.h>
-#include "version.h"
+#include "CbcConfig.h"
 
-// #define VERSION_COMPARE(A, B) ( A[0] >= B[0] && A[2] >= B[2] && A[4] >=B[4] )
-
-// #if VERSION_COMPARE(CBC_VERSION, "2.6.0")
-#ifdef CBC_VERSION
-//#define CBC_VERSION_COMP 
-//#if CBC_VERSION_COMP
-  #if strcmp(CBC_VERSION, "2.5.0")
-    #include "OsiCbcSolverInterface_2_8.hpp"
-  #else
-    #include "OsiCbcSolverInterface_2_5.hpp"
-  #endif
+// CBC_VERSION_MAJOR defined for Cbc > 2.5
+#ifndef CBC_VERSION_MAJOR
+#include "OsiCbcSolverInterface_2_5.hpp"
+#else
+#include "OsiCbcSolverInterface_2_8.hpp"
 #endif
 
 #endif
