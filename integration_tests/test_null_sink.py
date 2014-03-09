08260eef21278f779ffbea1d4daecb8fde5a8200
Olzhas <olzhasrakhimov7@gmail.com>
2014-03-08 18:17:43 -0600

    Tests are working.

--- integration_tests/test_null_sink.py
+++ integration_tests/test_null_sink.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-from nose.tools import assert_false
+from nose.tools import assert_false, assert_true, assert_equal, assert_not_equal
 import os
 import tables
 import numpy as np
@@ -8,20 +8,69 @@ from tools import check_cmd
 
 """ Tests """
 def test_null_sink():
-    """ Testing for null sink case without a source facility """
-    #Cyclus simulation input for null sink testing
+    """ Testing for null sink case without a source facility.
+    No transactions are expected in this test; therefore, a table with
+    transaction records must not exist in order to pass this test.
+    In addition, tests check if SimpleSink is deployed and decommissioned for
+    this simulation. This may be removed in future.
+    May also consider breaking this test into multiple tests.
+    """
+    # Cyclus simulation input for null sink testing
     sim_input = "./Inputs/null_sink.xml"
-    holdsrtn = [1] # needed because nose does not send() to test generator
+    holdsrtn = [1]  # needed because nose does not send() to test generator
     cmd = ["cyclus", "-o", "./output_temp.h5", "--input-file", sim_input]
     check_cmd(cmd, '.', holdsrtn)
     rtn = holdsrtn[0]
     if rtn != 0:
-        return # don't execute further commands
+        return  # don't execute further commands
 
     output = tables.open_file("./output_temp.h5", mode = "r")
-    paths = ["/Transactions", "/TransactedResources"] # this must contain tables to test
-    #No resource exchange is expected
-    for path in paths:
+    legal_paths = ["/AgentEntry", "/AgentExit", "/Info"]
+    illegal_paths = ["/Transactions"]  # this must contain tables to test
+    # Check if these tables exist
+    tables_there = True
+    for path in legal_paths:
+        yield assert_true, output.__contains__(path)
+        if tables_there and not output.__contains__(path):
+            tables_there = False
+
+    if not tables_there:
+        output.close()
+        os.remove("./output_temp.h5")
+        return
+
+    # Get specific data
+    agent_entry = output.get_node("/AgentEntry")[:]
+    agent_exit = output.get_node("/AgentExit")[:]
+    info = output.get_node("/Info")[:]
+
+    # SimpleSink's deployment and decommissioning
+    agent_ids = agent_entry["AgentId"]
+    agent_impl = agent_entry["Implementation"]
+    duration = info["Duration"][0]
+
+    sink_id = 0
+    i = 0
+    for agent in agent_impl:
+        if agent == "SimpleSink":
+            sink_id = agent_ids[i]
+            break
+        else:
+            i += 1
+    # Test if SimpleSink is actually deployed
+    yield assert_not_equal, sink_id, 0
+
+    # Test if SimpleSink is decommissioned at the end of the simulation
+    i = 0
+    for index in agent_exit["AgentId"]:
+        if index == sink_id:
+            yield assert_equal, duration, agent_exit["ExitTime"][i]
+            break
+        else:
+            i += 1
+
+    # No resource exchange is expected
+    for path in illegal_paths:
         yield assert_false, output.__contains__(path)
 
     output.close()
--- integration_tests/test_source_to_sink.py
+++ integration_tests/test_source_to_sink.py
@@ -9,21 +9,28 @@ from tools import check_cmd
 
 """ Tests """
 def test_source_to_sink():
-    """ Tests linear growth of sink inventory """
-    #Cyclus simulation input for source_to_sink
+    """ Tests linear growth of sink inventory by checking if the transactions
+    were of equal quantities and only between these two facilities.
+    In future, may eliminate checks if needed tables exist, and rely on errors.
+    In future, may eliminate tests for the existance and uniqueness of the
+    facilities. In addition, may eliminate other non-integration testing relevant
+    code and tests.
+    """
+    # Cyclus simulation input for source_to_sink
     sim_inputs = ["./Inputs/source_to_sink.xml"]
 
     for sim_input in sim_inputs:
-        holdsrtn = [1] # needed because nose does not send() to test generator
+        holdsrtn = [1]  # needed because nose does not send() to test generator
         cmd = ["cyclus", "-o", "./output_temp.h5", "--input-file", sim_input]
         check_cmd(cmd, '.', holdsrtn)
         rtn = holdsrtn[0]
         if rtn != 0:
-            return # don't execute further commands
+            return  # don't execute further commands
 
         output = tables.open_file("./output_temp.h5", mode = "r")
         # tables of interest
-        paths = ["/Agents", "/Resources", "/Transactions","/TransactedResources"]
+        paths = ["/AgentEntry", "/AgentExit", "/Resources", "/Transactions",
+                "/Info"]
         # Check if these tables exist
         tables_there = True
         for path in paths:
@@ -38,33 +45,35 @@ def test_source_to_sink():
             return
 
         # Get specific tables and columns
-        agents = output.get_node("/Agents")[:]
+        agent_entry = output.get_node("/AgentEntry")[:]
+        agent_exit = output.get_node("/AgentExit")[:]
+        info = output.get_node("/Info")[:]
         resources = output.get_node("/Resources")[:]
         transactions = output.get_node("/Transactions")[:]
-        transacted_res = output.get_node("/TransactedResources")[:]
 
         # Find agent ids of source and sink facilities
-        agent_ids = agents["ID"]
-        agent_prototypes = agents["Prototype"]
-        source_index = []
-        sink_index = []
+        agent_ids = agent_entry["AgentId"]
+        agent_impl = agent_entry["Implementation"]
+        duration = info["Duration"][0]
+        source_id = []
+        sink_id = []
         i = 0
-        for prototype in agent_prototypes:
-            if prototype == "Source":
-                source_index.append(i)
-            elif prototype == "Sink":
-                sink_index.append(i)
+        for impl in agent_impl:
+            if impl == "SimpleSource":
+                source_id.append(i)
+            elif impl == "SimpleSink":
+                sink_id.append(i)
             i += 1
         # Test for only one source and one sink
-        yield assert_equal, len(source_index), 1
-        yield assert_equal, len(sink_index), 1
+        yield assert_equal, len(source_id), 1
+        yield assert_equal, len(sink_id), 1
         # Get ids of the source and sink
-        source_id = agent_ids[source_index[0]]
-        sink_id = agent_ids[sink_index[0]]
+        source_id = agent_ids[source_id[0]]
+        sink_id = agent_ids[sink_id[0]]
 
         # Check if transactions are only between source and sink
-        sender_ids = transactions["SenderID"]
-        receiver_ids = transactions["ReceiverID"]
+        sender_ids = transactions["SenderId"]
+        receiver_ids = transactions["ReceiverId"]
         expected_sender_array = np.empty(sender_ids.size)
         expected_sender_array.fill(source_id)
         expected_receiver_array = np.empty(receiver_ids.size)
@@ -73,24 +82,17 @@ def test_source_to_sink():
         yield assert_array_equal, receiver_ids, expected_receiver_array
 
         # Transaction ids must be equal range from 1 to the number of rows
-        # from both Transactions and TransactedResources tables
-        expected_trans_ids = np.arange(1, sender_ids.size + 1, 1)
-        yield assert_array_equal, transactions["ID"], expected_trans_ids
-        yield assert_array_equal, transacted_res["TransactionID"], expected_trans_ids
+        expected_trans_ids = np.arange(0, sender_ids.size, 1)
+        yield assert_array_equal, transactions["TransactionId"], expected_trans_ids
 
         # Track transacted resources
-        trans_resource_ids = transacted_res["ResourceID"]
+        resource_ids = resources["ResourceId"]
         quantities = resources["Quantity"]
-        transacted_quantities = np.zeros(trans_resource_ids.size)
-        expected_quantities = np.empty(trans_resource_ids.size)
+        expected_quantities = np.empty(resource_ids.size)
         # Expect that every transaction quantity is the same amount
-        expected_quantities.fill(quantities[trans_resource_ids[0] - 1])
-        j = 0
-        for id in trans_resource_ids:
-            transacted_quantities[j] = quantities[id-1]
-            j += 1
+        expected_quantities.fill(quantities[0])
 
-        yield assert_array_equal, transacted_quantities, expected_quantities
+        yield assert_array_equal, quantities, expected_quantities
 
         output.close()
         os.remove("./output_temp.h5")
