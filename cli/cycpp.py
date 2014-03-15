c1458c159adfff6835a46d8e8d0b17fe3af5a927
Anthony Scopatz <scopatz@gmail.com>
2014-03-15 15:42:00 -0500

    finished pass 1

--- cli/cycpp.py
+++ cli/cycpp.py
@@ -17,11 +17,53 @@ The three passes of cycpp are:
 cycpp is implemented entirely in this file and with tools from the Python standard
 library. It requires Python 2.7+ or Python 3.3+ to run.
 """
+from __future__ import print_function
+import os
+from collections import Sequence
+from subprocess import Popen, PIPE
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
+def preprocess_file(filename, cpp_path='cpp', cpp_args=('-xc++', '-pipe')):
+    """Preprocess a file using cpp.
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file you want to preprocess.
+    cpp_path : str, optional
+    cpp_args : str, optional
+        Refer to the documentation of parse_file for the meaning of these arguments.
+
+    Notes
+    -----
+    This was forked from pycparser: https://github.com/eliben/pycparser
+    """
+    path_list = [cpp_path]
+    if isinstance(cpp_args, Sequence):
+        path_list += cpp_args
+    elif cpp_args != '':
+        path_list += [cpp_args]
+    path_list += [filename]
+    try:
+        # Note the use of universal_newlines to treat all newlines
+        # as \n for Python's purpose
+        pipe = Popen(path_list, stdout=PIPE, universal_newlines=True)
+        text = pipe.communicate()[0]
+    except OSError as e:
+        raise RuntimeError(("Unable to invoke 'cpp'.  Make sure its path was passed "
+                            "correctly\nOriginal error: {0}").format(e))
+    return text
 
 def main():
-    pass
+    parser = ArgumentParser(prog="cycpp", description=__doc__, 
+                            formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('path', help="path to source file")
+    ns = parser.parse_args()
+
+    canon = preprocess_file(ns.path)  # pass 1
+    print(canon)
 
 if __name__ == "__main__":
     main()
+
 
