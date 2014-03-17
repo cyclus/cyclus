fb4c7efcd9a98799970f8a9e54787d8edfc2ba1a
Olzhas <olzhasrakhimov7@gmail.com>
2014-03-17 00:35:06 -0500

    Minimal cycle tests are functioning.

--- integration_tests/test_minimal_cycle.py
+++ integration_tests/test_minimal_cycle.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-from nose.tools import assert_equal, assert_true
+from nose.tools import assert_equal, assert_almost_equal, assert_true
 from numpy.testing import assert_array_equal
 import os
 import tables
@@ -30,22 +30,23 @@ def change_k_factors(fs_read, fs_write, k_factor_in, k_factor_out, n = 1):
     k_in_changed = False
     i = 0  # to tract a number of sets
 
-    while i != n:
-        for f in fs_read:
-            if f.count("k_factor_in"):
-                assert(not k_in_changed)
-                f = f.split("<")[0] + "<k_factor_in>" + str(k_factor_in) + \
-                    "</k_factor_in>\n"
-                k_in_changed = True
+    for f in fs_read:
+        if f.count("k_factor_in"):
+            assert(not k_in_changed)
+            f = f.split("<")[0] + "<k_factor_in>" + str(k_factor_in) + \
+                "</k_factor_in>\n"
+            k_in_changed = True
 
-            elif f.count("k_factor_out"):
-                assert(k_in_changed)
-                f = f.split("<")[0] + "<k_factor_out>" + str(k_factor_out) + \
-                    "</k_factor_out>\n"
-                k_in_changed = False
-                i += 1
+        elif f.count("k_factor_out"):
+            assert(k_in_changed)
+            f = f.split("<")[0] + "<k_factor_out>" + str(k_factor_out) + \
+                "</k_factor_out>\n"
+            k_in_changed = False
+            i += 1
 
-            fs_write.write(f)
+        fs_write.write(f)
+        if i == n: break
+    return
 
 def create_sim_input(ref_input, k_factor_in, k_factor_out):
     """Creates xml input file from a reference xml input file.
@@ -91,94 +92,142 @@ def change_minimal_input(ref_input, k_factor_a, k_factor_b):
     fw = open(fw_path, "w")
     fr = open(ref_input, "r")
     # change k_factors for the first facility
-    change_k_factors(fr, fw, k_factor_a, k_factor_b)
-    # reverse change k_factors for the second facility
     change_k_factors(fr, fw, k_factor_b, k_factor_a)
+    # reverse change k_factors for the second facility
+    change_k_factors(fr, fw, k_factor_a, k_factor_b)
     # write the rest of the file
-    fw.write(fr.read())
+    for f in fr:
+        fw.write(f)
 
-""" Tests """
-def tset_minimal_cycle():
-    """ Tests simulations with two facilities with different conversion factors.
+    # Closing open files
+    fr.close()
+    fw.close()
+
+    return fw_path
+
+"""Tests"""
+def test_minimal_cycle():
+    """Tests simulations with two facilities with several conversion factors.
+
+    The commodities of the facilities are different. Facility A offers a
+    commodity A and requests commodity B; whereas, Facility B offers a
+    commodity B and requests commodity A. In addition, it is also assumed that
+    the first requests and offers are the same quantities for respective
+    receivers and senders.
     """
-    # Cyclus simulation input for source_to_sink
+    # Cyclus simulation input for minimal cycle with different commodities
     ref_input = "./Inputs/minimal_cycle.xml"
-    # Conversion factors for the three simulations
-    k_factors = [0.5, 1, 2]
+    # Conversion factors for the simulations
+    k_factors = [0.95, 1, 2]
 
-    for k_factor in k_factors:
-        sim_input = create_sim_input(ref_input, k_factor)
+    for k_factor_a in k_factors:
+        for k_factor_b in k_factors:
+            sim_input = change_minimal_input(ref_input, k_factor_a, k_factor_b)
 
-        holdsrtn = [1]  # needed because nose does not send() to test generator
-        cmd = ["cyclus", "-o", "./output_temp.h5", "--input-file", sim_input]
-        check_cmd(cmd, '.', holdsrtn)
-        rtn = holdsrtn[0]
-        if rtn != 0:
-            return  # don't execute further commands
+            holdsrtn = [1]  # needed because nose does not send() to test generator
+            cmd = ["cyclus", "-o", "./output_temp.h5", "--input-file", sim_input]
+            yield check_cmd, cmd, '.', holdsrtn
+            rtn = holdsrtn[0]
+            if rtn != 0:
+                return  # don't execute further commands
 
-        output = tables.open_file("./output_temp.h5", mode = "r")
-        # tables of interest
-        paths = ["/AgentEntry", "/AgentExit", "/Resources", "/Transactions",
-                "/Info"]
-        # Check if these tables exist
-        tables_there = True
-        for path in paths:
-            yield assert_true, output.__contains__(path)
-            # Have to stop further operations after these tests
-            if tables_there and not output.__contains__(path):
-                tables_there = False
+            output = tables.open_file("./output_temp.h5", mode = "r")
+            # tables of interest
+            paths = ["/AgentEntry", "/AgentExit", "/Resources", "/Transactions",
+                    "/Info"]
+            # Check if these tables exist
+            yield assert_true, table_exist(output, paths)
+            if not table_exist(output, paths):
+                output.close()
+                os.remove("./output_temp.h5")
+                return  # don't execute further commands
 
-        if not tables_there:
+            # Get specific tables and columns
+            agent_entry = output.get_node("/AgentEntry")[:]
+            agent_exit = output.get_node("/AgentExit")[:]
+            info = output.get_node("/Info")[:]
+            resources = output.get_node("/Resources")[:]
+            transactions = output.get_node("/Transactions")[:]
+
+            # Find agent ids
+            agent_ids = agent_entry["AgentId"]
+            agent_impl = agent_entry["Implementation"]
+            agent_protos = agent_entry["Prototype"]
+            duration = info["Duration"][0]
+
+            facility_id = find_ids("KFacility", agent_impl, agent_ids)
+            # Test for two KFacility
+            yield assert_equal, len(facility_id), 2
+
+            # Test for one Facility A and Facility B
+            facility_a = find_ids("FacilityA", agent_protos, agent_ids)
+            facility_b = find_ids("FacilityB", agent_protos, agent_ids)
+            yield assert_equal, len(facility_a), 1
+            yield assert_equal, len(facility_b), 1
+
+            # Test if both facilities are KFracility
+            # Assume FacilityA is deployed first according to the schema
+            yield assert_equal, facility_a[0], facility_id[0]
+            yield assert_equal, facility_b[0], facility_id[1]
+
+            # Test if the agents exit at the end of the simulation
+            yield assert_equal, duration, exit_times(facility_a[0], agent_exit)
+            yield assert_equal, duration, exit_times(facility_b[0], agent_exit)
+
+            # Test if the transactions are strictly between Facility A and
+            # Facility B. There are no Facility A to Facility A or vice versa.
+            sender_ids = transactions["SenderId"]
+            receiver_ids = transactions["ReceiverId"]
+            pattern_one = np.arange(0, sender_ids.size, 2)
+            pattern_two = np.arange(1, sender_ids.size, 2)
+            pattern_a = pattern_one  # expected pattern for Facility A as a sender
+            pattern_b = pattern_two  # expected pattern for Facility B as a sender
+            print pattern_a
+            print np.where(sender_ids == facility_a[0])
+            # Re-assign in case the expected patterns are incorrect
+            if sender_ids[0] == facility_b[0]:
+                pattern_a = pattern_two
+                pattern_b = pattern_one
+
+            yield assert_array_equal, np.where(sender_ids == facility_a[0])[0], \
+                pattern_a
+            yield assert_array_equal, np.where(receiver_ids == facility_a[0])[0], \
+                pattern_b  # reverse pattern when acted as a receiver
+
+            yield assert_array_equal, np.where(sender_ids == facility_b[0])[0], \
+                pattern_b
+            yield assert_array_equal, np.where(receiver_ids == facility_b[0])[0], \
+                pattern_a  # reverse pattern when acted as a receiver
+
+            # Transaction ids must be equal range from 1 to the number of rows
+            expected_trans_ids = np.arange(sender_ids.size)
+            yield assert_array_equal, transactions["TransactionId"], expected_trans_ids
+
+            # When k_factors are very low and the simulation time is big
+            # the number of transactions maybe shortened due to the lower
+            # limit cyclus::eps() for transaction amounts.
+            # Expect not to have shortened transactions, so for two facilities,
+            # there must be (2 * duration) number of transactions.
+            yield assert_equal, sender_ids.size, 2 * duration
+
+            # Track transacted resources
+            quantities = resources["Quantity"]
+
+            # Almost equal cases due to floating point k_factors
+            init_capacity_a = quantities[0]
+            init_capacity_b = quantities[1]
+            j = 0
+            for p in pattern_a:
+                yield assert_almost_equal, quantities[p], \
+                    init_capacity_a * k_factor_a ** j
+                j += 1
+
+            j = 0
+            for p in pattern_b:
+                yield assert_almost_equal, quantities[p], \
+                    init_capacity_b * k_factor_b ** j
+                j += 1
+
             output.close()
             os.remove("./output_temp.h5")
             os.remove(sim_input)
-            return
-
-        # Get specific tables and columns
-        agent_entry = output.get_node("/AgentEntry")[:]
-        agent_exit = output.get_node("/AgentExit")[:]
-        info = output.get_node("/Info")[:]
-        resources = output.get_node("/Resources")[:]
-        transactions = output.get_node("/Transactions")[:]
-
-        # Find agent ids
-        agent_ids = agent_entry["AgentId"]
-        agent_impl = agent_entry["Implementation"]
-        duration = info["Duration"][0]
-        facility_id = []
-        i = 0
-        for impl in agent_impl:
-            if impl == "KFacility":
-                facility_id.append(i)
-            i += 1
-        yield assert_equal, len(facility_id), 1
-        facility_id = agent_ids[facility_id[0]]
-
-        sender_ids = transactions["SenderId"]
-        receiver_ids = transactions["ReceiverId"]
-        expected_sender_array = np.empty(sender_ids.size)
-        expected_sender_array.fill(facility_id)
-        expected_receiver_array = np.empty(receiver_ids.size)
-        expected_receiver_array.fill(facility_id)
-        yield assert_array_equal, sender_ids, expected_sender_array
-        yield assert_array_equal, receiver_ids, expected_receiver_array
-
-        # Transaction ids must be equal range from 1 to the number of rows
-        expected_trans_ids = np.arange(0, sender_ids.size, 1)
-        yield assert_array_equal, transactions["TransactionId"], expected_trans_ids
-
-        # Track transacted resources
-        resource_ids = resources["ResourceId"]
-        quantities = resources["Quantity"]
-        expected_quantities = np.empty(resource_ids.size)
-        # Expect that every transaction quantity is the same amount
-        initial_inv = quantities[0]
-        for i in range(expected_quantities.size):
-            expected_quantities[i] = initial_inv * k_factor ** i
-
-        # Should find a logic for almost equal cases
-        yield assert_array_equal, quantities, expected_quantities
-
-        output.close()
-        os.remove("./output_temp.h5")
-        os.remove(sim_input)
