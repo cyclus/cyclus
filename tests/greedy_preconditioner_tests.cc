60efeb34ddb63cc65abeab9577d342522a232158
gidden <matthew.gidden@gmail.com>
2014-04-07 15:10:16 -0500

    added a test

--- tests/greedy_preconditioner_tests.cc
+++ tests/greedy_preconditioner_tests.cc
@@ -3,13 +3,38 @@
 #include "exchange_graph.h"
 
 #include "greedy_preconditioner.h"
+#include "greedy_solver.h"
 
 using cyclus::Arc;
+using cyclus::AvgPrefComp;
 using cyclus::ExchangeNode;
 using cyclus::ExchangeNodeGroup;
 using cyclus::ExchangeGraph;
 using cyclus::GreedyPreconditioner;
 using cyclus::RequestGroup;
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(ConditionerTests, AvgPref) {
+  ExchangeNode::Ptr u1(new ExchangeNode());
+  ExchangeNode::Ptr u2(new ExchangeNode());
+  ExchangeNode::Ptr v(new ExchangeNode());
+
+  Arc a1(u1, v);
+  Arc a2(u2, v);
+
+  u1->prefs[a1] = 1;
+  u2->prefs[a2] = 2;
+
+  std::vector<ExchangeNode::Ptr> nodes;
+  nodes.push_back(u1);
+  nodes.push_back(u2);
+
+  EXPECT_EQ(nodes[0], u1);
+  EXPECT_EQ(nodes[1], u2);
+  std::sort(nodes.begin(), nodes.end(), AvgPrefComp);
+  EXPECT_EQ(nodes[0], u2);
+  EXPECT_EQ(nodes[1], u1);
+}
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST(ConditionerTests, Conditioning) {
