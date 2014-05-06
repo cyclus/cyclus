e5d195123e10fb805c5fcf51c73708b06aa84292
Matthew Gidden <matthew.gidden@gmail.com>
2014-05-06 16:03:49 -0500

    added sim run to gen plot script

--- gen_plot.sh
+++ gen_plot.sh
@@ -1,4 +1,6 @@
-rm prey; rm pred;
+rm plot.png; rm prey; rm pred; rm cyclus.sqlite;
+cyclus ./tests/Inputs/lotka_volterra.xml -v LEV_INFO2
+cycpost cyclus.sqlite
 metric -db=cyclus.sqlite deployseries Predator > pred
 metric -db=cyclus.sqlite deployseries Prey > prey
 ./plot.p
