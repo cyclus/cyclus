2db6fd85e6620767f58c14617b95e6dd93fb708c
Matthew Gidden <matthew.gidden@gmail.com>
2014-05-08 11:04:02 -0500

    only adds arc if preference is non-negative

--- src/exchange_translator.h
+++ src/exchange_translator.h
@@ -62,23 +62,26 @@ class ExchangeTranslator {
       typename std::set<typename Bid<T>::Ptr>::const_iterator b_it;
       for (b_it = bids.begin(); b_it != bids.end(); ++b_it) {
         typename Bid<T>::Ptr bid = *b_it;
-
-        // get translated arc
-        Arc a = TranslateArc(xlation_ctx_, bid);
-        
-        // add unode's preference for this arc
         typename Request<T>::Ptr req = bid->request();
         double pref =
             ex_ctx_->trader_prefs.at(req->requester())[req][bid];
-        a.unode()->prefs[a] = pref; // request node is a.unode()
-        int n_prefs = a.unode()->prefs.size();
         
-        CLOG(LEV_DEBUG5) << "Updating preference for one of "
-                         << req->requester()->manager()->prototype()
-                         << "'s trade nodes:";
-        CLOG(LEV_DEBUG5) << "   preference: " << a.unode()->prefs[a];
-            
-        graph->AddArc(a);
+        if (pref < 0) {
+          CLOG(LEV_DEBUG1) << "Removing arc because of negative preference.";
+        } else {
+          // get translated arc
+          Arc a = TranslateArc(xlation_ctx_, bid);
+          
+          a.unode()->prefs[a] = pref; // request node is a.unode()
+          int n_prefs = a.unode()->prefs.size();
+          
+          CLOG(LEV_DEBUG5) << "Updating preference for one of "
+                           << req->requester()->manager()->prototype()
+                           << "'s trade nodes:";
+          CLOG(LEV_DEBUG5) << "   preference: " << a.unode()->prefs[a];
+          
+          graph->AddArc(a);
+        }
       }
     }
     
