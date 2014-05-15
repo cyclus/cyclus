a8b02382ba89273637b1af8ecc0fbbf1c49d3e4d
Matthew Gidden <matthew.gidden@gmail.com>
2014-05-15 09:57:19 -0500

    removed utility file

--- gen_plot.sh
+++
@@ -1,6 +0,0 @@
-rm plot.png; rm prey; rm pred; rm cyclus.sqlite;
-cyclus ./tests/input/lotka_volterra.xml -v LEV_INFO2 &&
-cycpost cyclus.sqlite && 
-metric -db=cyclus.sqlite deployseries Predator > pred &&
-metric -db=cyclus.sqlite deployseries Prey > prey &&
-./plot.p
