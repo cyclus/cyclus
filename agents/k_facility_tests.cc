6d658bc24a4d9df2011f9a70a17734a07e322a7e
Matthew Gidden <matthew.gidden@gmail.com>
2014-05-23 13:42:26 -0500

    removed extraneous tests

--- agents/k_facility_tests.cc
+++ agents/k_facility_tests.cc
@@ -24,45 +24,13 @@ class KFacilityTest : public ::testing::Test {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(KFacilityTest, clone) {
   KFacility* cloned_fac = dynamic_cast<KFacility*> (src_facility_->Clone());
+  delete cloned_fac;
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(KFacilityTest, InitialState) {
-  // Test things about the initial state of the facility here
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(KFacilityTest, Print) {
-  EXPECT_NO_THROW(std::string s = src_facility_->str());
-  // Test KFacility specific aspects of the print method here
-}
-
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(KFacilityTest, ReceiveMessage) {
-  // Test KFacility specific behaviors of the ReceiveMessage function here
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(KFacilityTest, Tick) {
-  int time = 1;
-  ASSERT_NO_THROW(src_facility_->Tick(time));
-  // Test KFacility specific behaviors of the Tick function here
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(KFacilityTest, Tock) {
-  int time = 1;
-  EXPECT_NO_THROW(src_facility_->Tock(time));
-  // Test KFacility specific behaviors of the Tock function here
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cyclus::Agent* KFacilityConstructor(cyclus::Context* ctx) {
   return dynamic_cast<cyclus::Agent*>(new KFacility(ctx));
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 INSTANTIATE_TEST_CASE_P(KFac, FacilityTests,
                         ::testing::Values(&KFacilityConstructor));
 
