0e539a4c58342b38e873d845c280829c7bb49bba
Matthew Gidden <matthew.gidden@gmail.com>
2014-05-23 14:00:43 -0500

    adds the agent test connection to test agents just in case of future application

--- tests/test_modules/test_inst.cc
+++ tests/test_modules/test_inst.cc
@@ -3,3 +3,10 @@
 extern "C" cyclus::Agent* ConstructTestInst(cyclus::Context* ctx) {
   return new TestInst(ctx);
 }
+
+// required to get functionality in cyclus agent unit tests library
+#ifndef CYCLUS_AGENT_TESTS_CONNECTED
+int ConnectAgentTests();
+static int cyclus_agent_tests_connected = ConnectAgentTests();
+#define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
+#endif // CYCLUS_AGENT_TESTS_CONNECTED
