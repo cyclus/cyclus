a7f077bf308e5d1cd502290cab10e588501ce800
Anthony Scopatz <scopatz@gmail.com>
2014-10-24 11:13:53 -0500

    Added warnings to stubs to not change dynamic loader

--- stubs/stub_inst.cc
+++ stubs/stub_inst.cc
@@ -13,7 +13,9 @@ std::string StubInst::str() {
   return Institution::str();
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// WARNING! Do not change the following this function!!! This enables your
+// archetype to be dynamically loaded and any alterations will cause your
+// archetype to fail.
 extern "C" cyclus::Agent* ConstructStubInst(cyclus::Context* ctx) {
   return new StubInst(ctx);
 }
