9942150e2c2b755c301a46b7d81c1adf17d64454
Matthew Gidden <matthew.gidden@gmail.com>
2015-01-19 11:15:24 -0600

    more robust find lib cyclus behavior in test abi

--- tests/test_abi.py
+++ tests/test_abi.py
@@ -17,18 +17,34 @@ try:
 except ImportError:
     smbchk = None
 
+def find_libcyc():
+    libcyc = os.path.join(cycdir, 'build', 'lib', 'libcyclus.so')
+    if os.path.exists(libcyc):
+        return libcyc
+
+    cmd = "find / -type f -name libcyclus.so -executable 2>/dev/null"
+    try:
+        output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+
+    print('output:', output)
+    return output.split('\n')[0]
+
 def test_abi_stability():
     if smbchk is None:
         raise SkipTest('Could not import smbchk!')
     if os.name != 'posix':
         raise SkipTest('can only check for ABI stability on posix systems.')
-    libcyc = os.path.join(cycdir, 'build', 'lib', 'libcyclus.so')
+    libcyc = find_libcyc()
     if not os.path.exists(libcyc):
         raise SkipTest('libcyclus could not be found, '
                        'cannot check for ABI stability')
-    args = '--update -t HEAD --no-save --check'.split()
+    prefix = os.path.join(os.path.dirname(libcyc), '..')
+    args = '--update -t HEAD --no-save --check --prefix {}'.format(prefix).split()
     with tools.indir(reldir):
         obs = smbchk.main(args=args)
+    print('obs ' + obs)
     assert_true(obs)
 
 if __name__ == "__main__":
