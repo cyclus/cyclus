7f243c88c7cad79826cda9e81d8d324e018bdcb8
Matthew Gidden <matthew.gidden@gmail.com>
2015-03-20 11:35:01 -0500

    finished cbc tests and added comments

--- tests/exchange_solver_tests.cc
+++ tests/exchange_solver_tests.cc
@@ -25,6 +25,13 @@ TEST(ExSolverTests, Interface) {
   EXPECT_EQ(2, s.i);
 }
 
+// Generates a very simple graph to test less-than constraints for requesters
+//
+// The graph is comprised of a single request node and a single bid node. The
+// request is made for 1 unit of resource.
+//
+// Based on consumer_lteq, a constraint is added either by the consumer or the
+// supplier. The same flow is expected in both cases.
 ExchangeGraph* gen(bool consumer_lteq) {
   ExchangeNode::Ptr u(new ExchangeNode());
   ExchangeNode::Ptr v(new ExchangeNode());
@@ -106,6 +113,18 @@ TEST(ExSolverTests, CustomConstraintsProg) {
   delete h;  
 }
 
+// This generates a slightly more complicated graph comprised of a single
+// requester with two bidders.
+//
+// Solvers can turn on and off exclusive trades; both cases are tested.
+//
+// There is a preference differential, with p1 >> p2. Without any LTEQ
+// constraints, the first arc is expected to be satisfied in all cases.
+//
+// A LTEQ is optionally added, where the first arc is constrained, i.e., a_1 >
+// a_2. In the non-exclusive case, the first arc is still met to its maximum
+// value. However, in the exclusive case, the first arc can not meet the total
+// demand, therefore the second arc is engaged.
 ExchangeGraph* gen2(bool consumer_lteq) {
   bool excl = true;
   ExchangeNode::Ptr u(new ExchangeNode(1, excl));
@@ -117,17 +136,13 @@ ExchangeGraph* gen2(bool consumer_lteq) {
   Arc a2(u2, w);
 
   u->unit_capacities[a].push_back(1);
-  u->prefs[a] = 1;
+  u->prefs[a] = 5;
   u2->unit_capacities[a2].push_back(1);
   u2->prefs[a2] = 0.5;
 
   RequestGroup::Ptr gu(new RequestGroup(1));
   gu->AddExchangeNode(u);
   gu->AddExchangeNode(u2);
-  // std::vector<ExchangeNode::Ptr> excl_grp;
-  // excl_grp.push_back(u);
-  // excl_grp.push_back(u2);
-  // gu->AddExclGroup(excl_grp);
   gu->AddCapacity(1);
 
   if (consumer_lteq) {
@@ -138,12 +153,8 @@ ExchangeGraph* gen2(bool consumer_lteq) {
 
   ExchangeNodeGroup::Ptr gv(new ExchangeNodeGroup());
   gv->AddExchangeNode(v);
-  // v->unit_capacities[a].push_back(1);
-  // gv->AddCapacity(1.0);
   ExchangeNodeGroup::Ptr gw(new ExchangeNodeGroup());
   gw->AddExchangeNode(w);
-  // w->unit_capacities[a2].push_back(1);
-  // gw->AddCapacity(1.0);
   
   ExchangeGraph* g = new ExchangeGraph();
   g->AddRequestGroup(gu);
@@ -156,9 +167,10 @@ ExchangeGraph* gen2(bool consumer_lteq) {
 
 
 TEST(ExSolverTests, CustomConstraintsGreedy2) {
-  bool consumer_lteq;
+  // test exclusive trades and non-exclusive trades
   GreedySolver greedy_nex(false);
   GreedySolver greedy_ex(true);
+  bool consumer_lteq;
   
   // test supplier and consumer constraints
   consumer_lteq = false;
@@ -194,29 +206,41 @@ TEST(ExSolverTests, CustomConstraintsGreedy2) {
 }
 
 TEST(ExSolverTests, CustomConstraintsProg2) {
+  // test exclusive trades and non-exclusive trades
   ProgSolver cbc_nex("cbc");
+  ProgSolver cbc_ex("cbc", true);
   bool consumer_lteq;
   
   // test supplier and consumer constraints
   consumer_lteq = false;
   ExchangeGraph* g = gen2(consumer_lteq);
-  Match exp_g = Match(g->arcs().at(0), 1.0);
+  Match exp_gnex = Match(g->arcs().at(0), 1.0);
   cbc_nex.graph(g);
   cbc_nex.Solve();
   ASSERT_EQ(g->matches().size(), 1);
-  EXPECT_EQ(exp_g, g->matches().at(0));
+  EXPECT_EQ(exp_gnex, g->matches().at(0));
   g->ClearMatches();
+  Match exp_gex = Match(g->arcs().at(0), 1.);
+  cbc_ex.graph(g);
+  cbc_ex.Solve();
+  ASSERT_EQ(g->matches().size(), 1);
+  EXPECT_EQ(exp_gex, g->matches().at(0));
   delete g;
 
   // test consumer with LTEQ constraints
   consumer_lteq = true;
   ExchangeGraph* h = gen2(consumer_lteq);
-  Match exp_h = Match(h->arcs().at(0), 0.5);
+  Match exp_hnex = Match(h->arcs().at(0), 0.5);
   cbc_nex.graph(h);
   cbc_nex.Solve();
   ASSERT_EQ(h->matches().size(), 1);
-  EXPECT_EQ(exp_h, h->matches().at(0));
+  EXPECT_EQ(exp_hnex, h->matches().at(0));
   h->ClearMatches();
+  Match exp_hex = Match(h->arcs().at(1), 1.);
+  cbc_ex.graph(h);
+  cbc_ex.Solve();
+  ASSERT_EQ(h->matches().size(), 1);
+  EXPECT_EQ(exp_hex, h->matches().at(0));
   delete h;  
 }
 
