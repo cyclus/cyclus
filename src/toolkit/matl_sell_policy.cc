4ccade9ea5ae08359775a50d7ff77fe57373c6bd
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-14 11:44:45 -0500

    streamlining some implementation

--- src/toolkit/matl_sell_policy.cc
+++ src/toolkit/matl_sell_policy.cc
@@ -17,11 +17,12 @@ MatlSellPolicy::MatlSellPolicy() : Trader(NULL) {
 MatlSellPolicy::~MatlSellPolicy() { manager()->context()->UnregisterTrader(this); }
 
 MatlSellPolicy& MatlSellPolicy::Init(Agent* manager, ResourceBuff* buf,
-                             std::string name, double quantize) {
+                                     std::string name, double quantize) {
   manager_ = manager;
-  quantize_ = quantize;
   buf_ = buf;
   name_ = name;
+  assert(quantize != 0);
+  quantize_ = quantize;
   return *this;
 }
 
@@ -37,59 +38,49 @@ void MatlSellPolicy::Stop() { manager()->context()->UnregisterTrader(this); }
 std::set<BidPortfolio<Material>::Ptr> MatlSellPolicy::GetMatlBids(
     CommodMap<Material>::type& commod_requests) {
   std::set<BidPortfolio<Material>::Ptr> ports;
-  if (buf_->empty()) {
+  if (buf_->empty() || buf_->quantity() < eps())
     return ports;
-  } else if (buf_->quantity() < eps()) {
-    return ports;
-  }
-
+  
+  double bcap = buf_->quantity();
+  bool excl = quantize_ > 0;
+  double limit = excl ?                                                 \
+                 bcap * static_cast<int>(std::floor(bcap / quantize_)) : bcap;
+  LGH(INFO3) << "bidding out " << limit << " kg";
+  
   BidPortfolio<Material>::Ptr port(new BidPortfolio<Material>());
-
-  LGH(INFO3) << "bidding out " << buf_->quantity() << " kg";
-
-  bool exclusive = true;
-  if (quantize_ < 0) {
-    exclusive = false;
-  }
-
-  std::set<std::string>::iterator it;
-  for (it = commods_.begin(); it != commods_.end(); ++it) {
-    std::string commod = *it;
+  CapacityConstraint<Material> cc(limit);
+  port->AddConstraint(cc);
+  ports.insert(port);
+  
+  std::string commod;
+  Request<Material>* req;
+  Material::Ptr m, offer;
+  double qty;
+  int nbids;
+  std::set<std::string>::iterator sit;
+  std::vector<Request<Material>*>::const_iterator rit;
+  for (sit = commods_.begin(); sit != commods_.end(); ++sit) {
+    commod = *sit;
     if (commod_requests.count(commod) < 1) {
       continue;
     }
 
     const std::vector<Request<Material>*>& requests =
         commod_requests.at(commod);
-
-    std::vector<Request<Material>*>::const_iterator it;
-    for (it = requests.begin(); it != requests.end(); ++it) {
-      Request<Material>* req = *it;
-      double qty = req->target()->quantity();
-      qty = std::min(qty, buf_->quantity());
-
-      if (quantize_ < 0) {
-        Material::Ptr m = buf_->Pop<Material>();
+    for (rit = requests.begin(); rit != requests.end(); ++rit) {
+      req = *rit;
+      qty = std::min(req->target()->quantity(), buf_->quantity());
+      nbids = excl ? static_cast<int>(std::floor(qty / quantize_)) : 1;
+      qty = excl ? quantize_ : qty;
+      for (int i = 0; i < nbids; i++) {
+        m = buf_->Pop<Material>();
         buf_->Push(m);
-        Material::Ptr offer = Material::CreateUntracked(qty, m->comp());
-        port->AddBid(req, offer, this, exclusive);
+        offer = Material::CreateUntracked(qty, m->comp());
+        port->AddBid(req, offer, this, excl);
         LG(INFO4) << "  - bid " << qty << " kg on a request for " << commod;
-      } else {
-        for (int i = 0; i < (int)(qty / quantize_); i++) {
-          Material::Ptr m = buf_->Pop<Material>();
-          buf_->Push(m);
-          Material::Ptr offer = Material::CreateUntracked(quantize_, m->comp());
-          port->AddBid(req, offer, this, exclusive);
-          LG(INFO4) << "  - bid " << quantize_ << " kg on a request for "
-                    << commod;
-        }
       }
     }
   }
-
-  CapacityConstraint<Material> cc(buf_->quantity());
-  port->AddConstraint(cc);
-  ports.insert(port);
   return ports;
 }
 
