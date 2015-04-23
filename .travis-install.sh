00eea1f2d0cb330e37c51f28bb8cbe7f7ee5c6c7
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-23 11:38:01 -0500

    early exit if bad build or install

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,4 +1,13 @@
 wget https://github.com/gidden/ciclus/archive/travis.zip -O ciclus.zip
 unzip -j ciclus.zip "*/cyclus/*" -d conda-recipe
 conda build --no-test conda-recipe
+
+status=$?
+if [[ $status != 0 ]]; then
+    exit $status
+
 conda install --use-local cyclus=0.0
+
+status=$?
+if [[ $status != 0 ]]; then
+    exit $status
