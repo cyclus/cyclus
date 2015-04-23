96a0ef3e90958256cdea21254b8f1e4685c425a6
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-23 12:11:19 -0500

    fixed syntax errors added logging

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,13 +1,22 @@
 wget https://github.com/gidden/ciclus/archive/travis.zip -O ciclus.zip
 unzip -j ciclus.zip "*/cyclus/*" -d conda-recipe
-conda build --no-test conda-recipe
 
+# build
+cmd="conda build --no-test conda-recipe"
+echo "cmd: $cmd"
+$cmd
 status=$?
+echo "status: $status"
 if [[ $status != 0 ]]; then
     exit $status
+fi
 
-conda install --use-local cyclus=0.0
-
+# install
+cmd="conda install --use-local cyclus=0.0"
+echo "cmd: $cmd"
+$cmd
 status=$?
+echo "status: $status"
 if [[ $status != 0 ]]; then
     exit $status
+fi
