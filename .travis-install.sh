c49a8320ffc2643f3691bc45d705db9eb33b4347
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-24 11:44:30 -0500

    rebase rm'd prior fixes of set and comments

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,22 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+
 wget https://github.com/gidden/ciclus/archive/travis.zip -O ciclus.zip
 unzip -j ciclus.zip "*/cyclus/*" -d conda-recipe
 
 # build
-cmd="conda build --no-test conda-recipe"
-echo "cmd: $cmd"
-$cmd
-status=$?
-echo "status: $status"
-if [[ $status != 0 ]]; then
-    exit $status
-fi
+conda build --no-test conda-recipe
 
 # install
-cmd="conda install --use-local cyclus=0.0"
-echo "cmd: $cmd"
-$cmd
-status=$?
-echo "status: $status"
-if [[ $status != 0 ]]; then
-    exit $status
-fi
+conda install --use-local cyclus=0.0
