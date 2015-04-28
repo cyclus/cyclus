be03ef122598426ea65c5012fd520e5cb92a1d69
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-28 16:12:38 -0500

    moving conda files to this repo

--- .travis-install.sh
+++ .travis-install.sh
@@ -7,12 +7,8 @@ set -e # exit as soon as an error occurs
 msg=`git log --pretty=oneline -1`
 echo "Building commit: $msg" 
 
-# setup conda recipe
-wget https://github.com/cyclus/ciclus/archive/master.zip -O ciclus.zip
-unzip -j ciclus.zip "*/cyclus/*" -d conda-recipe
-
 # build
-conda build --no-test conda-recipe
+conda build --no-test conda
 
 # install
 conda install --use-local cyclus=0.0
