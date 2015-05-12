41abfe757d220299e8e8d9a157011b088ddda987
Robert Carlsen <rwcarlsen@gmail.com>
2015-05-12 17:16:53 -0500

    better tests for all timeseries

--- tests/toolkit/timeseries_tests.cc
+++ tests/toolkit/timeseries_tests.cc
@@ -8,18 +8,44 @@
 namespace cyclus {
 namespace toolkit {
 
+template <TimeSeriesType T>
+void testseries(std::string tbl) {
+  Recorder rec;
+  Timer ti;
+  Context ctx(&ti, &rec);
+  SqliteBack* back = new SqliteBack(":memory:");
+  rec.RegisterBackend(back);
+
+  Agent* a = new TestAgent(&ctx);
+  RecordTimeSeries<T>(a, 42.0);
+  rec.Close();
+
+  SqliteDb db = back->db();
+  cyclus::SqlStatement::Ptr stmt = db.Prepare("SELECT agentid,value FROM TimeSeries" + tbl);
+  ASSERT_NO_THROW(stmt->Step());
+  EXPECT_EQ(a->id(), stmt->GetInt(0));
+  EXPECT_EQ(42.0, stmt->GetDouble(1));
+
+  delete back;
+}
+
+
 TEST(TimeSeriesTests, Power) {
-  TestContext tc;
-  Agent* a = new TestAgent(tc.get());
-  RecordTimeSeries<POWER>(a, 42.0);
-  delete a;
+  testseries<POWER>("Power");
+}
+
+TEST(TimeSeriesTests, EnrichSWU) {
+  testseries<ENRICH_SWU>("EnrichmentSWU");
+}
+
+TEST(TimeSeriesTests, EnrichFeed) {
+  testseries<ENRICH_FEED>("EnrichmentFeed");
 }
 
 TEST(TimeSeriesTests, RawPower) {
   TestContext tc;
   Agent* a = new TestAgent(tc.get());
   RecordTimeSeries<double>("Power", a, 42.0);
-  delete a;
 }
 
 
