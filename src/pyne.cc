db696b9e4e1318fb61447f64633e1e5709f94d76
Matthew Gidden <matthew.gidden@gmail.com>
2015-05-27 12:16:21 -0500

    final release commit after maintenence

--- src/pyne.cc
+++ src/pyne.cc
@@ -20,7 +20,7 @@
 //
 // start of license.txt
 //
-// Copyright 2011-2014, the PyNE Development Team. All rights reserved.
+// Copyright 2011-2015, the PyNE Development Team. All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
