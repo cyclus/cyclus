da9f4aff189fff4b0f9ee945ad13fdff7874beae
Matthew Gidden <matthew.gidden@gmail.com>
2015-07-09 10:55:34 -0500

    use a static constant for default preference

--- src/request.h
+++ src/request.h
@@ -9,6 +9,8 @@
 
 namespace cyclus {
 
+static const double kDefaultPref = 1; 
+  
 class Trader;
 template <class T> class RequestPortfolio;
 
@@ -35,7 +37,7 @@ class Request {
       Trader* requester,
       typename RequestPortfolio<T>::Ptr portfolio,
       std::string commodity = "",
-      double preference = 1,
+      double preference = kDefaultPref,
       bool exclusive = false) {
     return new Request<T>(target, requester, portfolio,
                           commodity, preference, exclusive);
@@ -46,7 +48,7 @@ class Request {
   inline static Request<T>* Create(boost::shared_ptr<T> target,
                                    Trader* requester,
                                    std::string commodity = "",
-                                   double preference = 1,
+                                   double preference = kDefaultPref,
                                    bool exclusive = false) {
     return new Request<T>(target, requester, commodity, preference,
                           exclusive);
@@ -75,7 +77,7 @@ class Request {
  private:
   /// @brief constructors are private to require use of factory methods
   Request(boost::shared_ptr<T> target, Trader* requester,
-          std::string commodity = "", double preference = 1,
+          std::string commodity = "", double preference = kDefaultPref,
           bool exclusive = false)
       : target_(target),
         requester_(requester),
@@ -85,7 +87,7 @@ class Request {
 
   Request(boost::shared_ptr<T> target, Trader* requester,
           typename RequestPortfolio<T>::Ptr portfolio,
-          std::string commodity = "", double preference = 1,
+          std::string commodity = "", double preference = kDefaultPref,
           bool exclusive = false)
       : target_(target),
         requester_(requester),
