eb895048189b9149c6b3799801f1c123e5f10d7f
Robert Carlsen <rwcarlsen@gmail.com>
2015-07-27 10:25:42 -0500

    install cyclus' cmake files

--- cmake/UseCyclus.cmake
+++ cmake/UseCyclus.cmake
@@ -38,6 +38,10 @@
 #   test_driver : (optional) the custom test driver to use with unit tests,
 #                 or NONE
 #
+# The CYCLUS_CUSTOM_HEADERS variable can optionally be set to contain one or
+# more (space separated) header files before calling the USE_CYCLUS
+# macro to add shared headers used by the archetype library being built.
+#
 # The following vars are updated.
 #
 # CYCLUS_LIBRARIES   : updated to include <lib_root>_LIB
@@ -83,12 +87,28 @@ MACRO(USE_CYCLUS lib_root src_root)
         SET(INCL_ARGS "${INCL_ARGS}:${DIR}")
     ENDFOREACH(DIR ${DIRS})
 
+
     # set cpp path
     IF("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
         SET(PREPROCESSOR "--cpp-path=clang++")
     ELSE()
         SET(PREPROCESSOR "--cpp-path=cpp")
     ENDIF()
+
+    # copy custom headers
+    FOREACH(fname ${CYCLUS_CUSTOM_HEADERS})
+        SET(src "${CMAKE_CURRENT_SOURCE_DIR}/${fname}")
+        SET(dst "${BUILD_DIR}/${fname}")
+        MESSAGE(STATUS "Copying ${src} to ${dst}.")
+        EXECUTE_PROCESS(COMMAND "cp" "${src}" "${dst}")
+        ADD_CUSTOM_COMMAND(
+            OUTPUT "${dst}"
+            COMMAND "cp" "${src}" "${dst}"
+            DEPENDS "${src}"
+            COMMENT "Copying ${src} to ${dst}."
+            )
+    ENDFOREACH()
+    MESSAGE("dstcustomheaders: ${CYCLUS_CUSTOM_HEADERS}")
 
     # process header
     SET(ORIG "--pass3-use-orig")
@@ -123,6 +143,7 @@ MACRO(USE_CYCLUS lib_root src_root)
             DEPENDS ${HIN}
             DEPENDS ${CCIN}
             DEPENDS ${CYCPP}
+            DEPENDS ${CYCLUS_CUSTOM_HEADERS}
             COMMENT "Executing ${CYCPP} ${HIN} ${PREPROCESSOR} ${HFLAG} ${ORIG} ${INCL_ARGS}"
             COMMENT "Executing ${CYCPP} ${CCIN} ${PREPROCESSOR} ${CCFLAG} ${ORIG} ${INCL_ARGS}"
             )
@@ -137,6 +158,7 @@ MACRO(USE_CYCLUS lib_root src_root)
             COMMAND ${CYCPP} ${CCIN} ${PREPROCESSOR} ${CCFLAG} ${ORIG} ${INCL_ARGS}
             DEPENDS ${CCIN}
             DEPENDS ${CYCPP}
+            DEPENDS ${CYCLUS_CUSTOM_HEADERS}
             COMMENT "Executing ${CYCPP} ${CCIN} ${PREPROCESSOR} ${CCFLAG} ${ORIG} ${INCL_ARGS}"
             )
     ENDIF(EXISTS "${HIN}")
@@ -161,6 +183,7 @@ MACRO(USE_CYCLUS lib_root src_root)
                 DEPENDS ${CCIN}
                 DEPENDS ${HTIN}
                 DEPENDS ${CCTIN}
+                DEPENDS ${CYCLUS_CUSTOM_HEADERS}
                 COMMENT "Copying ${HTIN} to ${HTOUT}."
                 COMMENT "Copying ${CCTIN} to ${CCTOUT}."
                 )
@@ -176,6 +199,7 @@ MACRO(USE_CYCLUS lib_root src_root)
             COMMAND ${CMD} ${CCTIN} ${CCTOUT}
             DEPENDS ${CCTIN}
             DEPENDS ${CCIN}
+            DEPENDS ${CYCLUS_CUSTOM_HEADERS}
             COMMENT "Copying ${CCTIN} to ${CCTOUT}."
             )
         SET("${lib_root}_TEST_CC" "${${lib_root}_TEST_CC}" "${CCOUT}" "${CCTOUT}"
