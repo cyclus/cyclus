2108d6425f92c96ea9a0532d8a3e864ff4b99b9c
Matthew Gidden <matthew.gidden@gmail.com>
2015-07-29 11:03:54 -0500

    tests now work up to the second install

--- tests/test_stubs.py
+++ tests/test_stubs.py
@@ -11,29 +11,31 @@ def tmpdir():
     os.rmdir(d)
 
 def test_stubs():
-    flavors = ['facility', 'institution', 'region']
+    flavors = ['facility', 'inst', 'region']
     pth = 'foo'
     with tmpdir() as d:
         src = os.path.join(d, pth)
         bld = os.path.join(d, 'bar')
         inst = os.path.join(d, 'baz')
         
-        stub_cmd = 'cycstub {0} {1}:{0}:{0}'
+        stub_cmd = 'cycstub --type {0} {1}:{1}:{2}'
         inst_cmd = 'python install.py --build_dir {0} --prefix {1}'
         tst_cmd = '{}_unit_tests'
         
         os.mkdir(src)
         for flav in flavors:
             # generate stub
-            cmd = stub_cmd.format(flav, pth)
+            cmd = stub_cmd.format(flav, pth, flav.capitalize())
+            print(cmd)
             subprocess.call(cmd.split(), shell=(os.name=='nt'), cwd=src)
             # build/install stub
             cmd = inst_cmd.format(bld, inst)
             subprocess.call(cmd.split(), shell=(os.name=='nt'), cwd=src)
             # run unit tests for stub
-            cmd = tst_cmd.format(flav)
-            subprocess.call(cmd.split(), shell=(os.name=='nt'), 
-                            cwd=os.path.join(inst, pth))
+        
+        cmd = tst_cmd.format(pth)
+        subprocess.call(cmd.split(), shell=(os.name=='nt'), 
+                        cwd=os.path.join(inst, pth))
 
 if __name__ == '__main__':
     test_stubs()
