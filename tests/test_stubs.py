0fcc7fa84646fa0a7b588c4745e125fc40b5a876
Matthew Gidden <matthew.gidden@gmail.com>
2015-08-03 13:06:11 -0500

    added some logging capability, subprocess is terrible

--- tests/test_stubs.py
+++ tests/test_stubs.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
 import shutil
+import sys
 import tempfile
+import io
 
 from contextlib import contextmanager
 
@@ -10,6 +12,11 @@ def tmpdir():
     d = tempfile.mkdtemp()
     yield d
     shutil.rmtree(d)
+
+@contextmanager
+def tmplog(fname):
+    yield io.open(fname, mode='w')
+    os.remove(fname)    
 
 def test_stubs():
     flavors = ['facility', 'inst', 'region']
@@ -23,20 +30,42 @@ def test_stubs():
         inst_cmd = 'python install.py --build_dir {0} --prefix {1}'
         tst_cmd = './bin/{}_unit_tests'
         
+        log = 'stub_test.log'
+        msg = '**Error, check the log in {}**'.format(log)
         os.mkdir(src)
         for flav in flavors:
             # generate stub
             cmd = stub_cmd.format(flav, pth, 'tmp' + flav.capitalize())
             print(cmd)
-            subprocess.call(cmd.split(), shell=(os.name=='nt'), cwd=src)
+            try:
+                with tmplog(log) as f:
+                    subprocess.check_call(cmd.split(), shell=(os.name=='nt'), 
+                                          cwd=src, stdout=f, stderr=f)
+            except subprocess.CalledProcessError as e:
+                print(msg)
+                raise e
+
         # build/install stub
         cmd = inst_cmd.format(bld, inst)
         print(cmd)
-        subprocess.call(cmd.split(), shell=(os.name=='nt'), cwd=src)
+        try:
+            with tmplog(log) as f:
+                subprocess.check_call(cmd.split(), shell=(os.name=='nt'), 
+                                      cwd=src, stdout=f, stderr=f)
+        except subprocess.CalledProcessError as e:
+            print(msg)
+            raise e
+
         # run unit tests for stub
         cmd = tst_cmd.format(pth)
         print(cmd)
-        subprocess.call(cmd.split(), shell=(os.name=='nt'), cwd=inst)
+        try:
+            with tmplog(log) as f:
+                subprocess.check_call(cmd.split(), shell=(os.name=='nt'), cwd=inst, 
+                                      stdout=f, stderr=f)
+        except subprocess.CalledProcessError as e:
+            print(msg)
+            raise e
 
 if __name__ == '__main__':
     test_stubs()
