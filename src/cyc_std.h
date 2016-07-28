5e0572fd1d25ddc120b6ca8796a453978780b3ce
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-28 16:36:56 -0500

    auto style cyc_std

--- src/cyc_std.h
+++ src/cyc_std.h
@@ -8,18 +8,17 @@
 namespace cyclus {
 
 /// @brief a less-than comparison for pairs
-template<class T> struct SecondLT : std::binary_function<T, T, bool> {
-  bool operator()(const T& x, const T& y) const {
-    return x.second < y.second;
-  }
+template <class T>
+struct SecondLT : std::binary_function<T, T, bool> {
+  bool operator()(const T& x, const T& y) const { return x.second < y.second; }
 };
 
 // taken from
 // http://stackoverflow.com/questions/8473009/how-to-efficiently-compare-two-maps-of-strings-in-c
 template <typename Map>
 bool map_compare(Map const& lhs, Map const& rhs) {
-  return lhs.size() == rhs.size()
-         && std::equal(lhs.begin(), lhs.end(), rhs.begin());
+  return lhs.size() == rhs.size() &&
+         std::equal(lhs.begin(), lhs.end(), rhs.begin());
 }
 
 }  // namespace cyclus
