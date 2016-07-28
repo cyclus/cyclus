5cd7c502b8e765c4df9fa1955102074bab4a6f41
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-28 17:41:47 -0500

    auto style trade_management.h

--- src/trader_management.h
+++ src/trader_management.h
@@ -3,92 +3,90 @@
 
 #include "error.h"
 #include "exchange_context.h"
-#include "product.h"
 #include "material.h"
+#include "product.h"
 #include "trader.h"
 
 namespace cyclus {
 
 // template specializations to support inheritance and virtual functions
-template<class T>
-inline static std::set<typename RequestPortfolio<T>::Ptr>
-    QueryRequests(Trader* t) {
+template <class T>
+inline static std::set<typename RequestPortfolio<T>::Ptr> QueryRequests(
+    Trader* t) {
   throw StateError("Non-specialized version of QueryRequests not supported");
 }
 
-template<>
-inline std::set<RequestPortfolio<Material>::Ptr>
-    QueryRequests<Material>(Trader* t) {
+template <>
+inline std::set<RequestPortfolio<Material>::Ptr> QueryRequests<Material>(
+    Trader* t) {
   return t->GetMatlRequests();
 }
 
-template<>
-inline std::set<RequestPortfolio<Product>::Ptr>
-    QueryRequests<Product>(Trader* t) {
+template <>
+inline std::set<RequestPortfolio<Product>::Ptr> QueryRequests<Product>(
+    Trader* t) {
   return t->GetProductRequests();
 }
 
-template<class T>
-inline static std::set<typename BidPortfolio<T>::Ptr>
-    QueryBids(Trader* t, typename CommodMap<T>::type& map) {
+template <class T>
+inline static std::set<typename BidPortfolio<T>::Ptr> QueryBids(
+    Trader* t, typename CommodMap<T>::type& map) {
   throw StateError("Non-specialized version of QueryBids not supported");
 }
 
-template<>
-inline std::set<BidPortfolio<Material>::Ptr>
-    QueryBids<Material>(Trader* t, CommodMap<Material>::type& map) {
+template <>
+inline std::set<BidPortfolio<Material>::Ptr> QueryBids<Material>(
+    Trader* t, CommodMap<Material>::type& map) {
   return t->GetMatlBids(map);
 }
 
-template<>
-inline std::set<BidPortfolio<Product>::Ptr>
-    QueryBids<Product>(Trader* t, CommodMap<Product>::type& map) {
+template <>
+inline std::set<BidPortfolio<Product>::Ptr> QueryBids<Product>(
+    Trader* t, CommodMap<Product>::type& map) {
   return t->GetProductBids(map);
 }
 
-template<class T>
+template <class T>
 inline static void PopulateTradeResponses(
-    Trader* trader,
-    const std::vector< Trade<T> >& trades,
-    std::vector< std::pair<Trade<T>, typename T::Ptr> >& responses) {
-  throw StateError("Non-specialized version of "
-                   "PopulateTradeResponses not supported");
+    Trader* trader, const std::vector<Trade<T> >& trades,
+    std::vector<std::pair<Trade<T>, typename T::Ptr> >& responses) {
+  throw StateError(
+      "Non-specialized version of "
+      "PopulateTradeResponses not supported");
 }
 
-template<>
+template <>
 inline void PopulateTradeResponses<Material>(
-    Trader* trader,
-    const std::vector< Trade<Material> >& trades,
+    Trader* trader, const std::vector<Trade<Material> >& trades,
     std::vector<std::pair<Trade<Material>, Material::Ptr> >& responses) {
   trader->GetMatlTrades(trades, responses);
 }
 
-template<>
+template <>
 inline void PopulateTradeResponses<Product>(
-    Trader* trader,
-    const std::vector< Trade<Product> >& trades,
+    Trader* trader, const std::vector<Trade<Product> >& trades,
     std::vector<std::pair<Trade<Product>, Product::Ptr> >& responses) {
   trader->GetProductTrades(trades, responses);
 }
 
-template<class T>
+template <class T>
 inline static void AcceptTrades(
     Trader* trader,
-    const std::vector< std::pair<Trade<T>, typename T::Ptr> >& responses) {
+    const std::vector<std::pair<Trade<T>, typename T::Ptr> >& responses) {
   throw StateError("Non-specialized version of AcceptTrades not supported");
 }
 
-template<>
+template <>
 inline void AcceptTrades(
     Trader* trader,
-    const std::vector< std::pair<Trade<Material>, Material::Ptr> >& responses) {
+    const std::vector<std::pair<Trade<Material>, Material::Ptr> >& responses) {
   trader->AcceptMatlTrades(responses);
 }
 
-template<>
+template <>
 inline void AcceptTrades(
     Trader* trader,
-    const std::vector< std::pair<Trade<Product>, Product::Ptr> >& responses) {
+    const std::vector<std::pair<Trade<Product>, Product::Ptr> >& responses) {
   trader->AcceptProductTrades(responses);
 }
 
