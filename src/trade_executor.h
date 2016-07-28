1de0290415a2076dd76ff80f7d96fd107849330e
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-28 17:42:04 -0500

    auto style trade_executor.h

--- src/trade_executor.h
+++ src/trade_executor.h
@@ -22,17 +22,18 @@ struct TradeExecutionContext {
   std::set<Trader*> requesters;
 
   // the key is the supplier
-  std::map<Trader*, std::vector< Trade<T> > > trades_by_supplier;
+  std::map<Trader*, std::vector<Trade<T> > > trades_by_supplier;
 
   // the key is the requester, values are a vector of the target Trade with the
   // associated response resource provided by the supplier
-  std::map<Trader*, std::vector< std::pair<Trade<T>, typename T::Ptr> > >
+  std::map<Trader*, std::vector<std::pair<Trade<T>, typename T::Ptr> > >
       trades_by_requester;
 
   // by convention, the first trader is the supplier, the second is the
   // requester
   std::map<std::pair<Trader*, Trader*>,
-      std::vector< std::pair<Trade<T>, typename T::Ptr> > > all_trades;
+           std::vector<std::pair<Trade<T>, typename T::Ptr> > >
+      all_trades;
 };
 
 /// @class TradeExecutor
@@ -46,7 +47,7 @@ struct TradeExecutionContext {
 template <class T>
 class TradeExecutor {
  public:
-  explicit TradeExecutor(const std::vector< Trade<T> >& trades)
+  explicit TradeExecutor(const std::vector<Trade<T> >& trades)
       : trades_(trades) {}
 
   /// @brief execute all trades, collecting responders from bidders and sending
@@ -76,19 +77,20 @@ class TradeExecutor {
   /// occur
   void RecordTrades(Context* ctx) {
     // record all trades
-    typename std::map<std::pair<Trader*, Trader*>,
-        std::vector< std::pair<Trade<T>, typename T::Ptr> > >::iterator m_it;
+    typename std::map<
+        std::pair<Trader*, Trader*>,
+        std::vector<std::pair<Trade<T>, typename T::Ptr> > >::iterator m_it;
     for (m_it = trade_ctx_.all_trades.begin();
          m_it != trade_ctx_.all_trades.end(); ++m_it) {
       Agent* supplier = m_it->first.first->manager();
       Agent* requester = m_it->first.second->manager();
-      typename std::vector< std::pair<Trade<T>, typename T::Ptr> >&
-          trades = m_it->second;
-      typename std::vector< std::pair<Trade<T>, typename T::Ptr> >::iterator
+      typename std::vector<std::pair<Trade<T>, typename T::Ptr> >& trades =
+          m_it->second;
+      typename std::vector<std::pair<Trade<T>, typename T::Ptr> >::iterator
           v_it;
       for (v_it = trades.begin(); v_it != trades.end(); ++v_it) {
         Trade<T>& trade = v_it->first;
-        typename T::Ptr rsrc =  v_it->second;
+        typename T::Ptr rsrc = v_it->second;
         ctx->NewDatum("Transactions")
             ->AddVal("TransactionId", ctx->NextTransactionID())
             ->AddVal("SenderId", supplier->id())
@@ -104,20 +106,18 @@ class TradeExecutor {
   inline const TradeExecutionContext<T>& trade_ctx() const {
     return trade_ctx_;
   }
-  inline TradeExecutionContext<T>& trade_ctx() {
-    return trade_ctx_;
-  }
+  inline TradeExecutionContext<T>& trade_ctx() { return trade_ctx_; }
 
  private:
-  const std::vector< Trade<T> >& trades_;
+  const std::vector<Trade<T> >& trades_;
   TradeExecutionContext<T> trade_ctx_;
 };
 
 /// @brief populates suppliers_, requesters_, and trades_by_supplier_
-template<class T>
+template <class T>
 void GroupTradesBySupplier(TradeExecutionContext<T>& trade_ctx,
-                           const std::vector< Trade<T> >& trades) {
-  typename std::vector< Trade<T> >::const_iterator it;
+                           const std::vector<Trade<T> >& trades) {
+  typename std::vector<Trade<T> >::const_iterator it;
   for (it = trades.begin(); it != trades.end(); ++it) {
     trade_ctx.trades_by_supplier[it->bid->bidder()].push_back(*it);
     trade_ctx.suppliers.insert(it->bid->bidder());
@@ -127,19 +127,19 @@ void GroupTradesBySupplier(TradeExecutionContext<T>& trade_ctx,
 
 /// @brief queries each supplier for the responses to thier matched trade and
 /// populates trades_by_requester_ and all_trades_ with the results
-template<class T>
+template <class T>
 static void GetTradeResponses(TradeExecutionContext<T>& trade_ctx) {
   std::set<Trader*>::iterator it;
   for (it = trade_ctx.suppliers.begin(); it != trade_ctx.suppliers.end();
        ++it) {
     // get responses
     Trader* supplier = *it;
-    std::vector< std::pair<Trade<T>, typename T::Ptr> > responses;
+    std::vector<std::pair<Trade<T>, typename T::Ptr> > responses;
     PopulateTradeResponses(supplier, trade_ctx.trades_by_supplier[supplier],
                            responses);
 
     // populate containers
-    typename std::vector< std::pair<Trade<T>, typename T::Ptr> >::iterator r_it;
+    typename std::vector<std::pair<Trade<T>, typename T::Ptr> >::iterator r_it;
     for (r_it = responses.begin(); r_it != responses.end(); ++r_it) {
       // @todo unsure if this is needed...
       // Trade<T>& trade = r_it->first;
@@ -149,7 +149,8 @@ static void GetTradeResponses(TradeExecutionContext<T>& trade_ctx) {
       // }
       Trader* requester = r_it->first.request->requester();
       trade_ctx.trades_by_requester[requester].push_back(*r_it);
-      trade_ctx.all_trades[std::make_pair(supplier, requester)].push_back(*r_it);
+      trade_ctx.all_trades[std::make_pair(supplier, requester)].push_back(
+          *r_it);
     }
   }
 }
