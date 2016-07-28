03f145a3dec5dc8e25c2a12d42128cd8e952bf4c
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-28 16:30:00 -0500

    auto style capacity_constraint

--- src/capacity_constraint.h
+++ src/capacity_constraint.h
@@ -12,9 +12,9 @@ namespace cyclus {
 /// @class Converter
 ///
 /// @brief a simple interface for converting resource objects to unit capacities
-template<class T>
+template <class T>
 struct Converter {
-  typedef boost::shared_ptr< Converter<T> > Ptr;
+  typedef boost::shared_ptr<Converter<T> > Ptr;
 
   /// @brief convert a capacitated quantity for an offer in its exchange context
   /// @param offer the resource being offered
@@ -23,30 +23,24 @@ struct Converter {
   ///
   /// @warning it is up to the user to inherit default parameters
   virtual double convert(
-      boost::shared_ptr<T> offer,
-      Arc const * a = NULL,
-      ExchangeTranslationContext<T> const * ctx = NULL) const = 0;
+      boost::shared_ptr<T> offer, Arc const* a = NULL,
+      ExchangeTranslationContext<T> const* ctx = NULL) const = 0;
 
   /// @brief operator== is available for subclassing, see
   /// cyclus::TrivialConverter for an example
-  virtual bool operator==(Converter& other) const {
-    return false;
-  }
-  bool operator!=(Converter& other) const {
-    return !operator==(other);
-  }
+  virtual bool operator==(Converter& other) const { return false; }
+  bool operator!=(Converter& other) const { return !operator==(other); }
 };
 
 /// @class TrivialConverter
 ///
 /// @brief The default converter returns the resource's quantity
-template<class T>
+template <class T>
 struct TrivialConverter : public Converter<T> {
   /// @returns the quantity of resource offer
   inline virtual double convert(
-      boost::shared_ptr<T> offer,
-      Arc const * a = NULL,
-      ExchangeTranslationContext<T> const * ctx = NULL) const  {
+      boost::shared_ptr<T> offer, Arc const* a = NULL,
+      ExchangeTranslationContext<T> const* ctx = NULL) const {
     return offer->quantity();
   }
 
@@ -65,9 +59,7 @@ class CapacityConstraint {
  public:
   /// @brief constructor for a constraint with a non-trivial converter
   CapacityConstraint(double capacity, typename Converter<T>::Ptr converter)
-      : capacity_(capacity),
-        converter_(converter),
-        id_(next_id_++) {
+      : capacity_(capacity), converter_(converter), id_(next_id_++) {
     if (capacity_ <= 0)
       throw ValueError("Capacity is not positive, no trades will be executed");
   }
@@ -75,8 +67,7 @@ class CapacityConstraint {
   /// @brief constructor for a constraint with a trivial converter (i.e., one
   /// that simply returns 1)
   explicit CapacityConstraint(double capacity)
-      : capacity_(capacity),
-        id_(next_id_++) {
+      : capacity_(capacity), id_(next_id_++) {
     if (capacity_ <= 0)
       throw ValueError("Capacity is not positive, no trades will be executed");
     converter_ = typename Converter<T>::Ptr(new TrivialConverter<T>());
@@ -89,26 +80,18 @@ class CapacityConstraint {
         id_(next_id_++) {}
 
   /// @return the constraints capacity
-  inline double capacity() const {
-    return capacity_;
-  }
+  inline double capacity() const { return capacity_; }
 
   /// @return the converter
-  inline typename Converter<T>::Ptr converter() const {
-    return converter_;
-  }
+  inline typename Converter<T>::Ptr converter() const { return converter_; }
 
-  inline double convert(
-      boost::shared_ptr<T> offer,
-      Arc const * a = NULL,
-      ExchangeTranslationContext<T> const * ctx = NULL) const {
+  inline double convert(boost::shared_ptr<T> offer, Arc const* a = NULL,
+                        ExchangeTranslationContext<T> const* ctx = NULL) const {
     return converter_->convert(offer, a, ctx);
   }
 
   /// @return a unique id for the constraint
-  inline int id() const {
-    return id_;
-  }
+  inline int id() const { return id_; }
 
  private:
   double capacity_;
@@ -117,22 +100,23 @@ class CapacityConstraint {
   static int next_id_;
 };
 
-template<class T> int CapacityConstraint<T>::next_id_ = 0;
+template <class T>
+int CapacityConstraint<T>::next_id_ = 0;
 
 /// @brief CapacityConstraint-CapacityConstraint equality operator
-template<class T>
+template <class T>
 inline bool operator==(const CapacityConstraint<T>& lhs,
                        const CapacityConstraint<T>& rhs) {
-  return  ((lhs.capacity() == rhs.capacity()) &&
-           (*lhs.converter() == *rhs.converter()));
+  return ((lhs.capacity() == rhs.capacity()) &&
+          (*lhs.converter() == *rhs.converter()));
 }
 
 /// @brief CapacityConstraint-CapacityConstraint comparison operator, allows
 /// usage in ordered containers
-template<class T>
+template <class T>
 inline bool operator<(const CapacityConstraint<T>& lhs,
                       const CapacityConstraint<T>& rhs) {
-  return  (lhs.id() < rhs.id());
+  return (lhs.id() < rhs.id());
 }
 
 }  // namespace cyclus
