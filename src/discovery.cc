6bed21387aa9bb39f73cb994547d26f898aa39ca
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-28 16:40:24 -0500

    auto style discovery

--- src/discovery.cc
+++ src/discovery.cc
@@ -5,10 +5,10 @@
 #include <streambuf>
 #include <vector>
 
-#include <boost/filesystem.hpp>
-#include <boost/foreach.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
 
 #include "agent.h"
 #include "context.h"
@@ -26,20 +26,22 @@ std::set<std::string> DiscoverArchetypes(const std::string s) {
   std::set<string> archs;
   size_t offset = 0;
   size_t end_offset = 0;
-  const string words = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
-                                 "abcdefghijklmnopqrstuvwxyz";
+  const string words =
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+      "abcdefghijklmnopqrstuvwxyz";
   string construct = "Construct";
-  if (SUFFIX == ".dylib")
-    construct = "_Construct";
+  if (SUFFIX == ".dylib") construct = "_Construct";
   size_t lenconstruct = construct.length();
   while ((offset = s.find(construct, offset)) != string::npos) {
-    end_offset = s.find_first_not_of(words, offset+lenconstruct);
-    if (words.find(s[offset-1]) != string::npos || offset+lenconstruct == end_offset) {
+    end_offset = s.find_first_not_of(words, offset + lenconstruct);
+    if (words.find(s[offset - 1]) != string::npos ||
+        offset + lenconstruct == end_offset) {
       // make sure construct starts the word
       offset += lenconstruct;
       continue;
     }
-    archs.insert(string(s, offset+lenconstruct, end_offset - offset - lenconstruct));
+    archs.insert(
+        string(s, offset + lenconstruct, end_offset - offset - lenconstruct));
     offset = end_offset;
   }
   return archs;
@@ -54,13 +56,13 @@ std::set<std::string> DiscoverSpecs(std::string p, std::string lib) {
   libpath = Env::FindModule(libpath);
 
   // read in file, pre-allocates space
-  std::ifstream f (libpath.c_str());
+  std::ifstream f(libpath.c_str());
   std::string s;
   f.seekg(0, std::ios::end);
   s.reserve(f.tellg());
   f.seekg(0, std::ios::beg);
   s.assign((std::istreambuf_iterator<char>(f)),
-            std::istreambuf_iterator<char>());
+           std::istreambuf_iterator<char>());
 
   // find specs
   set<string> archs = DiscoverArchetypes(s);
@@ -70,8 +72,7 @@ std::set<std::string> DiscoverSpecs(std::string p, std::string lib) {
   for (set<string>::iterator it = archs.begin(); it != archs.end(); ++it) {
     spec = p + ":" + lib + ":" + (*it);
     agentspec = AgentSpec(spec);
-    if (DynamicModule::Exists(agentspec))
-      specs.insert(spec);
+    if (DynamicModule::Exists(agentspec)) specs.insert(spec);
   }
   return specs;
 }
@@ -89,8 +90,7 @@ std::set<std::string> DiscoverSpecsInDir(std::string d) {
   fs::recursive_directory_iterator last;
   for (; it != last; it.increment(errc)) {
     if (errc != no_err) {
-      if (it.level() > 0)
-        it.pop();
+      if (it.level() > 0) it.pop();
       continue;
     }
     pth = it->path();
@@ -103,14 +103,16 @@ std::set<std::string> DiscoverSpecsInDir(std::string d) {
     string p = pth.parent_path().string();
     string lib = pth.filename().string();
     if (d.length() < p.length())
-      p = p.substr(d.length()+1, string::npos);
+      p = p.substr(d.length() + 1, string::npos);
     else
       p = "";
     lib = lib.substr(3, lib.rfind(".") - 3);  // remove 'lib' prefix and suffix
     try {
       libspecs = DiscoverSpecs(p, lib);
-    } catch (cyclus::IOError& e) {}
-    for (set<string>::iterator ls = libspecs.begin(); ls != libspecs.end(); ++ls)
+    } catch (cyclus::IOError& e) {
+    }
+    for (set<string>::iterator ls = libspecs.begin(); ls != libspecs.end();
+         ++ls)
       specs.insert(*ls);
   }
   return specs;
@@ -123,9 +125,11 @@ std::set<std::string> DiscoverSpecsInCyclusPath() {
   set<string> specs;
   set<string> dirspecs;
   vector<string> cycpath = Env::cyclus_path();
-  for (vector<string>::iterator it = cycpath.begin(); it != cycpath.end(); ++it) {
+  for (vector<string>::iterator it = cycpath.begin(); it != cycpath.end();
+       ++it) {
     dirspecs = DiscoverSpecsInDir((*it).length() == 0 ? "." : (*it));
-    for (set<string>::iterator ds = dirspecs.begin(); ds != dirspecs.end(); ++ds)
+    for (set<string>::iterator ds = dirspecs.begin(); ds != dirspecs.end();
+         ++ds)
       specs.insert(*ds);
   }
   return specs;
@@ -156,7 +160,7 @@ Json::Value DiscoverMetadataInCyclusPath() {
   root["specs"] = spec;
   root["annotations"] = anno;
   root["schema"] = schm;
-  
+
   return root;
 }
 
