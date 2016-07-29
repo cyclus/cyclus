bf0ab24b400f2e633799334ee626edbffe003aa2
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-29 14:28:24 -0500

    fix on script

--- docker/release-ci/build_udpload_deb.sh
+++ docker/release-ci/build_udpload_deb.sh
@@ -6,7 +6,7 @@ fi
 
 
 sed -e s/XX/${1}/g docker/release-ci/Dockerfile_sample > Dockerfile
-docker build -t deb/ubuntu-${1} .: # build cyclus against ubuntu 14.04 in the docker container
+docker build -t deb/ubuntu-${1} . # build cyclus against ubuntu 14.04 in the docker container
 docker create --name=deb-${1}.04 deb/ubuntu-${1} # build deb in docker
 docker cp deb-${1}.04:/cyclus/deb ./ # extract deb
 scp ./deb/cyclus-*-ubuntu.${1}.04.deb root@dory.fuelcycle.org:./cyclus-deb/ # push the deb on dory
