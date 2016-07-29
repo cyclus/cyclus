b4fccecb6dd73331dc2720c14355eae929ff6871
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-29 11:08:12 -0500

    auto style enrichment.cc

--- src/toolkit/enrichment.cc
+++ src/toolkit/enrichment.cc
@@ -3,8 +3,8 @@
 #include <cmath>
 #include <sstream>
 
-#include "error.h"
 #include "cyc_limits.h"
+#include "error.h"
 #include "logger.h"
 #include "mat_query.h"
 
@@ -13,24 +13,16 @@ namespace toolkit {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Assays::Assays(double feed, double product, double tails)
-    : feed_(feed),
-      product_(product),
-      tails_(tails) {}
+    : feed_(feed), product_(product), tails_(tails) {}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-double Assays::Feed() const {
-  return feed_;
-}
+double Assays::Feed() const { return feed_; }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-double Assays::Product() const {
-  return product_;
-}
+double Assays::Product() const { return product_; }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-double Assays::Tails() const {
-  return tails_;
-}
+double Assays::Tails() const { return tails_; }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 double UraniumAssay(Material::Ptr rsrc) {
@@ -39,9 +31,8 @@ double UraniumAssay(Material::Ptr rsrc) {
   double u235 = mq.atom_frac(922350000);
   double u238 = mq.atom_frac(922380000);
 
-  LOG(LEV_DEBUG1, "CEnr") << "Comparing u235 atom fraction : "
-                          << u235 << " with u238 atom fraction: "
-                          << u238;
+  LOG(LEV_DEBUG1, "CEnr") << "Comparing u235 atom fraction : " << u235
+                          << " with u238 atom fraction: " << u238;
 
   if (u235 + u238 > 0) {
     value = u235 / (u235 + u238);
@@ -60,18 +51,14 @@ double UraniumQty(Material::Ptr rsrc) {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 double FeedQty(double product_qty, const Assays& assays) {
   double factor =
-      (assays.Product() - assays.Tails())
-      /
-      (assays.Feed() - assays.Tails());
+      (assays.Product() - assays.Tails()) / (assays.Feed() - assays.Tails());
   return product_qty * factor;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 double TailsQty(double product_qty, const Assays& assays) {
   double factor =
-      (assays.Product() - assays.Feed())
-      /
-      (assays.Feed() - assays.Tails());
+      (assays.Product() - assays.Feed()) / (assays.Feed() - assays.Tails());
   return product_qty * factor;
 }
 
@@ -98,10 +85,9 @@ double ValueFunc(double frac) {
 double SwuRequired(double product_qty, const Assays& assays) {
   double feed = FeedQty(product_qty, assays);
   double tails = TailsQty(product_qty, assays);
-  double swu =
-      product_qty * ValueFunc(assays.Product()) +
-      tails * ValueFunc(assays.Tails()) -
-      feed * ValueFunc(assays.Feed());
+  double swu = product_qty * ValueFunc(assays.Product()) +
+               tails * ValueFunc(assays.Tails()) -
+               feed * ValueFunc(assays.Feed());
   return swu;
 }
 
