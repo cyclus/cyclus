bf00b7a8606f5b0973531212dd4d3aae2e5e6314
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-07-29 11:05:19 -0500

    auto style commodity.cc

--- src/toolkit/commodity.cc
+++ src/toolkit/commodity.cc
@@ -10,9 +10,7 @@ Commodity::Commodity() : name_("") {}
 Commodity::Commodity(std::string name) : name_(name) {}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-std::string Commodity::name() const {
-  return name_;
-}
+std::string Commodity::name() const { return name_; }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 bool Commodity::operator==(const Commodity& other) const {
