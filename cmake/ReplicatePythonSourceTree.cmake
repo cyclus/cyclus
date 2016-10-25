3a1c92cf8d5734b7379aeccf3d5a70e3c562ce78
Anthony Scopatz <scopatz@gmail.com>
2016-10-25 17:49:28 -0400

    yup

--- cmake/ReplicatePythonSourceTree.cmake
+++ cmake/ReplicatePythonSourceTree.cmake
@@ -1,4 +1,11 @@
 # Note: when executed in the build dir, then CMAKE_CURRENT_SOURCE_DIR is the
 # build dir.
-file( COPY setup.py src test bin DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.py" )
+file(COPY setup.py cyclus DESTINATION "${CMAKE_ARGV3}"
+     FILES_MATCHING PATTERN "*.py"
+                    PATTERN "*.pyw"
+                    PATTERN "*.csv"
+                    PATTERN "*.txt"
+                    PATTERN "*.inp"
+                    PATTERN "*.html"
+                    PATTERN "*.pxi"
+                    PATTERN "*.mas12")
