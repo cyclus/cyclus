25b4fdcc75dbe4385328565f4e19e34ce47b4027
gyutaepark <gtpark96@gmail.com>
2016-11-28 20:17:28 -0600

    changed implementation to store data as degrees of seconds

--- src/gis.cc
+++ src/gis.cc
@@ -7,61 +7,55 @@ GIS::GIS(){
     longitude = 0.0;
 }
 
-GIS::GIS(float lat, float lon){
-    string temp_lat = to_string(lat);
-    if (temp_lat.length() != 8){
-        throw std::invalid_argument("Please format the latitude properly. Proper format is shown in the documentation");
-    }
-    string temp_lon = to_string(lon);
-    if (temp_lon.length() != 9){
-        throw std::invalid_argument("Please format the longitude properly. Proper format is shown in the documentation");
-    }
-    latitude = lat;
-    longitude = lon;
+GIS::GIS(float decimal_lat, float decimal_lon){
+	latitude = setPrecision(decimal_lat * 3600, 1);
+	longitude = setPrecision(decimal_lon * 3600, 1);
 }
 
 GIS::~GIS(){
 }
 
 float GIS::get_latitude_decimal() const{
-    string temp_lat = to_string(latitude);
-    float lat = std::stof(temp_lat.substr(0,2)) + std::stof(temp_lat.substr(2,2))/60 + std::stof(temp_lat.substr(4,4))/3600;
-    temp_lat = to_string(lat);
-    lat = std::stof(temp_lat.substr(0,8));
-    return lat;
-}
-
-float GIS::get_latitude_degrees() const{
-    return latitude;
+    return setPrecision(latitude / 3600, 6);
 }
 
 float GIS::get_longitude_decimal() const{
-    string temp_lon = to_string(longitude);
-    float lon = std::stof(temp_lon.substr(0,2)) + std::stof(temp_lon.substr(2,2))/60 + std::stof(temp_lon.substr(4,4))/3600;
-    temp_lon = to_string(lon);
-    lon = std::stof(temp_lon.substr(0,8));
-    return lon;
-}
-
-float GIS::get_longitude_degrees() const{
-    return longitude;
+    return setPrecision(longitude / 3600, 6);
 }
 
 void GIS::set_latitude_decimal(float lat){
-}
-
-void GIS::set_latitude_degrees(float lat){
-    latitude = lat;
+	latitude = setPrecision(lat * 3600, 1);
 }
 
 void GIS::set_longitude_decimal(float lon){
+	longitude = setPrecision(lon * 3600, 1);
+}
+/*
+float GIS::get_latitude_degrees() const{
+	// to be implemented if necessary
+}
+float GIS::get_longitude_degrees() const{
+	// to be implemented if necessary
+}
+
+void GIS::set_latitude_degrees(float lat){
+	// to be implemented if necessary
 }
 
 void GIS::set_longitude_degrees(float lon){
-    longitude = lon;
+	// to be implemented if necessary
 }
-
+*/
 double GIS::get_distance(GIS target) const{
+	double curr_longitude = this.get_longitude_decimal();
+	double curr_latitude = this.get_latitude_decimal();
+	double target_longitude = target.get_longitude_decimal();
+	double target_latitude = target.get_latitude_decimal();
+	double diff_longitude = target_longitude - curr_longitude;
+	double diff_latitude = target_latitude - curr_latitude;
+	double angle = pow(sin(diff_latitude/2), 2) + cos(curr_latitude)) * cos(target_latitude) * pow(sin(diff_longitude/2), 2);
+	double c = 2 * atan2(sqrt(angle), sqrt(1-angle));
+	return 6371.01 * c;	
 }
 
 GIS[] GIS::nearby(double range) const{
@@ -74,6 +68,18 @@ string GIS::toString() const{
 }
 
 double GIS::sort(double &list[]){
+}
+
+/// Converts decimal to sexagesimal
+float GIS::decimaltoSexagesimal(float decimal){
+}
+
+/// Converts degrees of seconds to sexagesimal
+float GIS::degreeSecondstoSexagesimal(float degreeSeconds){
+}
+
+float GIS::setPrecision(float value, int precision){
+    return (floor((value * pow(10, precision) + 0.5)) / pow(10, precision)); 
 }
 
 }
