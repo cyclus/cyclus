6e5934f4889bd7c97f40a8d34fe7ca2a796ae559
Anthony Scopatz <scopatz@gmail.com>
2016-12-20 19:18:46 -0800

    member

--- cyclus/memback.pxd
+++ cyclus/memback.pxd
@@ -1,6 +1,8 @@
 """An in-memory Python backend for Cyclus."""
-from libcpp.vector cimport vector
-from libcpp.utility cimport pair
+from libcpp.map cimport map as std_map
+from libcpp.set cimport set as std_set
+from libcpp.vector cimport vector as std_vector
+from libcpp.utility cimport pair as std_pair
 from libcpp.string cimport string as std_string
 from libcpp cimport bool as cpp_bool
 
@@ -8,12 +10,17 @@ from cyclus cimport cpp_cyclus
 from cyclus cimport lib
 
 
-cdef cppclass CyclusMemBack "CyclusMemBack" (cpp_cyclus.FullBackend):
+cdef cppclass CyclusMemBack "CyclusMemBack" (cpp_cyclus.RecBackend):
+    # Recorder backend interface
     void Notify(cpp_cyclus.DatumList) except +
     std_string Name() except +
     void Flush() except +
     void Close() except +
-
+    # Queryable backend interface
+    #cpp_cyclus.QueryResult Query(std_string, vector[cpp_cyclus.Cond]*) except +
+    #std_map[std_string, cpp_cyclus.DbTypes] ColumnTypes(std_string) except +
+    #std_set[std_string] Tables() except +
+    dict cache
 
 cdef class _MemBack(lib._FullBackend):
     pass  # pointer declared on full backend
--- cyclus/memback.pyx
+++ cyclus/memback.pyx
@@ -1,9 +1,9 @@
 """An in-memory Python backend for Cyclus."""
 from __future__ import print_function, unicode_literals
-from libcpp.map cimport map
-from libcpp.set cimport set
-from libcpp.vector cimport vector
-from libcpp.utility cimport pair
+from libcpp.map cimport map as std_map
+from libcpp.set cimport set as std_set
+from libcpp.vector cimport vector as std_vector
+from libcpp.utility cimport pair as std_pair
 from libcpp.string cimport string as std_string
 from libcpp cimport bool as cpp_bool
 
@@ -12,8 +12,8 @@ from cyclus cimport lib
 from cyclus import lib
 
 
-cdef cppclass CyclusMemBack "CyclusMemBack" (cpp_cyclus.FullBackend):
-    # A C++ class that acts as a full backend, but stores its data in
+cdef cppclass CyclusMemBack "CyclusMemBack" (cpp_cyclus.RecBackend):
+    # A C++ class that acts as a rec backend, but stores its data in
     # memory in a Python friendly format.
 
     void Notify(cpp_cyclus.DatumList dl):
@@ -41,6 +41,52 @@ cdef class _MemBack(lib._FullBackend):
         cdef CyclusMemBack* cpp_ptx = <CyclusMemBack*> self.ptx
         del cpp_ptx
 
+    def query(self, table, conds=None):
+        """Queries a database table.
+
+        Parameters
+        ----------
+        table : str
+            The table name.
+        conds : iterable, optional
+            A list of conditions.
+
+        Returns
+        -------
+        results : pd.DataFrame
+            Pandas DataFrame the represents the table
+        """
+        raise NotImplementedError
+
+    @property
+    def tables(self):
+        raise NotImplementedError
+
+    @tables.setter
+    def tables(self, value):
+        """Retrieves the set of tables present in the database."""
+        raise NotImplementedError
+
+
+    def flush(self):
+        """No-op since in-memory."""
+        # this is a no-op
+
+    def close(self):
+        """Closes the backend, flushing it in the process."""
+        self.flush()  # just in case
+        (<CyclusMemBack*> self.ptx).Close()
+
+    @property
+    def name(self):
+        """The name of the database."""
+        return "<Python In-Memory Backend at " + str(self.ptx) + ">"
+
 
 class MemBack(_MemBack, lib.FullBackend):
-    """An in-memory Python backend database."""
+    """An in-memory Python backend database.
+
+    Note that even though the underlying C++ class is a RecBackend,
+    the Python wrapper inherits from FullBackend and the QueryableBackend
+    interface in implemented in Cython.
+    """
