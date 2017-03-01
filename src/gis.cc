afd8ec1d450e51f014ed8651e7a3bd4f77e03764
gyutaepark <gtpark96@gmail.com>
2017-02-28 20:01:54 -0600

    Styled code according to style guide and recommendations from arfc repository

--- src/gis.cc
+++ src/gis.cc
@@ -4,39 +4,28 @@ using namespace std;
 
 namespace cyclus {
 
-GIS::GIS() {
-  latitude = 0.0;
-  longitude = 0.0;
-}
+GIS::GIS() : latitude_(0), longitude_(0) {}
 
 GIS::GIS(float decimal_lat, float decimal_lon) {
-  latitude = setPrecision(decimal_lat * 3600, 1);
-  longitude = setPrecision(decimal_lon * 3600, 1);
+  latitude_ = setPrecision(decimal_lat * 3600, 1);
+  longitude_ = setPrecision(decimal_lon * 3600, 1);
 }
 
 GIS::~GIS() {}
 
-float GIS::get_latitude_decimal() const {
-  return setPrecision(latitude / 3600, 6);
-}
+float GIS::get_latitude() const { return setPrecision(latitude_ / 3600, 6); }
 
-float GIS::get_longitude_decimal() const {
-  return setPrecision(longitude / 3600, 6);
-}
+float GIS::get_longitude() const { return setPrecision(longitude_ / 3600, 6); }
 
-void GIS::set_latitude_decimal(float lat) {
-  latitude = setPrecision(lat * 3600, 1);
-}
+void GIS::set_latitude(float lat) { latitude_ = setPrecision(lat * 3600, 1); }
 
-void GIS::set_longitude_decimal(float lon) {
-  longitude = setPrecision(lon * 3600, 1);
-}
+void GIS::set_longitude(float lon) { longitude_ = setPrecision(lon * 3600, 1); }
 
-double GIS::get_distance(GIS target) const {
-  double curr_longitude = this->get_longitude_decimal() * M_PI / 180;
-  double curr_latitude = this->get_latitude_decimal() * M_PI / 180;
-  double target_longitude = target.get_longitude_decimal() * M_PI / 180;
-  double target_latitude = target.get_latitude_decimal() * M_PI / 180;
+double GIS::Distance(GIS target) const {
+  double curr_longitude = this->get_longitude() * M_PI / 180;
+  double curr_latitude = this->get_latitude() * M_PI / 180;
+  double target_longitude = target.get_longitude() * M_PI / 180;
+  double target_latitude = target.get_latitude() * M_PI / 180;
   double dlong = target_longitude - curr_longitude;
   double dlat = target_latitude - curr_latitude;
 
@@ -45,61 +34,46 @@ double GIS::get_distance(GIS target) const {
       pow(sin(dlong / 2), 2) * cos(curr_latitude) * cos(target_latitude);
 
   double temp2 = 2 * atan2(sqrt(temp), sqrt(1 - temp));
-  return 6372.8 * temp2;
+  return 6372.8 * temp2;  // 6372.8 is the radius of earth in KM
 }
 
-string GIS::toStringD() const {
+string GIS::toString(string return_format) const {
   stringstream lat_string;
   stringstream lon_string;
-  float lat = this->get_latitude_decimal();
-  float lon = this->get_longitude_decimal();
+  float lat = this->get_latitude();
+  float lon = this->get_longitude();
+  switch (return_format) {
+    case "d":
+      lat_string << ToStringHelperLat(return_format, lat);
+      lon_string << ToStringHelperLon(return_format, lon);
 
-  lat_string << toStringhelperlat(1, lat);
-  lon_string << toStringhelperlon(1, lon);
-
-  lat_string << setprecision(7) << fabs(lat);
-  lon_string << setprecision(7) << fabs(lon) << "/";
-
+      lat_string << setprecision(7) << fabs(lat);
+      lon_string << setprecision(7) << fabs(lon) << "/";
+      break;
+    case "dm":
+      lat_string << ToStringHelperLat(return_format, lat);
+      lon_string << ToStringHelperLon(return_format, lon);
+      break;
+    case "dms":
+      lat_string << ToStringHelperLat(return_format, lat);
+      lon_string << ToStringHelperLon(return_format, lon);
+      break;
+  }
   return lat_string.str() + lon_string.str();
 }
 
-string GIS::toStringDM() const {
-  stringstream lat_string;
-  stringstream lon_string;
-  double lat = this->get_latitude_decimal();
-  double lon = this->get_longitude_decimal();
-
-  lat_string << toStringhelperlat(2, lat);
-  lon_string << toStringhelperlon(2, lon);
-
-  return lat_string.str() + lon_string.str();
-}
-
-string GIS::toStringDMS() const {
-  stringstream lat_string;
-  stringstream lon_string;
-  double lat = this->get_latitude_decimal();
-  double lon = this->get_longitude_decimal();
-
-  lat_string << toStringhelperlat(3, lat);
-  lon_string << toStringhelperlon(3, lon);
-
-  return lat_string.str() + lon_string.str();
-}
-
-float GIS::setPrecision(float value, double precision) const {
+float GIS::SetPrecision(float value, double precision) const {
+  if (precision == 0) {
+    return floor(value);
+  }
   return (floor((value * pow(10, precision) + 0.5)) / pow(10, precision));
 }
 
-string GIS::toStringhelperlat(int mode, int lat) {
+string GIS::ToStringHelperLat(string mode, float lat) {
   string lat_string;
   double lat_int;
 
-  if (lat > 0) {
-    lat_string << "+";
-  } else {
-    lat_string << "-";
-  }
+  lat_string << ToStringHelper(lat);
 
   lat = modf(lat, &lat_int);
   if (((int)lat_int) / 10 == 0) {
@@ -107,45 +81,23 @@ string GIS::toStringhelperlat(int mode, int lat) {
   }
 
   switch (mode) {
-    case 1:
-
+    case "d":
       break;
-
-    case 2:
-
-      lat_string << fabs(lat_int);
-      lat = fabs(lat) * 60;
-      if ((int)fabs(lat) / 10 == 0) {
-        lat_string << "0";
-      }
-      lat_string << setprecision(5) << lat;
-
-    case 3:
-
-      lat_string << abs((int)lat_int);
-      lat = modf(fabs(lat) * 60, &lat_int);
-      if (((int)lat_int) / 10 == 0) {
-        lat_string << "0";
-      }
-      lat_string << fabs(lat_int);
-      lat = lat * 60;
-      if ((int)fabs(lat) / 10 == 0) {
-        lat_string << "0";
-      }
-      lat_string << setprecision(1) << fixed << lat;
+    case "dm":
+      lat_string << ToStringHelperDM("lat", lat);
+      break;
+    case "dms":
+      lat_string << ToStringHelperDMS("lat", lat);
+      break;
   }
   return lat_string;
 }
 
-string GIS::toStringhelperlon(int mode, int lon) {
+string GIS::ToStringHelperLon(string mode, float lon) {
   string lon_string;
   double lon_int;
 
-  if (lon > 0) {
-    lon_string << "+";
-  } else {
-    lon_string << "-";
-  }
+  lon_string << ToStringHelper(lon);
 
   lon = modf(lon, &lon_int);
   if (((int)lon_int) / 100 == 0) {
@@ -156,35 +108,83 @@ string GIS::toStringhelperlon(int mode, int lon) {
   }
 
   switch (mode) {
-    case 1:
-
+    case "d":
       break;
-
-    case 2:
-
-      lon_string << fabs(lon_int);
-      lon = fabs(lon) * 60;
-      if ((int)fabs(lon) / 10 == 0) {
-        lon_string << "0";
-      }
-      lon_string << setprecision(5) << lon << "/";
+    case "dm":
+      lon_string << ToStringHelperDM("lon", lon);
       break;
-
-    case 3:
-
-      lon_string << abs((int)lon_int);
-      lon = modf(fabs(lon) * 60, &lon_int);
-      if (((int)lon_int) / 10 == 0) {
-        lon_string << "0";
-      }
-      lon_string << fabs(lon_int);
-      lon = lon * 60;
-      if ((int)fabs(lon) / 10 == 0) {
-        lon_string << "0";
-      }
-      lon_string << setprecision(1) << fixed << lon << "/";
+    case "dms":
+      lon_string << ToStringHelperDMS("lon", lon);
       break;
   }
   return lon_string;
+}
+
+string GIS::ToStringHelper(float value) {
+  string temp;
+  if (value > 0) {
+    temp << "+";
+  } else {
+    temp << "-";
+  }
+  return temp;
+}
+
+       {
+  string temp;
+  double value_int;
+  switch (mode) {
+    case "lat":
+      temp << fabs(value_int);
+      value = fabs(value) * 60;
+      if ((int)fabs(value) / 10 == 0) {
+        temp << "0";
+      }
+      temp << setprecision(5) << value;
+      break;
+    case "lon":
+      temp << fabs(value_int);
+      value = fabs(value) * 60;
+      if ((int)fabs(value) / 10 == 0) {
+        temp "0";
+      }
+      temp << setprecision(5) << value << "/";
+      break;
+  }
+  return temp;
+}
+
+string GIS::ToStringHelperDMS(string mode, float value) {
+  string temp;
+  double value_int;
+  switch (mode) {
+    case "lat":
+      temp << abs((int)value_int);
+      value = modf(fabs(value) * 60, &value_int);
+      if (((int)value_int) / 10 == 0) {
+        temp << "0";
+      }
+      temp << fabs(value_int);
+      value = value * 60;
+      if ((int)fabs(value) / 10 == 0) {
+        temp << "0";
+      }
+      temp << setprecision(1) << fixed << value;
+      break;
+    case "lon":
+      temp << abs((int)value_int);
+      value = modf(fabs(value) * 60, &value_int);
+      if (((int)value_int) / 10 == 0) {
+        temp << "0";
+      }
+      temp << fabs(value_int);
+      value = value * 60;
+      if ((int)fabs(value) / 10 == 0) {
+        temp << "0";
+      }
+      temp << setprecision(1) << fixed << value << "/";
+      break;
+  }
+  return temp;
 }
 }
--- src/gis.h
+++ src/gis.h
@@ -6,8 +6,6 @@
 #include <sstream>
 #include <string>
 
-#include <boost/shared_ptr.hpp>
-
 #include "cyclus.h"
 
 // forward declarations
@@ -17,93 +15,155 @@ class GIS;
 
 namespace cyclus {
 
-///
 /// @class GIS
 /// The GIS class is a basic class that stores the geographic location of each
-/// agents in latitude and longitude and follows the ISO
-/// 6709 standard. Allows, string expression of GIS objects as specified in
-/// ANNEX H.
+/// agent in latitude and longitude and follows the ISO
+/// 6709 standard. Allows string expression of GIS objects as specified in
+/// ANNEX H of ISO 6709 found here: 'https://www.iso.org/standard/39242.html'
+/// or a quick simplified version found here
+/// 'https://en.wikipedia.org/wiki/ISO_6709'
 ///
 /// Brief Description of ISO 6709:
 /// 1. Latitude comes before Longitude
 /// 2. North latitude is positive
 /// 3. East longitude is positive
-/// 4. Fraction of degrees is prefered in digital data exchange.
+/// 4. Fraction of degrees (decimal values) is prefered in digital data
+/// exchange.
 ///
 /// The class is adapted from 'https://github.com/jaime-olivares/coordinate'
 /// under the MIT License.
 ///
+/// Longitude and Latitude is stored as seconds of degrees. This was also
+/// adapted from 'https://github.com/jaime-olivares/coordinate'. This, according
+/// to the jaime-olivares, allows the coordinate elements such as degrees,
+/// minutes, and seconds to "remain on the integral portion of values, with the
+/// exception of decimal of seconds, avoiding loss of precision." This is
+/// calculated by multiplying decimal degrees by 3600.
+/// example: 05.2169 -> 18780.84
 
 class GIS {
  public:
-  /// The default constructor for GIS. Make sure to add latitude and longitude
-  /// if this constructor is used.
+  /// The default constructor for GIS. This will creat an object with
+  /// lat =0, and long=0.
   GIS();
 
-  /// GIS constructor with latitude and longditude as decimal number.
-  /// @param latitude and longditude expressed in Degrees.
+  /// GIS constructor with latitude and longditude as degrees as a decimal
+  /// number.
+  /// @param deciaml_lat latitude expressed in degrees as a decimal number.
+  /// @param decimal_lon longditude expressed in degrees as a decimal number.
   GIS(float decimal_lat, float decimal_lon);
 
   /// The default destructor for GIS
   ~GIS();
 
-  /// Getters for latitude and longitude
-  /// @return decimal representation of latitude or longitude.
-  float get_latitude_decimal() const;
-  float get_longitude_decimal() const;
+  /// Returns the current latitude.
+  /// @return decimal representation of latitude.
+  float get_latitude() const;
 
-  /// Setter for latitude in decimals. Please input the latitude as degrees not
-  /// as DDMMSS.S
-  /// @param latitude in decimals
-  void set_latitude_decimal(float lat);
+  /// Returns the current longitude.
+  /// @return decimal representation of longitude.
+  float get_longitude() const;
 
-  /// Setter for latitude in decimals. Please input the longitude as degrees not
-  /// as DDDMMSS.S
-  /// @param longitude in decimals
-  void set_longitude_decimal(float lon);
+  /// Sets a new latitude
+  /// @param lat latitude expressed in decimal degrees.
+  void set_latitude(float lat);
 
-  /// returns the distance between the GIS object the function has been called
-  /// in kilometers
-  /// @param GIS object that this is compared to
+  /// Sets a new longitude
+  /// @param lon longitude expressed in decimal degrees.
+  void set_longitude(float lon);
+
+  /// Returns the distance (in km) between this GIS object and the target GIS
+  /// object.
+  /// @param target the GIS object some distnace away from the current one
   /// @return distance between this and target in kilometers
-  double get_distance(GIS target) const;
+  double Distance(GIS target) const;
 
-  /// converts GIS location into a string expression that follows ISO 6709 Annex
-  /// H as degrees.
-  /// @return string representation of the GIS object that follows ISO 6709
-  /// Annex H. Degrees format.
-  std::string toStringD() const;
-
-  /// converts GIS location into a string expression that follows ISO 6709 Annex
-  /// H as degrees and minutes.
-  /// @return string representation of the GIS object that follows ISO 6709
-  /// Annex H. Degrees and Minutes format.
-  std::string toStringDM() const;
-
-  /// converts GIS location into a string expression that follows ISO 6709 Annex
-  /// H as degrees minutes and seconds.
-  /// @return string representation of the GIS object that follows ISO 6709
-  /// Annext H. Degrees, minutes and seconds format.
-  std::string toStringDMS() const;
+  /// Converts GIS location into a string expression that follows ISO 6709 Annex
+  /// H.
+  /// @param return_format
+  /// @parblock
+  /// 			The format of output. Options are 'd', 'dm', or 'dms':
+  ///
+  /// 			d:   return in degrees format
+  ///			dm:  return in degrees and minutes format
+  ///			dms: return in egrees minutes seconds format
+  /// @endparblock
+  /// @return String representation of the GIS object that complies with ISO6709
+  /// Annex H
+  std::string ToString(std::string return_format) const;
 
  private:
-  /// Latitude is stored as seconds of degree.
-  float latitude;
+  /// Latitude is stored as seconds of degree. Explanation and example is
+  /// available above in class documentation
+  float latitude_;
 
   /// Longitude is stored as seconds of degree.
-  float longitude;
+  float longitude_;
 
   /// Sets the precision for float values.
-  /// @param value to set the precision, and the precision
-  /// @return value with the precision as defined
-  float setPrecision(float value, double precision) const;
+  /// @param value that requires change of precision
+  /// @param precision the number decimal places wanted
+  /// @return value with the number of decimal places (precision) as defined
+  float SetPrecision(float value, double precision) const;
 
-  /// Helper for toString functions
-  /// @param value detemines which to string function this helps
-  /// 1:D, 2:DM, 3:DMS
+  /// Formats longitude for toString function
+  /// @param mode
+  /// @parblock
+  /// 			The format of output. Options are 'd', 'dm' or 'dms' :
+  ///
+  /// 			d:   return in degrees format
+  ///			dm:  return in degrees and minutes format
+  ///			dms: return in degrees minutes seconds format
+  /// @endparblock
+  /// @param lon longitude of the object
   /// @return partially formatted string
-  string toStringhelperlon(int mode, int lon);
-  string toStringhelperlat(int mode, int lat);
+  string ToStringHelperLon(std::string mode, int lon);
+
+  /// Formats latitude for toString function
+  /// @param mode
+  /// @parblock
+  /// 			The format of output. Options are 'd', 'dm' or 'dms' :
+  ///
+  /// 			d:   return in degrees format
+  ///			dm:  return in degrees and minutes format
+  ///			dms: return in degrees minutes seconds format
+  /// @endparblock
+  /// @param lat latitude of the object
+  /// @return partially formatted string
+  string ToStringHelperLat(std::string mode, int lat);
+
+  /// Adds "+" or "-" sign for the ToStringHelperLon/Lat function
+  /// @param value longitude or latitude of the object
+  /// @returns "+" or "-" for the ToString function
+  string ToStringHelper(float value);
+
+  /// Formats longitude and latitude in degrees and minutes for the
+  /// ToStringHelperLon/Lat function
+  /// @param mode
+  /// @parblock
+  /// 			The format of input. Options are "lon" and "lat":
+  ///
+  /// 			lon:  return formatted string representation of
+  /// longitude
+  ///			lat:  return formatted string representation of latitude
+  /// @endparblock
+  /// @param value longitude or latitude of the object
+  /// @return formatted string representation of longiiiitude or latitude
+  string ToStringHelperDM(string mode, float value);
+
+  /// Formats longitude and latitude in degrees minutes and seconds for the
+  /// ToStringHelperLon/Lat function
+  /// @param mode
+  /// @parblock
+  /// 			The format of input. Options are "lon" and "lat":
+  ///
+  /// 			lon:  return formatted string representation of
+  /// longitude
+  ///			lat:  return formatted string representation of latitude
+  /// @endparblock
+  /// @param value longitude or latitude of the object
+  /// @return formatted string representation of longiiiitude or latitude
+  string ToStringHelperDMS(string mode, float value);
 };
 }
 
