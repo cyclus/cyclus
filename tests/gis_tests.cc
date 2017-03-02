7aba9470e0280d9157104de1418f8cb182bce0f4
gyutaepark <gtpark96@gmail.com>
2017-03-01 22:43:58 -0600

    Created Test fixture and updated function names

--- tests/gis_tests.cc
+++ tests/gis_tests.cc
@@ -8,7 +8,10 @@ using namespace std;
 
 using cyclus::GIS;
 
-TEST(GISTests, check_distance) {
+class GISTest : public ::testing::TEST {
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
   GIS eiffel(48.858222, 2.2945);
   GIS museum(48.861111, 2.336389);
   GIS amsterdam(52.373056, 4.892222);
@@ -16,30 +19,31 @@ TEST(GISTests, check_distance) {
   GIS urbana(40.109665, -88.204247);
   GIS newyork(40.7127, -74.0059);
   GIS bloomington(40.484167, -88.993611);
-
-  EXPECT_NEAR(eiffel.get_distance(museum), 3.188, 3.188 * 0.05)
-      << "eiffel vs museum failed";
-  EXPECT_NEAR(eiffel.get_distance(amsterdam), 432.126, 432.126 * 0.01)
-      << "eiffel vs amsterdam failed";
-  EXPECT_NEAR(amsterdam.get_distance(barcelona), 1240.110, 1240.110 * 0.01)
-      << "amsterdam vs barcelona failed";
-  EXPECT_NEAR(amsterdam.get_distance(newyork), 5868.701, 5868.701 * 0.01)
-      << "amsterdam vs newyork failed";
-  EXPECT_NEAR(newyork.get_distance(urbana), 1204.246, 1204.246 * 0.01)
-      << "newyork vs urbana failed";
-  EXPECT_NEAR(bloomington.get_distance(urbana), 78.663, 78.663 * 0.01)
-      << "bloomington vs urbana failed";
-}
-
-TEST(GISTests, check_toStringD) {
   GIS amsterdam(52.373056, 4.892222);
   GIS sydney(-33.865, 151.209444);
   GIS saopaulo(-23.55, -46.633333);
-  GIS urbana(40.109665, -88.204247);
-  string ams_str = amsterdam.toStringD();
-  string syd_str = sydney.toStringD();
-  string sao_str = saopaulo.toStringD();
-  string urb_str = urbana.toStringD();
+}
+
+TEST(GISTests, Distance) {
+  EXPECT_NEAR(eiffel.Distance(museum), 3.188, 3.188 * 0.05)
+      << "eiffel vs museum failed";
+  EXPECT_NEAR(eiffel.Distance(amsterdam), 432.126, 432.126 * 0.01)
+      << "eiffel vs amsterdam failed";
+  EXPECT_NEAR(amsterdam.Distance(barcelona), 1240.110, 1240.110 * 0.01)
+      << "amsterdam vs barcelona failed";
+  EXPECT_NEAR(amsterdam.Distance(newyork), 5868.701, 5868.701 * 0.01)
+      << "amsterdam vs newyork failed";
+  EXPECT_NEAR(newyork.Distance(urbana), 1204.246, 1204.246 * 0.01)
+      << "newyork vs urbana failed";
+  EXPECT_NEAR(bloomington.Distance(urbana), 78.663, 78.663 * 0.01)
+      << "bloomington vs urbana failed";
+}
+
+TEST(GISTests, ToStringD) {
+  string ams_str = amsterdam.toString("d");
+  string syd_str = sydney.toString("d");
+  string sao_str = saopaulo.toString("d");
+  string urb_str = urbana.toString("d");
 
   ASSERT_TRUE(ams_str == "+52.37305+004.892222/");
   ASSERT_TRUE(syd_str == "-33.865+151.2094/");
@@ -47,15 +51,11 @@ TEST(GISTests, check_toStringD) {
   ASSERT_TRUE(urb_str == "+40.10966-088.20425/");
 }
 
-TEST(GISTests, check_toStringDM) {
-  GIS amsterdam(52.373056, 4.892222);
-  GIS sydney(-33.865, 151.209444);
-  GIS saopaulo(-23.55, -46.633333);
-  GIS urbana(40.109665, -88.204247);
-  string ams_str = amsterdam.toStringDM();
-  string syd_str = sydney.toStringDM();
-  string sao_str = saopaulo.toStringDM();
-  string urb_str = urbana.toStringDM();
+TEST(GISTests, ToStringDM) {
+  string ams_str = amsterdam.toString("dm");
+  string syd_str = sydney.toString("dm");
+  string sao_str = saopaulo.toString("dm");
+  string urb_str = urbana.toString("dm");
 
   ASSERT_TRUE(ams_str == "+5222.383+00453.533/");
   ASSERT_TRUE(syd_str == "-3351.9+15112.567/");
@@ -63,15 +63,11 @@ TEST(GISTests, check_toStringDM) {
   ASSERT_TRUE(urb_str == "+4006.5799-08812.255/");
 }
 
-TEST(GISTests, check_toStringDMS) {
-  GIS amsterdam(52.373056, 4.892222);
-  GIS sydney(-33.865, 151.209444);
-  GIS saopaulo(-23.55, -46.633333);
-  GIS urbana(40.109665, -88.204247);
-  string ams_str = amsterdam.toStringDMS();
-  string syd_str = sydney.toStringDMS();
-  string sao_str = saopaulo.toStringDMS();
-  string urb_str = urbana.toStringDMS();
+TEST(GISTests, ToStringDMS) {
+  string ams_str = amsterdam.toString("dms");
+  string syd_str = sydney.toString("dms");
+  string sao_str = saopaulo.toString("dms");
+  string urb_str = urbana.toString("dms");
 
   ASSERT_TRUE(ams_str == "+522223.0+0045332.0/");
   ASSERT_TRUE(syd_str == "-335154.0+1511234.0/");
