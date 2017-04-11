9d414bb2c0bcbca894ec98eb23602c97c7ac56f8
gyutaepark <gtpark96@gmail.com>
2017-04-10 23:36:51 -0500

    added & tested distance test

--- agents/gisfac_tests.cc
+++ agents/gisfac_tests.cc
@@ -16,33 +16,47 @@ class GISFacTest : public ::testing::Test {
   GISFac* facility_a_;
   GISFac* facility_b_;
 
-  virtual void SetUp() {}
+  virtual void SetUp() {
+    facility_a_ = new GISFac(tc_.get());
+    facility_b_ = new GISFac(tc_.get());
+    facility_a_->gis = new cyclus::GIS();
+    facility_b_->gis = new cyclus::GIS();
+  }
 
   virtual void TearDown() {}
 };
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(GISFacTest, set_longitude) {
-  facility_a_->gis->set_longitude(32.5);
-  EXPECT_NEAR(facility_a_->gis->get_longitude(), 32.5, 32.5 * 0.01);
-  facility_a_->gis->set_longitude(-2.5546);
-  EXPECT_NEAR(facility_a_->gis->get_longitude(), -2.5546, 2.5546 * 0.01);
+  facility_a_->gis->set_longitude(46.545821);
+  EXPECT_NEAR(facility_a_->gis->get_longitude(), 46.545821, 32.5 * 0.01);
+  facility_a_->gis->set_longitude(-82.5546);
+  EXPECT_NEAR(facility_a_->gis->get_longitude(), -82.5546, 82.5546 * 0.01);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(GISFacTest, set_latitude) {
   facility_a_->gis->set_latitude(32.5);
-  std::cout << facility_a_->gis->get_latitude() << std::endl;
   EXPECT_NEAR(facility_a_->gis->get_latitude(), 32.5, 32.5 * 0.01);
   facility_a_->gis->set_latitude(-2.5546);
-  std::cout << facility_a_->gis->get_latitude() << std::endl;
   EXPECT_NEAR(facility_a_->gis->get_latitude(), -2.5546, 2.5546 * 0.01);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(GISFacTest, InitialState) {
   // Test things about the initial state of the facility here
-  EXPECT_EQ(facility_a_->gis->get_longitude(), 0);
-  EXPECT_EQ(facility_a_->gis->get_latitude(), 0);
+  EXPECT_NEAR(facility_a_->gis->get_longitude(), 0, 0.001);
+  EXPECT_NEAR(facility_a_->gis->get_latitude(), 0, 0.001);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(GISFacTest, Distance) {
+  EXPECT_NEAR(facility_a_->gis->Distance(*(facility_b_->gis)), 0, 0.0001);
+  facility_a_->gis->set_latitude(48.858222);
+  facility_a_->gis->set_longitude(2.2945);
+  facility_b_->gis->set_latitude(52.37305);
+  facility_b_->gis->set_longitude(4.892222);
+  EXPECT_NEAR(facility_a_->gis->Distance(*(facility_b_->gis)), 432.126,
+              432.126 * 0.01);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
