7742d499cba2679fccc1930f3e2333300f71328b
gyutaepark <gtpark96@gmail.com>
2017-05-17 18:00:12 -0500

    added basic gisfac class for testing

---
+++ tests/test_agents/test_gisfac.h
@@ -0,0 +1,17 @@
+#ifndef CYCLUS_TESTS_TEST_AGENTS_TEST_GISFAC_H_
+#define CYCLUS_TESTS_TEST_AGENTS_TEST_GISFAC_H_
+
+#include <sstream>
+#include "cyclus.h"
+#include "toolkit/gis.h"
+
+namespace cyclus {
+/// This is the simplest possible GISFac, for testing
+class TestGISFac : public cyclus::toolkit::GIS {
+ public:
+  TestGISFac(long latitude, long longitude) : cyclus::toolkit::GIS(0, 0) {}
+  virtual ~TestGISFac() {}
+  // virtual cyclus::Agent* Clone() { return new TestGISFac(context()); }
+};
+}
+#endif  // CYCLUS_TESTS_TEST_AGENTS_TEST_GISFAC_H_
