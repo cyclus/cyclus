e17ecebfba456a7c418e5522765fbcc0df8838bf
gyutaepark <gtpark96@gmail.com>
2017-05-17 17:55:16 -0500

    Replaced gisfac_tests to agent_tests

--- tests/agent_tests/gisfac_tests.cc
+++ tests/agent_tests/gisfac_tests.cc
@@ -1,24 +1,23 @@
 #include <gtest/gtest.h>
 
-#include "tests/test_agents/gisfac.h"
+#include "test_agents/test_gisfac.h"
 
 #include <iostream>
 #include "agent_tests.h"
 #include "context.h"
 #include "facility_tests.h"
 
-using cyclus::GISFacisa;
+using cyclus::TestGISFac;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class GISFacisaTest : public ::testing::Test {
+class GISFacTest : public ::testing::Test {
  protected:
-  cyclus::TestContext tc_;
-  GISFacisa* facility_a_;
-  GISFacisa* facility_b_;
+  TestGISFac* facility_a_;
+  TestGISFac* facility_b_;
 
   virtual void SetUp() {
-    facility_a_ = new GISFacisa(tc_.get());
-    facility_b_ = new GISFacisa(tc_.get());
+    facility_a_ = new TestGISFac(0, 0);
+    facility_b_ = new TestGISFac(0, 0);
   }
 
   virtual void TearDown() {
@@ -27,7 +26,7 @@ class GISFacisaTest : public ::testing::Test {
   }
 };
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(GISFacisaTest, set_longitude) {
+TEST_F(GISFacTest, set_longitude) {
   facility_a_->set_longitude(46.545821);
   EXPECT_NEAR(facility_a_->get_longitude(), 46.545821, 32.5 * 0.01);
   facility_a_->set_longitude(-82.5546);
@@ -35,7 +34,7 @@ TEST_F(GISFacisaTest, set_longitude) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(GISFacisaTest, set_latitude) {
+TEST_F(GISFacTest, set_latitude) {
   facility_a_->set_latitude(32.5);
   EXPECT_NEAR(facility_a_->get_latitude(), 32.5, 32.5 * 0.01);
   facility_a_->set_latitude(-2.5546);
@@ -43,14 +42,14 @@ TEST_F(GISFacisaTest, set_latitude) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(GISFacisaTest, InitialState) {
+TEST_F(GISFacTest, InitialState) {
   // Test things about the initial state of the facility here
   EXPECT_NEAR(facility_a_->get_longitude(), 0, 0.001);
   EXPECT_NEAR(facility_a_->get_latitude(), 0, 0.001);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(GISFacisaTest, Constructor) {
+TEST_F(GISFacTest, Constructor) {
   facility_a_->set_latitude(48.858222);
   facility_a_->set_longitude(2.2945);
   EXPECT_NEAR(facility_a_->get_latitude(), 48.858222, 48.858222 * 0.01);
@@ -58,7 +57,7 @@ TEST_F(GISFacisaTest, Constructor) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(GISFacisaTest, Distance) {
+TEST_F(GISFacTest, Distance) {
   EXPECT_NEAR(facility_a_->Distance(*(facility_b_)), 0, 0.0001);
   facility_a_->set_latitude(48.858222);
   facility_a_->set_longitude(2.2945);
@@ -67,21 +66,9 @@ TEST_F(GISFacisaTest, Distance) {
   EXPECT_NEAR(facility_a_->Distance(*(facility_b_)), 432.126, 432.126 * 0.01);
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Agent* GISFacisaConstructor(cyclus::Context* ctx) {
-  return new GISFacisa(ctx);
-}
-
 // Required to get functionality in cyclus agent unit tests library
 #ifndef CYCLUS_AGENT_TESTS_CONNECTED
 int ConnectAgentTests();
 static int cyclus_agent_tests_connected = ConnectAgentTests();
 #define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
 #endif  // CYCLUS_AGENT_TESTS_CONNECTED
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-INSTANTIATE_TEST_CASE_P(GISFacisaFac, FacilityTests,
-                        ::testing::Values(&GISFacisaConstructor));
-
-INSTANTIATE_TEST_CASE_P(GISFacisaFac, AgentTests,
-                        ::testing::Values(&GISFacisaConstructor));
