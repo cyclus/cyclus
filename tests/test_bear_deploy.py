79bd4f29512f3e7b014981aec044a41e8ee40aa6
flanflanagan <flanny65@gmail.com>
2018-10-05 14:21:46 -0600

    test fix

--- tests/test_bear_deploy.py
+++ tests/test_bear_deploy.py
@@ -19,8 +19,8 @@ inputfile = {
    'config': {
     'DemandFac': {
      'commodity': 'bears',
-     'production_rate_max': 10,
-     'production_rate_min': 10,
+     'production_rate_max': 12,
+     'production_rate_min': 8,
     },
    },
    'name': 'BearStore',
@@ -57,7 +57,7 @@ def test_bear_deploy():
     # test that the institution deploys a BearStore
     assert_in("New fac: BearStore", s)
     # test that the first agents exist with right minimum production.
-    agents = re.compile('Agent \d+ 10\.0')
+    agents = re.compile('Agent \d+ 8\.0')
     all_agents = set(agents.findall(s))
     assert_greater_equal(len(all_agents), 9)
     if os.path.exists('bears.json'):
