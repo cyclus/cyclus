d3042b397a14ec2017a50f52432cc09d9378bcf5
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-06-11 09:56:56 -0500

    prepararing metadata.cycpp.h

--- src/toolkit/metadata.cycpp.h
+++ src/toolkit/metadata.cycpp.h
@@ -2,31 +2,28 @@
 // This includes the required header to add geographic coordinates to a
 // archetypes.
 // One only need to:
-// - '#include "toolkit/position.cycpp"' in the core of the archetype class (as
+// - '#include "toolkit/metadata.cycpp"' in the core of the archetype class (as
 // private)
 // - and in the EnterNotify() method:
-//   - set the coordinates 'coordinates = cyclus::toolkit::Position(latitude,
-//   longitude);'
-//   - call the record method: 'coordinates.RecordPosititon(this);'
-
-cyclus::toolkit::Position coordinates;
+//   - set the metadata 
+//      'coordinates = cyclus::toolkit::Position(latitude,longitude);'
 
 #pragma cyclus var { \
-"default": 0.0, \
-"uilabel": "Geographical latitude in degrees as a double", \
-"doc": "Latitude of the agent's geographical position. The value should " \
-       "be expressed in degrees as a double." \
-}
-double latitude;
+    "alias": ["usagemetadata", "keyword", ["usage", "key", "value"]], \
+    "uitype": ["onemore", "string", ["onemore", "string", "double"]], \
+    "uilabel": "", \
+    "doc": "", \
+  }
+  std::map<std::string, std::map<std::string, double> > usage_metadata_;
 // required for compilation but not added by the cycpp preprocessor...
-std::vector<int> cycpp_shape_latitude;
+  std::vector<int> cycpp_shape_usage_metadata_;
 
 #pragma cyclus var { \
-"default": 0.0, \
-"uilabel": "Geographical longitude in degrees as a double", \
-"doc": "Longitude of the agent's geographical position. The value should " \
-       "be expressed in degrees as a double." \
-}
-double longitude;
+    "alias": ["usagemetadata", "keyword", ["usage", "key", "value"]], \
+    "uitype": ["onemore", "string", ["onemore", "string", "string"]], \
+    "uilabel": "", \
+    "doc": "", \
+  }
+  std::map<std::string, std::map<std::string, std::string> > metadata_;
 // required for compilation but not added by the cycpp preprocessor...
-std::vector<int> cycpp_shape_longitude;
+  std::vector<int> cycpp_shape_metadata_;
