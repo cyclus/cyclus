786792731c5fe945c7657740d55a68c390b63e2e
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-09-09 16:35:06 -0500

    does it work better ?

--- news/news_test.sh
+++ news/news_test.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+# If default branch skip the test
+branch=`git branch | grep \* | cut -d ' ' -f2`
+def_branch="master"
+if [ "$branch" == "$def" ]; then
+    return 0
+fi
+
 folder=$1
 added_news_file=$((`git diff origin/master --name-only $folder |wc -l`))
+
 if [ $added_news_file -eq 0 ]; then
     exit 1
 fi
---
+++ news/news_trigger_2.rst
@@ -0,0 +1,12 @@
+**Added:** None
+
+**Changed:** None
+
+**Deprecated:** None
+
+**Removed:** None
+
+**Fixed:** None
+-fix news test trigger
+
+**Security:** None
