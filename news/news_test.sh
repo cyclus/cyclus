f7aa86e9e311f7ef1441f4014dd25753fb4e9cfb
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-09-13 15:31:07 -0500

    add soem comment

--- news/news_test.sh
+++ news/news_test.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 folder=$1
+#setup temp remote 
 git_remote_name=ci_news_`git log --pretty=format:'%h' -n 1`
 git remote add ${git_remote_name} https://github.com/cyclus/cyclus/
 git fetch ${git_remote_name}
+# diff against temp remote
 added_news_file=$((`git diff ${git_remote_name}/master --name-only $folder |wc -l`))
+#cleaning temp remote
 git remote remove ${git_remote_name}
+
 if [ $added_news_file -eq 0 ]; then
     exit 1
 fi
