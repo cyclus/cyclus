8a5488a05f7cf5655151872a107bac8b5fd58d09
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-09-13 15:29:43 -0500

    setup a temp remote to diff against

--- news/news_test.sh
+++ news/news_test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 folder=$1
-added_news_file=$((`git diff origin/master --name-only $folder |wc -l`))
+git_remote_name=ci_news_`git log --pretty=format:'%h' -n 1`
+git remote add ${git_remote_name} https://github.com/cyclus/cyclus/
+git fetch ${git_remote_name}
+added_news_file=$((`git diff ${git_remote_name}/master --name-only $folder |wc -l`))
+git remote remove ${git_remote_name}
 if [ $added_news_file -eq 0 ]; then
     exit 1
 fi
