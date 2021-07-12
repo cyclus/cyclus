794d06cad5c7899d13e6d024f8c2d3988984cdbd
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-07-12 07:10:42 -0500

    is the cpp include actually required, as we include the hpp in the header?

--- src/OsiCbcSolverInterface.cpp
+++ src/OsiCbcSolverInterface.cpp
@@ -1,15 +1,15 @@
 #include "CbcConfig.h"
 
-// CBC_VERSION_MAJOR defined for Cbc > 2.5
-#ifndef CBC_VERSION_MAJOR
-#include "OsiCbcSolverInterface_2_5.cpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
-#include "OsiCbcSolverInterface_2_8.cpp"
-#elif CBC_VERSION_MAJOR == 2 && ( CBC_VERSION_MINOR == 9 || CBC_VERSION_MINOR == 10)
-#include "OsiCbcSolverInterface_2_9_10.cpp"
-#else
-#error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
-#endif
+// // CBC_VERSION_MAJOR defined for Cbc > 2.5
+// #ifndef CBC_VERSION_MAJOR
+// #include "OsiCbcSolverInterface_2_5.cpp"
+// #elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
+// #include "OsiCbcSolverInterface_2_8.cpp"
+// #elif CBC_VERSION_MAJOR == 2 && ( CBC_VERSION_MINOR == 9 || CBC_VERSION_MINOR == 10)
+// #include "OsiCbcSolverInterface_2_9_10.cpp"
+// #else
+// #error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
+// #endif
 
 
 // for some reason these symbol doesn't exist in the mac binaries
