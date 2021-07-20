245cb0e1f1e9900cb96bf9ce760ea34f404e5524
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-07-20 08:16:56 -0500

    readding CbC cpp inclusion

--- src/OsiCbcSolverInterface.cpp
+++ src/OsiCbcSolverInterface.cpp
@@ -2,15 +2,15 @@
 #include "OsiCbcSolverInterface.hpp"
 
 // // CBC_VERSION_MAJOR defined for Cbc > 2.5
-// #ifndef CBC_VERSION_MAJOR
-// #include "OsiCbcSolverInterface_2_5.hpp"
-// #elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
-// #include "OsiCbcSolverInterface_2_8.hpp"
-// #elif CBC_VERSION_MAJOR == 2 && ( CBC_VERSION_MINOR == 9 || CBC_VERSION_MINOR == 10)
-// #include "OsiCbcSolverInterface_2_9_10.hpp"
-// #else
-// #error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
-// #endif
+#ifndef CBC_VERSION_MAJOR
+#include "OsiCbcSolverInterface_2_5.cpp"
+#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
+#include "OsiCbcSolverInterface_2_8.cpp"
+#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 10)
+#include "OsiCbcSolverInterface_2_10.cpp"
+#else
+#error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
+#endif
 
 
 // for some reason these symbol doesn't exist in the mac binaries
