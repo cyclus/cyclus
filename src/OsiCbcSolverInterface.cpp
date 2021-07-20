bbd0b4b85f26086f68f00840ed3aef9d9c703e3a
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-07-20 08:29:03 -0500

    removing parentheses

--- src/OsiCbcSolverInterface.cpp
+++ src/OsiCbcSolverInterface.cpp
@@ -6,7 +6,7 @@
 #include "OsiCbcSolverInterface_2_5.cpp"
 #elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
 #include "OsiCbcSolverInterface_2_8.cpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 10)
+#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 10
 #include "OsiCbcSolverInterface_2_10.cpp"
 #else
 #error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
