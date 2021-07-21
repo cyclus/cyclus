3836cd34e42fafa6cb276dd85879965e891d39ee
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-07-21 17:16:37 -0500

    how about now ?

--- src/toolkit/mat_query.cc
+++ src/toolkit/mat_query.cc
@@ -20,9 +20,11 @@ double MatQuery::moles(Nuc nuc) {
     std::cout << __FILE__ << " " << __LINE__ << std::endl;
     double loc_mass = mass(nuc);
     std::cout << __FILE__ << " " << __LINE__ << std::endl;
-    double atm = pyne::atomic_mass(nuc) * units::g;
+    double atm = pyne::atomic_mass(nuc);
     std::cout << __FILE__ << " " << __LINE__ << std::endl;
-    double rtn = loc_mass / atm;
+    double atm_u = atm * units::g;
+    std::cout << __FILE__ << " " << __LINE__ << std::endl;
+    double rtn = loc_mass / atm_u;
     std::cout << __FILE__ << " " << __LINE__ << std::endl;
 
     return rtn;
