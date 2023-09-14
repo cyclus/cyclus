acd284d87d32b84af38a95088c81b6e62f6f2d53
Paul P.H. Wilson <paul.wilson@wisc.edu>
2023-09-14 17:01:38 -0500

    update pandas testing module

--- tests/test_memback.py
+++ tests/test_memback.py
@@ -7,7 +7,7 @@ from cyclus import typesystem as ts
 
 import numpy as np
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 
 def make_rec_back(inject_sim_id=False):
