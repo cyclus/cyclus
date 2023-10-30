50b2dc06ebab25d0024deabe8c99c4035fd54c6f
bennibbelink <bnibbs@vt.edu>
2023-10-30 15:36:35 -0500

    moved cbc includes around

--- src/OsiCbcSolverInterface.hpp
+++ src/OsiCbcSolverInterface.hpp
@@ -8,13 +8,13 @@
 
 // CBC_VERSION_MAJOR defined for Cbc > 2.5
 #ifndef CBC_VERSION_MAJOR
-#include "OsiCbcSolverInterface_2_5.hpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
-#include "OsiCbcSolverInterface_2_8.hpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 10
-#include "OsiCbcSolverInterface_2_10.hpp"
-#else
 #error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
+#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR < 8
+#include "OsiCbcSolverInterface_2_5.hpp"
+#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR < 10
+#include "OsiCbcSolverInterface_2_8.hpp"
+#else
+#include "OsiCbcSolverInterface_2_10.hpp"
 #endif
 
 #endif
