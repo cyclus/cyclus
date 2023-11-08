0827f29d1e6d545c3a438889e4e00ce18d3ad142
unknown <bennibbelink@gmail.com>
2023-11-08 10:30:43 -0600

    consolidate OsiCbcSolverInterface version files

--- src/OsiCbcSolverInterface.cpp
+++ src/OsiCbcSolverInterface.cpp
@@ -1,17 +1,7 @@
 #include "CbcConfig.h"
 #include "OsiCbcSolverInterface.hpp"
 
-// CBC_VERSION_MAJOR defined for Cbc > 2.5
-#ifndef CBC_VERSION_MAJOR
-#include "OsiCbcSolverInterface_2_5.cpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
-#include "OsiCbcSolverInterface_2_8.cpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 10
 #include "OsiCbcSolverInterface_2_10.cpp"
-#else
-#error "Cyclus cannot yet handle your version of CoinCBC. Please open an issue with your CoinCBC version."
-#endif
-
 
 // for some reason these symbol doesn't exist in the mac binaries
 // Those were also not present in condaforge/linux-anvil-comp7 docker container
