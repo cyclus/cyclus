5b3547dfd20c0dbda7a9d617ddafe522303ae566
Ben Nibbelink <bnibbs@vt.edu>
2024-02-06 12:41:53 -0600

    working with PYTHONPATH=/root/.local/lib and have the cython libs in /root/.local/lib

--- cyclus/__init__.py
+++ cyclus/__init__.py
@@ -1,7 +1,3 @@
-from __future__ import print_function, unicode_literals
-
 __version__ = '1.5.5'
-
-from cyclus.lib import py_import_call_init
-
-py_import_call_init()
+# from cyclus.lib import py_import_call_init, py_import_init
+import pymodule, eventhooks, pyinfile
