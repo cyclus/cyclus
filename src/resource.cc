391b5d7b540962ea0072bcc7fe86be29f55fa7d0
Katie Mummah <mummah@lanl.gov>
2024-04-17 10:34:37 -0600

    no package in resource

--- src/resource.cc
+++ src/resource.cc
@@ -10,22 +10,4 @@ void Resource::BumpStateId() {
   nextstate_id_++;
 }
 
-std::vector<Resource::Ptr> Resource::Package(Package::Ptr pkg) {
-  std::vector<Resource::Ptr> rs_pkgd;
-  Resource::Ptr r_pkgd;
-
-  double fill_mass = pkg->GetFillMass(quantity());
-  if (fill_mass == 0) {
-    return rs_pkgd;
-  }
-
-  while (quantity() > pkg->fill_min()) {
-    double pkg_fill = std::min(quantity(), fill_mass);
-    r_pkgd = ExtractRes(pkg_fill);
-    r_pkgd->ChangePackageId(pkg->id());
-    rs_pkgd.push_back(r_pkgd);
-  }
-  return rs_pkgd;
-}
-
 }  // namespace cyclus
