6525993a36d012e9ef4a4e6350e43597d431ce34
bennibbelink <79653949+bennibbelink@users.noreply.github.com>
2024-04-19 21:25:30 -0500

    gtest always has parameterized tests now, GTEST_HAS_PARAM_TEST no longer exists

--- tests/agent_tests/facility_tests.h
+++ tests/agent_tests/facility_tests.h
@@ -9,8 +9,6 @@
 #include "test_context.h"
 #include "test_agents/test_inst.h"
 
-#if GTEST_HAS_PARAM_TEST
-
 using ::testing::TestWithParam;
 using ::testing::Values;
 
@@ -31,17 +29,5 @@ class FacilityTests : public TestWithParam<AgentConstructor*> {
   cyclus::Facility* facility_;
   cyclus::TestContext tc_;
 };
-
-#else
-
-// Google Test may not support value-parameterized tests with some
-// compilers. If we use conditional compilation to compile out all
-// code referring to the gtest_main library, MSVC linker will not link
-// that library at all and consequently complain about missing entry
-// point defined in that library (fatal error LNK1561: entry point
-// must be defined). This dummy test keeps gtest_main linked in.
-TEST(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
-
-#endif  // GTEST_HAS_PARAM_TEST
 
 #endif  // CYCLUS_TESTS_AGENT_TESTS_FACILITY_TESTS_H_
