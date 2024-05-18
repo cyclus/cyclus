fc69db02e7749519b4237b4dabd367ff708ba0a7
bennibbelink <79653949+bennibbelink@users.noreply.github.com>
2024-05-18 18:24:15 -0500

    use cy_large_double instead of max double

--- agents/k_facility.h
+++ agents/k_facility.h
@@ -200,7 +200,7 @@ class KFacility : public cyclus::Facility {
   double current_capacity;
 
   #pragma cyclus var { \
-    "default": "cy_max_double", \
+    "default": "cy_large_double", \
     "tooltip": "k-facility maximum inventory size", \
     "doc": "total maximum inventory size of the k-facility", \
     "uilabel": "Maximum Inventory"			     \
