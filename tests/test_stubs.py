9ccbe001fd210f496487f08deeffe82af2fa9a94
Paul P.H. Wilson <paul.wilson@wisc.edu>
2025-05-30 19:48:12 -0500

    Add test to make sure the cycstub example input runs with different variations of the library name

--- tests/test_stubs.py
+++ tests/test_stubs.py
@@ -19,52 +19,62 @@ def tmplog(fname):
     yield file_ptr
     os.remove(fname)
 
-def test_stubs():
-    flavors = ['facility', 'inst', 'region']
-    pth = 'foo'
+@pytest.mark.parametrize("pth", ['stubtest', 'stub_test', 'stubTest'])
+def test_stubs(pth):
+    log = f'stub_test_{pth}.log'
+    src = os.path.join(d, pth)
+    bld = os.path.join(d, 'bar')
+    inst = os.path.join(d, 'baz')
+
+    stub_cmd = 'cycstub --type {0} {1}:{1}:{2}'
+    inst_cmd = 'python3 install.py --build_dir {0} --prefix {1}'
+    tst_cmd = f'./bin/{pth}_unit_tests'
+    run_cmd = 'cyclus example_tmp_facility.xml'
+
     with tmpdir() as d:
-        src = os.path.join(d, pth)
-        bld = os.path.join(d, 'bar')
-        inst = os.path.join(d, 'baz')
+        with tmplog(log) as f:
 
-        stub_cmd = 'cycstub --type {0} {1}:{1}:{2}'
-        inst_cmd = 'python3 install.py --build_dir {0} --prefix {1}'
-        tst_cmd = './bin/{}_unit_tests'
+            msg = f'**Error, check the log in {log}**'
+            os.mkdir(src)
+            for flav in ['facility', 'inst', 'region']:
+                # generate stub
+                cmd = stub_cmd.format(flav, pth, 'tmp' + flav.capitalize())
+                print(cmd)
+                try:
+                    subprocess.check_call(cmd.split(), shell=(os.name=='nt'),
+                                            cwd=src, stdout=f, stderr=f)
+                except subprocess.CalledProcessError as e:
+                    print(msg)
+                    raise e
 
-        log = 'stub_test.log'
-        msg = '**Error, check the log in {}**'.format(log)
-        os.mkdir(src)
-        for flav in flavors:
-            # generate stub
-            cmd = stub_cmd.format(flav, pth, 'tmp' + flav.capitalize())
+            # build/install stub
+            cmd = inst_cmd.format(bld, inst)
             print(cmd)
             try:
-                with tmplog(log) as f:
-                    subprocess.check_call(cmd.split(), shell=(os.name=='nt'),
-                                          cwd=src, stdout=f, stderr=f)
+                subprocess.check_call(cmd.split(), shell=(os.name=='nt'),
+                                        cwd=src, stdout=f, stderr=f)
+            except subprocess.CalledProcessError as e:
+                print(msg)
+                pytest.skip(msg)  # skip if we can't install for some reason.
+
+            # run unit tests for stub
+            cmd = tst_cmd
+            print(cmd)
+            try:
+                subprocess.check_call(cmd.split(), shell=(os.name=='nt'), cwd=inst,
+                                        stdout=f, stderr=f)
             except subprocess.CalledProcessError as e:
                 print(msg)
                 raise e
 
-        # build/install stub
-        cmd = inst_cmd.format(bld, inst)
-        print(cmd)
-        try:
-            with tmplog(log) as f:
-                subprocess.check_call(cmd.split(), shell=(os.name=='nt'),
-                                      cwd=src, stdout=f, stderr=f)
-        except subprocess.CalledProcessError as e:
-            print(msg)
-            pytest.skip(msg)  # skip if we can't install for some reason.
-
-        # run unit tests for stub
-        cmd = tst_cmd.format(pth)
-        print(cmd)
-        try:
-            with tmplog(log) as f:
-                subprocess.check_call(cmd.split(), shell=(os.name=='nt'), cwd=inst,
-                                      stdout=f, stderr=f)
-        except subprocess.CalledProcessError as e:
-            print(msg)
-            raise e
-
+            # run sample file for stub
+            cmd = run_cmd.format('tmp_facility')
+            print(cmd)
+            my_env = os.environ.copy()
+            my_env["CYCLUS_PATH"] = f"{inst}/lib/cyclus"
+            try:
+                subprocess.check_call(cmd.split(), shell=(os.name=='nt'), cwd=src,
+                                        stdout=f, stderr=f, env=my_env)
+            except subprocess.CalledProcessError as e:
+                print(msg)
+                raise e
