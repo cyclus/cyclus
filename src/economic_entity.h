fff59884bd2219f4f6b096ab7d4a7b490d2602b4
Dean Krueger <dean.krueger@wisc.edu>
2025-07-19 21:02:50 -0500

    made some minor bug-fix-type corrections which became apparant after implementation

--- src/economic_entity.h
+++ src/economic_entity.h
@@ -129,9 +129,12 @@ class EconomicEntity {
       return pv;
   }
 
+  protected:
+  static constexpr int kDefaultBidCost = 1;
+
   private:
   std::unordered_map<std::string, double> financial_data_;
-  static constexpr int kDefaultBidCost = 1;
+
 };
 
 #endif  // ECONOMIC_ENTITY_H
