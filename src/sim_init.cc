45e5fad57af6200e02ca5ec5bac68f5368d2609e
Dean Krueger <dean.krueger@wisc.edu>
2026-01-06 10:35:14 -0600

    Revert "added a catch for legacy inputs continuing after the change, should be able to gracefully handle missing ExchangeMode entries in the table"
    This reverts commit 85c2b498cdcd55bf6d2f1216c698ca3b2ffd6f82.

--- src/sim_init.cc
+++ src/sim_init.cc
@@ -153,13 +153,8 @@ void SimInit::LoadInfo() {
   std::string h = qr.GetVal<std::string>("Handle");
   QueryResult dq = b_->Query("DecayMode", NULL);
   std::string d = dq.GetVal<std::string>("Decay");
-  std::string e = "legacy";  // default for backward compatibility
-  try {
-    QueryResult eq = b_->Query("ExchangeMode", NULL);
-    e = eq.GetVal<std::string>("Exchange");
-  } catch (std::exception err) {
-    // table doesn't exist (okay) - use default "legacy"
-  }
+  QueryResult eq = b_->Query("ExchangeMode", NULL);
+  std::string e = eq.GetVal<std::string>("Exchange");
   si_ = SimInfo(dur, y0, m0, h, d, e);
 
   si_.seed = qr.GetVal<int>("Seed");
